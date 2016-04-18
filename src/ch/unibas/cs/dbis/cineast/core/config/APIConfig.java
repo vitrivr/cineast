453232416ae687397cf69a9f4c48618fbd454602
Luca Rossetto <luca.rossetto@unibas.ch>
2016-04-18 16:01:29 +0200

    added parse methods to APIConfig and ExtractorConfig

--- src/ch/unibas/cs/dbis/cineast/core/config/APIConfig.java
+++ src/ch/unibas/cs/dbis/cineast/core/config/APIConfig.java
@@ -1,5 +1,7 @@
 package ch.unibas.cs.dbis.cineast.core.config;
 
+import com.eclipsesource.json.JsonObject;
+
 public final class APIConfig {
 
 	private final int jsonApiPort;
@@ -33,6 +35,59 @@ public final class APIConfig {
 	
 	public boolean getEnableCli(){
 		return this.enableCLI;
+	}
+	
+	/**
+	 * 
+	 * expects a json object of the follwing form:
+	 * <pre>
+	 * {
+	 * 	"jsonApiPort" : (int)
+	 * 	"allowExtraction" : (boolean)
+	 * 	"enableCLI" : (boolean)
+	 * }
+	 * </pre>
+	 * @throws NullPointerException in case provided JsonObject is null
+	 * @throws IllegalArgumentException if any of the specified parameters does not have the expected type or is outside the valid range
+	 */
+	public static APIConfig parse(JsonObject obj) throws NullPointerException, IllegalArgumentException{
+		if(obj == null){
+			throw new NullPointerException("JsinObject was null");
+		}
+		
+		int jsonApiPort = DEFAULT_JSON_API_PORT;
+		if(obj.get("jsonApiPort") != null){
+			try{
+				jsonApiPort = obj.get("jsonApiPort").asInt();
+			}catch(UnsupportedOperationException e){
+				throw new IllegalArgumentException("'jsonApiPort' was not an integer in API configuration");
+			}
+			
+			if(jsonApiPort <= 0 || jsonApiPort >= 65535){
+				throw new IllegalArgumentException("'jsonApiPort' must be > 0 and < 65536");
+			}
+		}
+		
+		boolean allowExtraction = DEFAULT_ALLOW_EXTRACTION;
+		if(obj.get("allowExtraction") != null){
+			try{
+				allowExtraction = obj.get("allowExtraction").asBoolean();
+			}catch(UnsupportedOperationException e){
+				throw new IllegalArgumentException("'allowExtraction' was not a boolean in API configuration");
+			}
+		}
+		
+		boolean enableCLI = DEFAULT_ENABLE_CLI;
+		if(obj.get("enableCLI") != null){
+			try{
+				enableCLI = obj.get("enableCLI").asBoolean();
+			}catch(UnsupportedOperationException e){
+				throw new IllegalArgumentException("'enableCLI' was not a boolean in API configuration");
+			}
+		}
+		
+		return new APIConfig(jsonApiPort, allowExtraction, enableCLI);
+		
 	}
 	
 }
--- src/ch/unibas/cs/dbis/cineast/core/config/ExtractorConfig.java
+++ src/ch/unibas/cs/dbis/cineast/core/config/ExtractorConfig.java
@@ -1,5 +1,7 @@
 package ch.unibas.cs.dbis.cineast.core.config;
 
+import com.eclipsesource.json.JsonObject;
+
 public final class ExtractorConfig {
 
 	private final int shotQueueSize;
@@ -31,5 +33,69 @@ public final class ExtractorConfig {
 
 	public int getTaskQueueSize() {
 		return this.taskQueueSize;
+	}
+	
+	
+	/**
+	 * 
+	 * expects a json object of the follwing form:
+	 * <pre>
+	 * {
+	 * 	"shotQueueSize" : (int)
+	 * 	"threadPoolSize" : (int)
+	 * 	"taskQueueSize" : (int)
+	 * }
+	 * </pre>
+	 * 
+	 * @throws NullPointerException in case the provided JsonObject is null
+	 * @throws IllegalArgumentException if any of the specified values is not a positive integer
+	 * 
+	 */
+	public static ExtractorConfig parse(JsonObject obj){
+		if(obj == null){
+			throw new NullPointerException("JsonObject was null");
+		}
+		
+		int shotQueueSize = DEFAULT_SHOT_QUEUE_SIZE;
+		if(obj.get("shotQueueSize") != null){
+			try{
+				shotQueueSize = obj.get("shotQueueSize").asInt();
+			}catch(UnsupportedOperationException e){
+				throw new IllegalArgumentException("'shotQueueSize' was not an integer in extractor configuration");
+			}
+			
+			if(shotQueueSize <= 0){
+				throw new IllegalArgumentException("'shotQueueSize' must be > 0");
+			}
+		}
+		
+		int threadPoolSize = DEFAULT_THREAD_POOL_SIZE;
+		if(obj.get("threadPoolSize") != null){
+			try{
+				threadPoolSize = obj.get("threadPoolSize").asInt();
+			}catch(UnsupportedOperationException e){
+				throw new IllegalArgumentException("'threadPoolSize' was not an integer in extractor configuration");
+			}
+			
+			if(shotQueueSize <= 0){
+				throw new IllegalArgumentException("'threadPoolSize' must be > 0");
+			}
+		}
+		
+		int taskQueueSize = DEFAULT_TASK_QUEUE_SIZE;
+		if(obj.get("taskQueueSize") != null){
+			try{
+				taskQueueSize = obj.get("taskQueueSize").asInt();
+			}catch(UnsupportedOperationException e){
+				throw new IllegalArgumentException("'taskQueueSize' was not an integer in extractor configuration");
+			}
+			
+			if(shotQueueSize <= 0){
+				throw new IllegalArgumentException("'taskQueueSize' must be > 0");
+			}
+		}
+		
+		return new ExtractorConfig(shotQueueSize, threadPoolSize, taskQueueSize);
+		
 	}
 }
