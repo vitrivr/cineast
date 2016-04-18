abcb9a510cb1bbabbed3e630775147ee2b8662a6
Luca Rossetto <luca.rossetto@unibas.ch>
2016-04-18 15:42:55 +0200

    added parse method to DecoderConfig

--- src/ch/unibas/cs/dbis/cineast/core/config/DecoderConfig.java
+++ src/ch/unibas/cs/dbis/cineast/core/config/DecoderConfig.java
@@ -1,5 +1,7 @@
 package ch.unibas.cs.dbis.cineast.core.config;
 
+import com.eclipsesource.json.JsonObject;
+
 public final class DecoderConfig { //TODO add decoder class
 
 	private final int maxFrameWidth;
@@ -23,6 +25,52 @@ public final class DecoderConfig { //TODO add decoder class
 	
 	public int getMaxFrameHeight(){
 		return this.maxFrameHeight;
+	}
+	
+	/**
+	 * expects a json object of the follwing form:
+	 * <pre>
+	 * {
+	 * 	"maxFrameWidth" : (int)
+	 * 	"maxFrameHeight" : (int)
+	 * }
+	 * </pre>
+	 * @throws NullPointerException in case the given object is null
+	 * @throws IllegalArgumentException in case the specified frame width or height are not positive integers
+	 */
+	public static DecoderConfig parse(JsonObject obj) throws NullPointerException, IllegalArgumentException{
+		if(obj == null){
+			throw new NullPointerException("JsonObject was null");
+		}
+		
+		int maxFrameWidth = DEFAULT_MAX_FRAME_WIDTH;
+		if(obj.get("maxFrameWidth") != null){
+			try{
+				maxFrameWidth = obj.get("maxFrameWidth").asInt();
+			}catch(UnsupportedOperationException e){
+				throw new IllegalArgumentException("'maxFrameWidth' was not an integer in decoder configuration");
+			}
+			
+			if(maxFrameWidth <= 0){
+				throw new IllegalArgumentException("'maxFrameWidth' must be > 0");
+			}
+		}
+		
+		int maxFrameHeight = DEFAULT_MAX_FRAME_HEIGHT;
+		if(obj.get("maxFrameHeight") != null){
+			try{
+				maxFrameHeight = obj.get("maxFrameHeight").asInt();
+			}catch(UnsupportedOperationException e){
+				throw new IllegalArgumentException("'maxFrameHeight' was not an integer in decoder configuration");
+			}
+			
+			if(maxFrameHeight <= 0){
+				throw new IllegalArgumentException("'maxFrameHeight' must be > 0");
+			}
+		}
+		
+		return new DecoderConfig(maxFrameWidth, maxFrameHeight);
+		
 	}
 	
 }
