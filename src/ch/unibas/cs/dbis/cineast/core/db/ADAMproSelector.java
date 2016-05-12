dff88450316826abfc2f1f9dbe39230cf10a2c4c
Luca Rossetto <luca.rossetto@unibas.ch>
2016-05-12 16:16:49 +0200

    added distance definition to ADAMproSelector

--- src/ch/unibas/cs/dbis/cineast/core/db/ADAMproSelector.java
+++ src/ch/unibas/cs/dbis/cineast/core/db/ADAMproSelector.java
@@ -1,6 +1,7 @@
 package ch.unibas.cs.dbis.cineast.core.db;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -9,6 +10,8 @@ import com.google.common.util.concurrent.ListenableFuture;
 import ch.unibas.cs.dbis.cineast.core.config.QueryConfig;
 import ch.unibas.cs.dbis.cineast.core.data.FloatArrayIterable;
 import ch.unibas.cs.dbis.cineast.core.data.StringDoublePair;
+import ch.unibas.dmi.dbis.adam.http.Grpc.DistanceMessage;
+import ch.unibas.dmi.dbis.adam.http.Grpc.DistanceMessage.DistanceType;
 import ch.unibas.dmi.dbis.adam.http.Grpc.FeatureVectorMessage;
 import ch.unibas.dmi.dbis.adam.http.Grpc.NearestNeighbourQueryMessage;
 import ch.unibas.dmi.dbis.adam.http.Grpc.QueryResponseInfoMessage;
@@ -20,6 +23,17 @@ public class ADAMproSelector implements DBSelector {
 	private String entityName;
 	private SimpleQueryMessage.Builder sqmBuilder = SimpleQueryMessage.newBuilder();
 	private NearestNeighbourQueryMessage.Builder nnqmBuilder = NearestNeighbourQueryMessage.newBuilder();
+	
+	private static DistanceMessage minkowski_1;
+	
+	static{
+		
+		HashMap<String, String> tmp = new HashMap<>();
+		tmp.put("norm", "1");
+		
+		minkowski_1 = DistanceMessage.newBuilder().setDistancetype(DistanceType.minkowski).putAllOptions(tmp).build();
+		
+	}
 	
 	@Override
 	public boolean open(String name) {
@@ -40,7 +54,7 @@ public class ADAMproSelector implements DBSelector {
 		
 		FeatureVectorMessage fvqm = FeatureVectorMessage.newBuilder().addAllVector(new FloatArrayIterable(vector)).build();
 		
-		NearestNeighbourQueryMessage nnqMessage = nnqmBuilder.setColumn(column).setQuery(fvqm).setK(k).build();
+		NearestNeighbourQueryMessage nnqMessage = nnqmBuilder.setColumn(column).setQuery(fvqm).setK(k).setDistance(minkowski_1).build();
 		SimpleQueryMessage sqMessage = sqmBuilder.setEntity(entityName).setNnq(nnqMessage).build();
 		
 		ListenableFuture<QueryResponseInfoMessage> future = ADAMproWrapper.getInstance().standardQuery(sqMessage);
