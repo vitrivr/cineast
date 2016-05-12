1ea7b1c7338faba8b8b3d089985a8108e7c07454
zydu <zhiyingdu@gmail.com>
2016-05-12 19:35:55 +0800

    add motion2D estimator in order to eliminate camera motion

--- src/ch/unibas/cs/dbis/cineast/core/descriptor/PathList.java
+++ src/ch/unibas/cs/dbis/cineast/core/descriptor/PathList.java
@@ -7,15 +7,19 @@ import java.util.List;
 import boofcv.abst.feature.detect.interest.ConfigGeneralDetector;
 import boofcv.abst.feature.tracker.PointTrack;
 import boofcv.abst.feature.tracker.PointTracker;
+import boofcv.abst.sfm.d2.ImageMotion2D;
 import boofcv.alg.filter.derivative.GImageDerivativeOps;
 import boofcv.alg.tracker.klt.PkltConfig;
 import boofcv.factory.feature.tracker.FactoryPointTracker;
+import boofcv.factory.sfm.FactoryMotion2D;
 import boofcv.io.image.ConvertBufferedImage;
 import boofcv.struct.image.GrayU8;
 import ch.unibas.cs.dbis.cineast.core.data.Frame;
 import ch.unibas.cs.dbis.cineast.core.data.MultiImage;
 import ch.unibas.cs.dbis.cineast.core.data.Pair;
+import georegression.struct.homography.Homography2D_F64;
 import georegression.struct.point.Point2D_F32;
+import georegression.transform.homography.HomographyPointOps_F64;
 import gnu.trove.map.hash.TLongIntHashMap;
 import gnu.trove.map.hash.TLongObjectHashMap;
 
@@ -36,12 +40,26 @@ private PathList(){}
 		config.pyramidScaling = new int[] { 1, 2, 4, 8 };
 		PointTracker<GrayU8> tracker = FactoryPointTracker.klt(config, new ConfigGeneralDetector(numberOfPointsToTrack, 3, 1), GrayU8.class, GImageDerivativeOps.getDerivativeType(GrayU8.class));
 		
+		PointTracker<GrayU8> trackerForMotion2D = FactoryPointTracker.klt(config, new ConfigGeneralDetector(numberOfPointsToTrack, 3, 1), GrayU8.class, GImageDerivativeOps.getDerivativeType(GrayU8.class));
+		ImageMotion2D<GrayU8,Homography2D_F64> motion2D = FactoryMotion2D.createMotion2D(50,3,2,0,0.6,0.5,false,trackerForMotion2D,new Homography2D_F64());
+		Homography2D_F64 curToFirst = new Homography2D_F64(1.0,0.0,0.0,
+															0.0,1.0,0.0,
+															0.0,0.0,1.0);
+		Homography2D_F64 curToPrev = new Homography2D_F64();
+		PointTrack transP = new PointTrack();
+		
 		TLongObjectHashMap<LinkedList<Point2D_F32>> paths = new TLongObjectHashMap<LinkedList<Point2D_F32>>();
 		TLongIntHashMap trackStartFrames = new TLongIntHashMap();
 		ArrayList<PointTrack> tracks = new ArrayList<PointTrack>(numberOfPointsToTrack);
 		GrayU8 gray = null;
 		for(Frame f : frames){
 			gray = ConvertBufferedImage.convertFrom(f.getImage().getBufferedImage(), gray);
+			
+			motion2D.process(gray);
+			motion2D.getFirstToCurrent().invert(curToPrev);
+			Homography2D_F64 curToFirstOld = curToFirst.copy();
+			curToFirstOld.concat(curToPrev, curToFirst);
+			motion2D.setToFirst();
 			
 			tracker.process(gray);
 			tracks.clear();
@@ -59,7 +77,8 @@ private PathList(){}
 					path = new LinkedList<Point2D_F32>();
 					paths.put(p.featureId, path);
 				}
-				path.add(new Point2D_F32((float)(p.x / gray.width), (float)(p.y / gray.height)));
+				HomographyPointOps_F64.transform(curToFirst, p, transP);
+				path.add(new Point2D_F32((float)(transP.x / gray.width), (float)(transP.y / gray.height)));
 			}
 		}
 		
