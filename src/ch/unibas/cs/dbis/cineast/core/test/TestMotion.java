ea3a312f09b8b81f3bac933b9e2c9bf22304683b
zydu <zhiyingdu@gmail.com>
2016-08-02 00:11:42 +0800

    Added 3D gaussian blur filter & some small changes

--- src/ch/unibas/cs/dbis/cineast/core/test/TestMotion.java
+++ src/ch/unibas/cs/dbis/cineast/core/test/TestMotion.java
@@ -1,24 +1,14 @@
 package ch.unibas.cs.dbis.cineast.core.test;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import boofcv.gui.feature.VisualizeFeatures;
-import boofcv.gui.image.ImagePanel;
-import boofcv.gui.image.ShowImages;
-import boofcv.io.image.ConvertBufferedImage;
 import boofcv.struct.geo.AssociatedPair;
 import boofcv.struct.image.GrayU8;
 import ch.unibas.cs.dbis.cineast.core.data.Frame;
@@ -27,14 +17,15 @@ import ch.unibas.cs.dbis.cineast.core.data.Shot;
 import ch.unibas.cs.dbis.cineast.core.decode.video.JLibAVVideoDecoder;
 import ch.unibas.cs.dbis.cineast.core.decode.video.VideoDecoder;
 import ch.unibas.cs.dbis.cineast.core.descriptor.PathList;
+import ch.unibas.cs.dbis.cineast.core.util.Histogram;
+import ch.unibas.cs.dbis.cineast.core.util.VisualizationUtil;
+import ch.unibas.cs.dbis.cineast.core.util.MaskGenerator;
 import georegression.struct.point.Point2D_F32;
 
 
 public class TestMotion {
 
 	private static final Logger LOGGER = LogManager.getLogger();
-	
-	public static ImagePanel gui = new ImagePanel();
 	
 	public static void main(String[] args) {
 		if(args.length < 1){
@@ -61,8 +52,8 @@ public class TestMotion {
 		long separateTime = System.currentTimeMillis();
 		LOGGER.info("finished separate bg and fg in {}", formatTime(separateTime - denseTrackingTime));
 		
-		Pair<List<Double>, ArrayList<ArrayList<Float>>> foregroundPair = getSubDivHist(1, foregroundPaths);
-		Pair<List<Double>, ArrayList<ArrayList<Float>>> backgroundPair = getSubDivHist(1, backgroundPaths);
+		Pair<List<Double>, ArrayList<ArrayList<Float>>> foregroundPair = Histogram.getSubDivHist(1, foregroundPaths);
+		Pair<List<Double>, ArrayList<ArrayList<Float>>> backgroundPair = Histogram.getSubDivHist(1, backgroundPaths);
 		System.out.println(foregroundPair.first.get(0));
 		System.out.println(foregroundPair.first.get(0)/shot.getFrames().size());
 		System.out.println(foregroundPair.second);
@@ -72,147 +63,12 @@ public class TestMotion {
 		long getHistTime = System.currentTimeMillis();
 		LOGGER.info("finished getHist bg and fg in {}", formatTime(getHistTime - separateTime));
 		
-		LinkedList<GrayU8> masks = PathList.getFgMasks(shot.getFrames(), foregroundPaths);
+		ArrayList<GrayU8> masks = MaskGenerator.getFgMasks(shot.getFrames(), foregroundPaths);
 		long getFgMasksTime = System.currentTimeMillis();
 		LOGGER.info("finished getFgMasks in {}", formatTime(getFgMasksTime - getHistTime));
 		
-		visualize(shot.getFrames(),foregroundPaths,backgroundPaths,masks);
+		VisualizationUtil.visualize(shot.getFrames(),foregroundPaths,backgroundPaths,masks);
 		
-	}
-	
-	private static Pair<List<Double>, ArrayList<ArrayList<Float>>> getSubDivHist(
-			int subdiv, List<Pair<Integer, LinkedList<Point2D_F32>>> list) {
-
-		double[] sums = new double[subdiv * subdiv];
-		float[][] hists = new float[subdiv * subdiv][8];
-
-		for (Pair<Integer, LinkedList<Point2D_F32>> pair : list) {
-			LinkedList<Point2D_F32> path = pair.second;
-			if (path.size() > 1) {
-				Iterator<Point2D_F32> iter = path.iterator();
-				Point2D_F32 last = iter.next();
-				while (iter.hasNext()) {
-					Point2D_F32 current = iter.next();
-					double dx = current.x - last.x, dy = current.y - last.y;
-					int idx = ((int) Math.floor(4 * Math.atan2(dy, dx)
-							/ Math.PI) + 4) % 8;
-					double len = Math.sqrt(dx * dx + dy * dy);
-					hists[getidx(subdiv, last.x, last.y)][idx] += len;
-					last = current;
-				}
-			}
-		}
-
-		for (int i = 0; i < sums.length; ++i) {
-			float[] hist = hists[i];
-			double sum = 0;
-			for (int j = 0; j < hist.length; ++j) {
-				sum += hist[j];
-			}
-			if (sum > 0) {
-				for (int j = 0; j < hist.length; ++j) {
-					hist[j] /= sum;
-				}
-				hists[i] = hist;
-			}
-			sums[i] = sum;
-		}
-
-		ArrayList<Double> sumList = new ArrayList<Double>(sums.length);
-		for (double d : sums) {
-			sumList.add(d);
-		}
-
-		ArrayList<ArrayList<Float>> histList = new ArrayList<ArrayList<Float>>(
-				hists.length);
-		for (float[] hist : hists) {
-			ArrayList<Float> h = new ArrayList<Float>(8);
-			for (float f : hist) {
-				h.add(f);
-			}
-			histList.add(h);
-		}
-
-		return new Pair<List<Double>, ArrayList<ArrayList<Float>>>(sumList,
-				histList);
-	}
-	
-	private static int getidx(int subdiv, float x, float y) {
-		int ix = (int) Math.floor(subdiv * x), iy = (int) Math.floor(subdiv * y);
-		ix = Math.max(Math.min(ix, subdiv - 1), 0);
-		iy = Math.max(Math.min(iy, subdiv - 1), 0);
-
-		return ix * subdiv + iy;
-	}
-	
-	private static void visualize(List<Frame> frames,
-									ArrayList<Pair<Integer, LinkedList<Point2D_F32>>> foregroundPaths,
-									ArrayList<Pair<Integer, LinkedList<Point2D_F32>>> backgroundPaths,
-									LinkedList<GrayU8> masks){
-		BufferedImage bufferedImage = null;
-		BufferedImage track = null;
-		BufferedImage mask = null;
-		
-		track = frames.get(0).getImage().getBufferedImage();
-		int width = track.getWidth();
-		int height = track.getHeight();
-		int imageType = track.getType();
-		bufferedImage = new BufferedImage(width*2,height, imageType);
-		gui.setPreferredSize(new Dimension(width*2,height));
-		ShowImages.showWindow(gui,"visualize", true);
-		
-		ListIterator<Pair<Integer, LinkedList<Point2D_F32>>> fgPathItor = foregroundPaths.listIterator();
-		ListIterator<Pair<Integer, LinkedList<Point2D_F32>>> bgPathItor = backgroundPaths.listIterator();
-		
-		ListIterator<GrayU8> maskIter = masks.listIterator();
-		
-		int cnt = 0;
-		for (int frameIdx = 0; frameIdx < frames.size(); ++frameIdx){
-			if(cnt >= PathList.frameInterval){
-				cnt = 0;
-				continue;
-			}
-			cnt += 1;
-			
-			Frame frame = frames.get(frameIdx);
-			track = frame.getImage().getBufferedImage();
-			Graphics2D g2 = bufferedImage.createGraphics();
-			g2.drawImage(track,null,0,0);
-			
-			while(fgPathItor.hasNext()){
-				Pair<Integer, LinkedList<Point2D_F32>> pair = fgPathItor.next();
-				if(pair.first > frameIdx)
-					break;
-				Point2D_F32 p1 = pair.second.getFirst();
-				Point2D_F32 p2 = pair.second.getLast();
-				VisualizeFeatures.drawPoint(g2, (int)(p1.x*width), (int)(p1.y*height), 2, Color.red);
-				g2.drawLine((int)(p1.x*width), (int)(p1.y*height), (int)(p2.x*width), (int)(p2.y*height));
-			}
-			while(bgPathItor.hasNext()){
-				Pair<Integer, LinkedList<Point2D_F32>> pair = bgPathItor.next();
-				if(pair.first > frameIdx)
-					break;
-				Point2D_F32 p1 = pair.second.getFirst();
-				Point2D_F32 p2 = pair.second.getLast();
-				VisualizeFeatures.drawPoint(g2, (int)(p1.x*width), (int)(p1.y*height), 2, Color.green);
-				g2.drawLine((int)(p1.x*width), (int)(p1.y*height), (int)(p2.x*width), (int)(p2.y*height));
-			}
-			
-			if(maskIter.hasNext()){
-				mask = ConvertBufferedImage.convertTo(maskIter.next(),null);
-				g2.drawImage(mask,null,width,0);
-			}
-			
-			gui.setBufferedImage(bufferedImage);
-			gui.repaint();
-			
-			try{
-			    Thread thread = Thread.currentThread();
-			    thread.sleep(100);
-			}catch (InterruptedException e) {
-			    e.printStackTrace();
-			}
-		}
 	}
 	
 	private static Shot decodeVideo(VideoDecoder videoDecoder){
---
+++ src/ch/unibas/cs/dbis/cineast/core/util/MaskGenerator.java
@@ -0,0 +1,197 @@
+package ch.unibas.cs.dbis.cineast.core.util;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+import boofcv.abst.distort.FDistort;
+import boofcv.alg.filter.binary.ThresholdImageOps;
+import boofcv.alg.filter.blur.GBlurImageOps;
+import boofcv.alg.misc.PixelMath;
+import boofcv.factory.filter.kernel.FactoryKernelGaussian;
+import boofcv.gui.image.ShowImages;
+import boofcv.io.image.ConvertBufferedImage;
+import boofcv.struct.convolve.Kernel1D_I32;
+import boofcv.struct.image.GrayU8;
+import ch.unibas.cs.dbis.cineast.core.data.Frame;
+import ch.unibas.cs.dbis.cineast.core.data.Pair;
+import ch.unibas.cs.dbis.cineast.core.descriptor.PathList;
+import georegression.struct.point.Point2D_F32;
+
+public class MaskGenerator {
+
+	private MaskGenerator(){}
+	
+	public static ArrayList<GrayU8> getFgMasks(List<Frame> frames,
+			List<Pair<Integer, LinkedList<Point2D_F32>>> foregroundPaths) {
+		
+		if (frames == null || frames.isEmpty() || foregroundPaths == null) {
+			return null;
+		}
+
+		ArrayList<GrayU8> masksScaled = generateScaledMasksFromPath(frames, foregroundPaths);
+
+		ArrayList<GrayU8> masksScaledSmoothed1 = createNewMasks(masksScaled);
+		smoothMasks(masksScaled, masksScaledSmoothed1, 3, 3, 64, 16);
+		
+		ArrayList<GrayU8> masks = scaleUpMasks(masksScaledSmoothed1, frames.get(0).getImage().getBufferedImage().getWidth(), frames.get(0).getImage().getBufferedImage().getHeight());
+		
+		ArrayList<GrayU8> masksSmoothed1 = createNewMasks(masks);
+		smoothMasks(masks, masksSmoothed1, 2, 2, 64, 8);
+		
+		multiply3D(masksSmoothed1,masksSmoothed1,255);
+		
+		return masksSmoothed1;
+	}
+	
+	public static ArrayList<GrayU8> generateScaledMasksFromPath(List<Frame> frames,
+			List<Pair<Integer, LinkedList<Point2D_F32>>> foregroundPaths) {
+		
+		if (frames == null || frames.isEmpty() || foregroundPaths == null) {
+			return null;
+		}
+
+		ArrayList<GrayU8> masks = new ArrayList<GrayU8>();
+
+		int width = frames.get(0).getImage().getBufferedImage().getWidth();
+		int height = frames.get(0).getImage().getBufferedImage().getHeight();
+
+		ListIterator<Pair<Integer, LinkedList<Point2D_F32>>> fgPathItor = foregroundPaths.listIterator();
+
+		int cnt = 0;
+		for (int frameIdx = 0; frameIdx < frames.size(); ++frameIdx) {
+			if (cnt >= PathList.frameInterval) {
+				cnt = 0;
+				continue;
+			}
+			cnt += 1;
+
+			GrayU8 mask = new GrayU8(width / PathList.samplingInterval, height / PathList.samplingInterval);
+			
+			while (fgPathItor.hasNext()) {
+				Pair<Integer, LinkedList<Point2D_F32>> pair = fgPathItor.next();
+				if (pair.first > frameIdx)
+					break;
+				Point2D_F32 p1 = pair.second.getFirst();
+				int x = (int) (p1.x * width / PathList.samplingInterval);
+				int y = (int) (p1.y * height / PathList.samplingInterval);
+				if (mask.isInBounds(x, y)) {
+					mask.set(x, y, 1);
+				}
+			}
+			//showBineryImage(mask);
+			masks.add(mask);
+		}
+		
+		return masks;
+	}
+	
+	public static ArrayList<GrayU8> smoothMasks(ArrayList<GrayU8> input, ArrayList<GrayU8> output, int spatialRadius, int temporalRadius, double multipyFactor, int threshold){
+		
+		if(input == null || input.isEmpty()){
+			return input;
+		}
+		if(output == null){
+			output = createNewMasks(input);
+		}
+		if(output.size() != input.size()){
+			throw new IllegalArgumentException("size of input and output do not match. input: "+input.size()+" output: "+output.size());
+		}
+		
+		multiply3D(input, input, multipyFactor);
+		gaussianFilter3D(input, output, spatialRadius, temporalRadius);
+		threshold3D(output,output,threshold);
+		
+		return output;
+	}
+	
+	public static ArrayList<GrayU8> gaussianFilter3D(ArrayList<GrayU8> input, ArrayList<GrayU8> output, int spatialRadius, int temporalRadius) {
+		
+		ArrayList<GrayU8> spatialResult = createNewMasks(input);
+		gaussianFilterSpatial(input, spatialResult, spatialRadius);
+		gaussianFilterTemporal(spatialResult, output, temporalRadius);
+		
+		return output;
+	}
+	
+	public static ArrayList<GrayU8> gaussianFilterSpatial(ArrayList<GrayU8> input, ArrayList<GrayU8> output, int spatialRadius){
+		
+		for (int i = 0; i < input.size(); ++i){
+			GBlurImageOps.gaussian(input.get(i), output.get(i), -1, spatialRadius, null);
+		}
+		
+		return output;
+	}
+	
+	public static ArrayList<GrayU8> gaussianFilterTemporal(ArrayList<GrayU8> input, ArrayList<GrayU8> output, int spatialRadius){
+		int width = input.get(0).getWidth();
+		int height = input.get(0).getHeight();
+		int len = input.size();
+		
+		Kernel1D_I32 kernel = FactoryKernelGaussian.gaussian(Kernel1D_I32.class,-1,spatialRadius);
+		int divisor = kernel.computeSum();
+		int data1D[] = new int[len + 2*kernel.offset];
+		for (int x = 0; x < width; ++x){
+			for (int y = 0; y < height; ++y){
+				for(int i = 0; i < len; ++i){
+					data1D[i + kernel.offset] = input.get(i).get(x, y);
+				}
+				for(int i = 0; i < len; ++i){
+					int total = 0;
+					for (int k = 0; k < kernel.width; ++k){
+						total += (data1D[i+k] & 0xFF) * kernel.data[k];
+					}
+					output.get(i).set(x, y, total/divisor);
+				}
+			} 
+		}
+		
+		return output;
+	}
+	
+	public static ArrayList<GrayU8> multiply3D(ArrayList<GrayU8> input, ArrayList<GrayU8> output, double value){
+		
+		for (int i = 0; i < input.size(); ++i){
+			PixelMath.multiply(input.get(i), value, output.get(i));
+		}
+		
+		return output;
+	}
+	
+	public static ArrayList<GrayU8> threshold3D(ArrayList<GrayU8> input, ArrayList<GrayU8> output, int threshold){
+		
+		for (int i = 0; i < input.size(); ++i){
+			ThresholdImageOps.threshold(input.get(i), output.get(i), threshold, false);
+		}
+		
+		return output;
+	}
+	
+	public static ArrayList<GrayU8> createNewMasks(ArrayList<GrayU8> input){
+		ArrayList<GrayU8> output = new ArrayList<GrayU8>();
+		for (int i = 0; i < input.size(); ++i){
+			output.add(input.get(i).createSameShape());
+		}
+		return output;
+	}
+	
+	public static ArrayList<GrayU8> scaleUpMasks(ArrayList<GrayU8> input, int width, int height){
+		ArrayList<GrayU8> output = new ArrayList<GrayU8>();
+		for (int i = 0; i < input.size(); ++i){
+			GrayU8 in = input.get(i);
+			GrayU8 out = new GrayU8(width, height);
+			new FDistort(in, out).scaleExt().apply();
+			output.add(out);
+		}
+		return output;
+	}
+	
+	public static void showBineryImage(GrayU8 image){
+		PixelMath.multiply(image,255,image);
+		BufferedImage out = ConvertBufferedImage.convertTo(image,null);
+		ShowImages.showWindow(out,"Output");
+	}
+	
+}
