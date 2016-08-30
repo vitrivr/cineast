2f1c036d4d18649609d73124e38b4359aa8a631b
silvanheller <silvan.heller@unibas.ch>
2016-08-30 22:13:03 +0200

    Beautifying code Removed check from entity-creation

--- src/org/vitrivr/cineast/core/features/neuralnet/NeuralNetFeature.java
+++ src/org/vitrivr/cineast/core/features/neuralnet/NeuralNetFeature.java
@@ -39,7 +39,9 @@ public class NeuralNetFeature extends AbstractFeatureModule {
     private static final String classTableName = "features_neuralnet_classlabels";
     private float cutoff = 0.2f;
 
-    //TODO What is maxDist here?
+    public NeuralNetFeature(NeuralNetFactory factory) {
+        this(1f, factory);
+    }
 
     /**
      * Careful: This constructor does not initalize the neural net
@@ -48,7 +50,7 @@ public class NeuralNetFeature extends AbstractFeatureModule {
         super(fullVectorTableName, maxDist);
     }
 
-    protected NeuralNetFeature(float maxDist, NeuralNetFactory factory){
+    protected NeuralNetFeature(float maxDist, NeuralNetFactory factory) {
         this(maxDist, factory.get());
     }
 
@@ -57,7 +59,7 @@ public class NeuralNetFeature extends AbstractFeatureModule {
         this.net = net;
     }
 
-    public void setCutoff(float cutoff){
+    public void setCutoff(float cutoff) {
         this.cutoff = cutoff;
     }
 
@@ -66,7 +68,7 @@ public class NeuralNetFeature extends AbstractFeatureModule {
     }
 
     @Override
-    public void init(DBSelectorSupplier selectorSupplier){
+    public void init(DBSelectorSupplier selectorSupplier) {
         super.init(selectorSupplier);
         this.classificationSelector = selectorSupplier.get();
         this.classSelector = selectorSupplier.get();
@@ -78,30 +80,27 @@ public class NeuralNetFeature extends AbstractFeatureModule {
 
     /**
      * TODO This method needs heavy refactoring because creating entities this way is not really pretty, we're relying on the AdamGRPC
-     *
+     * <p>
      * Create tables that aren't created by super.
-     *
+     * <p>
      * Currently Objectid is a string. That is because we get a unique id which has the shape n+....
      * <p>
      * Schema:
      * Table 0: shotid | classification - vector - super
      * Table 1: shotid | objectid | confidence (ex. 4014 | n203843 | 0.4) - generated labels
-     *
+     * <p>
      * Table 2 is only touched for API-Calls about available labels and at init-time - not during extraction
      * Table 2: objectid | label or concept
      */
     @Override
     public void initalizePersistentLayer(Supplier<EntityCreator> supply) {
+        super.initalizePersistentLayer(supply);
         EntityCreator ec = supply.get();
         //TODO Set pk / Create idx -> Logic in the ecCreator
         AdamGrpc.AttributeDefinitionMessage.Builder attrBuilder = AdamGrpc.AttributeDefinitionMessage.newBuilder();
-        if(!ec.existsEntity(generatedLabelsTableName)){
-            //TODO Shotid is a string here is that correct?
-            ec.createIdEntity(generatedLabelsTableName, new EntityCreator.AttributeDefinition("shotid", AdamGrpc.AttributeType.STRING), new EntityCreator.AttributeDefinition("objectid", AdamGrpc.AttributeType.STRING), new EntityCreator.AttributeDefinition("probability", AdamGrpc.AttributeType.DOUBLE));
-        }
-        if(!ec.existsEntity(classTableName)){
-            ec.createIdEntity(classTableName, new EntityCreator.AttributeDefinition("objectid", AdamGrpc.AttributeType.STRING), new EntityCreator.AttributeDefinition("label", AdamGrpc.AttributeType.STRING));
-        }
+        //TODO Shotid is a string here is that correct?
+        ec.createIdEntity(generatedLabelsTableName, new EntityCreator.AttributeDefinition("shotid", AdamGrpc.AttributeType.STRING), new EntityCreator.AttributeDefinition("objectid", AdamGrpc.AttributeType.STRING), new EntityCreator.AttributeDefinition("probability", AdamGrpc.AttributeType.DOUBLE));
+        ec.createIdEntity(classTableName, new EntityCreator.AttributeDefinition("objectid", AdamGrpc.AttributeType.STRING), new EntityCreator.AttributeDefinition("label", AdamGrpc.AttributeType.STRING));
         ec.close();
     }
 
@@ -111,26 +110,28 @@ public class NeuralNetFeature extends AbstractFeatureModule {
         super.init(phandlerSupply);
         classificationWriter = phandlerSupply.get();
         classificationWriter.open(generatedLabelsTableName);
+        classificationWriter.setFieldNames("shotid","objectid","probability");
         classWriter = phandlerSupply.get();
         classWriter.open(classTableName);
+        classWriter.setFieldNames("objectid","label");
     }
 
     @Override
-    public void finish(){
+    public void finish() {
         super.finish();
-        if(this.classificationWriter!= null){
+        if (this.classificationWriter != null) {
             this.classificationWriter.close();
             this.classificationWriter = null;
         }
-        if(this.classWriter!=null){
+        if (this.classWriter != null) {
             this.classWriter.close();
             this.classWriter = null;
         }
-        if(this.classificationSelector!=null){
+        if (this.classificationSelector != null) {
             this.classificationSelector.close();
             this.classificationSelector = null;
         }
-        if(this.classSelector!=null){
+        if (this.classSelector != null) {
             this.classSelector.close();
             this.classSelector = null;
         }
@@ -143,18 +144,18 @@ public class NeuralNetFeature extends AbstractFeatureModule {
         ConceptReader cr = new ConceptReader(conceptsPath);
 
         //Fill Concept map
-        for(Map.Entry<String, String[]> entry : cr.getConceptMap().entrySet()) {
+        for (Map.Entry<String, String[]> entry : cr.getConceptMap().entrySet()) {
             //values are n... -values being labeled as entry.getKey()
-            for(String label : entry.getValue()){
+            for (String label : entry.getValue()) {
                 PersistentTuple tuple = classWriter.generateTuple(label, entry.getKey());
                 classWriter.persist(tuple);
             }
         }
 
         //Fill class names
-        for(int i = 0; i<net.getSynSetLabels().length;i++){
+        for (int i = 0; i < net.getSynSetLabels().length; i++) {
             String[] labels = net.getLabels(net.getSynSetLabels()[i]);
-            for(String label: labels){
+            for (String label : labels) {
                 PersistentTuple tuple = classWriter.generateTuple(net.getSynSetLabels()[i], label);
                 classWriter.persist(tuple);
             }
@@ -164,12 +165,13 @@ public class NeuralNetFeature extends AbstractFeatureModule {
     /**
      * Set neuralNet to specified net if you have called the default constructor
      */
-    public void setNeuralNet(NeuralNet net){
+    public void setNeuralNet(NeuralNet net) {
         this.net = net;
     }
 
     /**
      * Classifies an Image with the given neural net
+     *
      * @return A float array containing the probabilities given by the neural net.
      */
     private float[] classifyImage(BufferedImage img) {
@@ -200,7 +202,6 @@ public class NeuralNetFeature extends AbstractFeatureModule {
     }
 
     /**
-     *
      * TODO How do we calculate score?
      * Checks if labels have been specified. If no labels have been specified, takes the queryimage.
      * Might perform knn on the 1k-vector in the future.
@@ -212,37 +213,37 @@ public class NeuralNetFeature extends AbstractFeatureModule {
         TimeHelper.tic();
         List<StringDoublePair> _return = new ArrayList();
 
-        if(!sc.getTags().isEmpty()){
+        if (!sc.getTags().isEmpty()) {
             List<String> wnLabels = new ArrayList();
-            for(String label: sc.getTags()){
-                LOGGER.debug("Looking for tag: "+label);
+            for (String label : sc.getTags()) {
+                LOGGER.debug("Looking for tag: " + label);
                 wnLabels = new ArrayList();
-                for(Map<String, PrimitiveTypeProvider> row : classSelector.getRows("label", label)){
+                for (Map<String, PrimitiveTypeProvider> row : classSelector.getRows("label", label)) {
                     //TODO is this the proper way to get info from the row?
                     wnLabels.add(row.get("label").getString());
                 }
             }
 
             //TODO Eliminate Duplicates from wnLabels
-            for(String wnLabel : wnLabels){
-                for(Map<String, PrimitiveTypeProvider> row : classificationSelector.getRows("objectid", wnLabel)){
-                    LOGGER.debug("Found hit for query: "+row.get("shotid").getString(), row.get("probability").getDouble(), row.get("objectid").toString());
-                    _return.add(new StringDoublePair(row.get("shotid").getString(),row.get("probability").getDouble()));
+            for (String wnLabel : wnLabels) {
+                for (Map<String, PrimitiveTypeProvider> row : classificationSelector.getRows("objectid", wnLabel)) {
+                    LOGGER.debug("Found hit for query: " + row.get("shotid").getString(), row.get("probability").getDouble(), row.get("objectid").toString());
+                    _return.add(new StringDoublePair(row.get("shotid").getString(), row.get("probability").getDouble()));
                 }
             }
-        }else{
+        } else {
             //TODO Can we just take the most representative frame from the sc? Is that the query image?
             float[] res = classifyImage(sc.getMostRepresentativeFrame().getImage().getBufferedImage());
 
-            for(int i = 0; i<res.length; i++){
+            for (int i = 0; i < res.length; i++) {
                 //TODO This cutoff should be in queryConfig probably
-                if(res[i]>cutoff){
+                if (res[i] > cutoff) {
                     //Matching! Wub wub
 
-                    for(Map<String, PrimitiveTypeProvider> row : classificationSelector.getRows("objectid", net.getSynSetLabels()[i])){
+                    for (Map<String, PrimitiveTypeProvider> row : classificationSelector.getRows("objectid", net.getSynSetLabels()[i])) {
                         //TODO Duplicates?
-                        LOGGER.debug("Found hit for query: "+row.get("shotid").getString(), row.get("probability").getDouble(), row.get("objectid").toString());
-                        _return.add(new StringDoublePair(row.get("shotid").getString(),row.get("probability").getDouble()));
+                        LOGGER.debug("Found hit for query: " + row.get("shotid").getString(), row.get("probability").getDouble(), row.get("objectid").toString());
+                        _return.add(new StringDoublePair(row.get("shotid").getString(), row.get("probability").getDouble()));
                     }
                 }
             }
