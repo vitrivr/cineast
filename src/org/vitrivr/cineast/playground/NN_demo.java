cee69e0084cb6e0c28b3bf84cd27fa465abeb8fb
silvanheller <silvan.heller@unibas.ch>
2016-09-02 16:25:03 +0200

    Updated NN-Demo with cropping

--- src/org/vitrivr/cineast/playground/NN_demo.java
+++ src/org/vitrivr/cineast/playground/NN_demo.java
@@ -1,11 +1,16 @@
 package org.vitrivr.cineast.playground;
 
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.geometry.Position;
+import net.coobird.thumbnailator.geometry.Positions;
 import org.vitrivr.cineast.core.config.Config;
 import org.vitrivr.cineast.core.features.neuralnet.classification.NeuralNet;
 
 import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -18,7 +23,21 @@ public class NN_demo {
     public static void main(String[] args) throws IOException {
 
         NeuralNet nn = Config.getNeuralNetConfig().getNeuralNetFactory().get();
-        float[] probs = nn.classify(ImageIO.read(new File("src/resources/cat.jpg")));
+        BufferedImage keyframe = ImageIO.read(new File("thumbnails/v1/v1_38.jpg"));
+        float[] probs = new float[1000];
+        Arrays.fill(probs, 0f);
+        Position[] positions = new Position[]{Positions.CENTER, Positions.TOP_RIGHT, Positions.BOTTOM_RIGHT, Positions.BOTTOM_LEFT, Positions.TOP_LEFT};
+
+        int posidx = 0;
+        for(Position pos: positions){
+            System.out.println(posidx);
+            keyframe = Thumbnails.of(keyframe).size(224, 224).crop(pos).asBufferedImage();
+            ImageIO.write(keyframe, "png", new File("src/resources/crop_"+posidx+++".png"));
+            float[] curr = nn.classify(Thumbnails.of(keyframe).size(224, 224).crop(pos).asBufferedImage());
+            probs = maxpool(curr, probs);
+        }
+
+        //float[] probs = nn.classify(ImageIO.read(new File("thumbnails/v1/v1_20.jpg")));
         List<List<String>> labels = nn.getAllLabels();
         for (int i = 0; i < probs.length; i++) {
             if (probs[i] > 0.05) {
@@ -26,6 +45,24 @@ public class NN_demo {
                 System.out.println("Probability for " + String.join(", ", labels.get(i)) + "=" + probs[i]);
             }
         }
+    }
+
+    /**
+     * Takes the maximum value at each position
+     */
+    private static float[] maxpool(float[] curr, float[] probs) {
+        if(curr.length!=probs.length){
+            throw new IllegalArgumentException("Float[] need to have the same size");
+        }
+        float[] _ret = new float[curr.length];
+        for(int i = 0; i<curr.length;i++){
+            if(curr[i]>probs[i]){
+                _ret[i]= curr[i];
+            } else{
+                _ret[i]=probs[i];
+            }
+        }
+        return _ret;
     }
 }
 
