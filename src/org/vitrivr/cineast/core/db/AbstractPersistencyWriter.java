81eb98e6b25659eb83c78b3033346694c2bc9121
Luca Rossetto <luca.rossetto@unibas.ch>
2016-09-22 11:27:06 +0200

    added JsonFileWriter

---
+++ src/org/vitrivr/cineast/core/db/AbstractPersistencyWriter.java
@@ -0,0 +1,21 @@
+package org.vitrivr.cineast.core.db;
+
+public abstract class AbstractPersistencyWriter<R> implements PersistencyWriter<R> {
+
+  protected String[] names; 
+
+  protected AbstractPersistencyWriter(String...names){
+    this.names = names;
+  }
+  
+  protected AbstractPersistencyWriter(){
+    this("id", "feature");
+  }
+  
+  public void setFieldNames(String...names){
+    if(names != null && names.length > 0){
+      this.names = names;
+    }
+  }
+  
+}
