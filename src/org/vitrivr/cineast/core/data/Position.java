4f28abeb67a39587b150fa1ada2eed90b232809d
Luca Rossetto <luca.rossetto@unibas.ch>
2016-12-12 14:25:55 +0100

    some cleanup

--- src/org/vitrivr/cineast/core/data/Position.java
+++ src/org/vitrivr/cineast/core/data/Position.java
@@ -46,11 +46,7 @@ public class Position implements Serializable {
 
   @Override
   public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + x;
-    result = prime * result + y;
-    return result;
+   return (x >>> 16) | (x << 16) | y;
   }
 
   @Override
--- src/org/vitrivr/cineast/explorative/HCTFloatVectorValue.java
+++ src/org/vitrivr/cineast/explorative/HCTFloatVectorValue.java
@@ -2,54 +2,55 @@ package org.vitrivr.cineast.explorative;
 
 import java.io.Serializable;
 
-
 public class HCTFloatVectorValue
-        implements Comparable<HCTFloatVectorValue>,
-        Serializable, Printable{
+    implements Comparable<HCTFloatVectorValue>, Serializable, Printable {
 
-    private final float[] vector;
-    private final String id;
+  private static final long serialVersionUID = 5908031484648901716L;
+  private final float[] vector;
+  private final String id;
 
-    public HCTFloatVectorValue(float[] vector, String id) {
-        this.vector = vector;
-        this.id = id;
+  public HCTFloatVectorValue(float[] vector, String id) {
+    this.vector = vector;
+    this.id = id;
+  }
+
+  public float[] getVector() {
+    return vector;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  @Override
+  public int compareTo(HCTFloatVectorValue o) {
+    if (id.hashCode() > o.getId().hashCode()) {
+      return 1;
     }
-
-    public float[] getVector() {
-        return vector;
+    if (id.hashCode() < o.getId().hashCode()) {
+      return -1;
     }
+    return 0;
+  }
 
-    public String getId() {
-        return id;
-    }
+  @Override
+  public String printHtml() {
+    return "<img class=\"thumb\" src=\"/Applications/XAMPP/xamppfiles/htdocs/vitrivr-ui/thumbnails/"
+        + id + ".jpg\" />";
+  }
 
-    @Override
-    public int compareTo(HCTFloatVectorValue o) {
-        if(id.hashCode() > o.getId().hashCode() ){
-            return 1;
-        }
-        if(id.hashCode() < o.getId().hashCode()){
-            return -1;
-        }
-        return 0;
-    }
+  @Override
+  public String print() {
+    return id;
+  }
 
-    @Override
-    public String printHtml() {
-        return "<img class=\"thumb\" src=\"/Applications/XAMPP/xamppfiles/htdocs/vitrivr-ui/thumbnails/" + id + ".jpg\" />";
+  public String toString() {
+    StringBuilder stringBuilder = new StringBuilder();
+    for (float v : vector) {
+      stringBuilder.append(v + ", ");
     }
-
-    @Override
-    public String print() {
-        return id;
-    }
-
-    public String toString(){
-        StringBuilder stringBuilder = new StringBuilder();
-        for(float v : vector){
-            stringBuilder.append(v + ", ");
-        }
-        if(stringBuilder.length() >= 2) stringBuilder.delete(stringBuilder.length() -2, stringBuilder.length());
-        return stringBuilder.toString();
-    }
+    if (stringBuilder.length() >= 2)
+      stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
+    return stringBuilder.toString();
+  }
 }
