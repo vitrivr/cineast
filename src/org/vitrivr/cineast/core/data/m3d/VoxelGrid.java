403fe41d1b195381ed1ca57265a7fb14dcc2bbbe
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-01-07 23:36:50 +0100

    - Added a VoxelGrid model and a Voxelizer class. Latter voxelizes Meshes.

---
+++ src/org/vitrivr/cineast/core/data/m3d/VoxelGrid.java
@@ -0,0 +1,323 @@
+package org.vitrivr.cineast.core.data.m3d;
+
+import com.jogamp.opengl.GL2;
+import org.joml.Vector3f;
+
+import static com.jogamp.opengl.GL2.GL_COMPILE;
+import static com.jogamp.opengl.GL2ES3.GL_QUADS;
+
+/**
+ * @author rgasser
+ * @version 1.0
+ * @created 06.01.17
+ */
+public class VoxelGrid implements Renderable {
+    /**
+     * A single Voxel.
+     */
+    public class Voxel {
+        private final short x;
+        private final short y;
+        private final short z;
+        private boolean visible;
+
+        public short getX() {
+            return x;
+        }
+        public short getY() {
+            return y;
+        }
+        public short getZ() {
+            return z;
+        }
+        public Vector3f getCenter() {
+            return new Vector3f((x-VoxelGrid.this.sizeX/2) * VoxelGrid.this.resolution, (y-VoxelGrid.this.sizeY/2)*VoxelGrid.this.resolution, (z-VoxelGrid.this.sizeZ/2)*VoxelGrid.this.resolution);
+        }
+        public boolean getVisible() {
+            return visible;
+        }
+
+        public Voxel(short x, short y, short z, boolean visible) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.visible = visible;
+        }
+    }
+
+    /**
+     * Determines the size of a single voxel.
+     */
+    private float resolution;
+
+    /**
+     * Determines the half size of a single voxel.
+     */
+    private float halfResolution;
+
+    /**
+     * The size of the voxel grid in X direction.
+     */
+    private final short sizeX;
+
+    /**
+     * The size of the voxel grid in X direction.
+     */
+    private final short sizeY;
+
+    /**
+     * The size of the voxel grid in X direction.
+     */
+    private final short sizeZ;
+
+    /**
+     * The total lenght ot the voxel grid (i.e. the number of voxels in the grid).
+     */
+    private final int length;
+
+    /**
+     * Array holding the actual voxels.
+     */
+    private final Voxel[][][] voxelGrid;
+
+    /**
+     * Defines the center of the voxel (in the world coordinate system)
+     */
+    private final Vector3f center = new Vector3f(0f,0f,0f);
+
+    /**
+     * Handle of the vertex-list.
+     */
+    private int vertexList = -1;
+
+    /**
+     * Default constructor: Initializes a new, fully active Voxel-Grid
+     *
+     * @param sizeX X-size of the new voxel grid.
+     * @param sizeY Y-size of the new voxel grid.
+     * @param sizeZ Z-size of the new voxel grid.
+     * @param resolution Resolution of the grid, i.e. size of a single voxel.
+     */
+    public VoxelGrid(short sizeX, short sizeY, short sizeZ, float resolution) {
+        this(sizeX, sizeY, sizeZ, resolution, true);
+    }
+
+    /**
+     * Default constructor: Initializes a new Voxel-Grid.
+     *
+     * @param sizeX X-size of the new voxel grid.
+     * @param sizeY Y-size of the new voxel grid.
+     * @param sizeZ Z-size of the new voxel grid.
+     * @param resolution Resolution of the grid, i.e. size of a single voxel.
+     * @param active Indicates whether the grid should be initialized with active or inactive voxels.
+     */
+    public VoxelGrid(short sizeX, short sizeY, short sizeZ, float resolution, boolean active) {
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.sizeZ = sizeZ;
+        this.resolution = resolution;
+        this.halfResolution = resolution/2;
+        this.length = sizeX * sizeY * sizeZ;
+        this.voxelGrid = new Voxel[sizeX][sizeY][sizeZ];
+        for (short i = 0;i<this.sizeX;i++) {
+            for (short j = 0;j<this.sizeY;j++) {
+                for (short k = 0;k<this.sizeZ;k++) {
+                    this.voxelGrid[i][j][k] = new Voxel(i,j,k, active);
+                }
+            }
+        }
+    }
+
+    /**
+     * Getter for voxel grid's X-size.
+     *
+     * @return X-size of the voxel grid.
+     */
+    public final short getSizeX() {
+        return sizeX;
+    }
+
+    /**
+     * Getter for voxel grid's Y-size.
+     *
+     * @return Y-size of the voxel grid.
+     */
+    public final short getSizeY() {
+        return sizeY;
+    }
+
+    /**
+     * Getter for voxel grid's Z-size.
+     *
+     * @return Z-size of the voxel grid.
+     */
+    public final short getSizeZ() {
+        return sizeZ;
+    }
+
+    /**
+     * Getter for the length of the voxel-grid i.e. the number of voxels
+     * in the grid.
+     *
+     * @return Number of voxels in the grid.
+     */
+    public final int getLength() {
+        return length;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public final float getResolution() {
+        return resolution;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public final float getHalfResolution() {
+        return halfResolution;
+    }
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @param z
+     * @return
+     */
+    public final Voxel get(int x, int y, int z) {
+        return this.voxelGrid[x][y][z];
+    }
+
+    /**
+     *
+     * @param visible
+     * @param x
+     * @param y
+     * @param z
+     */
+    public final void toggle(boolean visible, int x, int y, int z) {
+        this.voxelGrid[x][y][z].visible = visible;
+    }
+
+
+    /**
+     * Assembles a mesh into a new glDisplayList. The method returns a handle for this
+     * newly created glDisplayList. To actually render the list - by executing the commands it contains -
+     * the glCallList function must be called!
+     *
+     * IMPORTANT: The glDisplayList bust be deleted once its not used anymore by calling glDeleteLists
+
+     * @return Handle for the newly created glDisplayList.
+     */
+    public int assemble(GL2 gl) {
+        if (this.vertexList > 0) return this.vertexList;
+        this.vertexList = gl.glGenLists(1);
+        gl.glNewList(this.vertexList, GL_COMPILE);
+        {
+            boolean[] visible = {true, true, true, true, true, true};
+
+            for (int i = 0; i < this.sizeX; i++) {
+                for (int j = 0; j < this.sizeY; j++) {
+                    for (int k = 0; k < this.sizeZ; k++) {
+                        /* Skip Voxel if its inactive. */
+                        if (!this.voxelGrid[i][j][k].visible) continue;
+
+                        /* Extract center of the voxel. */
+                        float x = this.center.x + this.voxelGrid[i][j][k].getCenter().x;
+                        float y = this.center.y + this.voxelGrid[i][j][k].getCenter().y;
+                        float z = this.center.z + this.voxelGrid[i][j][k].getCenter().z;
+
+                        /* Determine which faces to draw: Faced that are covered by another active voxel are switched off. */
+                        if(i > 0) visible[0] = !this.voxelGrid[i-1][j][k].visible;
+                        if(i < this.getSizeX()-1) visible[1] = !this.voxelGrid[i+1][j][k].visible;
+                        if(j > 0) visible[2] = !this.voxelGrid[i][j-1][k].visible;
+                        if(j < this.getSizeY()-1) visible[3] = !this.voxelGrid[i][j+1][k].visible;
+                        if(k > 0) visible[4] = !this.voxelGrid[i][j][k-1].visible;
+                        if(k < this.getSizeZ()-1) visible[5] = !this.voxelGrid[i][j][k+1].visible;
+
+                        /* Draw the cube. */
+                        this.drawCube(gl, x,y,z,visible);
+                    }
+                }
+            }
+        }
+
+        gl.glEndList();
+        return  this.vertexList;
+    }
+
+    /**
+     *
+     * @param gl
+     */
+    public void clear(GL2 gl) {
+        gl.glDeleteLists(this.vertexList, 1);
+        this.vertexList = -1;
+    }
+
+    /**
+     *
+     * @param gl
+     * @param x
+     * @param y
+     * @param z
+     * @param visible
+     */
+    private final void drawCube(GL2 gl, float x, float y, float z, boolean[] visible) {
+        gl.glBegin(GL_QUADS);
+        {
+            /* 1 */
+            if (visible[0]) {
+                gl.glVertex3f(x + halfResolution, y - halfResolution, z - halfResolution);
+                gl.glVertex3f(x - halfResolution, y - halfResolution, z - halfResolution);
+                gl.glVertex3f(x - halfResolution, y + halfResolution, z - halfResolution);
+                gl.glVertex3f(x + halfResolution, y + halfResolution, z - halfResolution);
+            }
+
+            /* 2 */
+            if (visible[1]) {
+                gl.glVertex3f(x - halfResolution, y - halfResolution, z + halfResolution);
+                gl.glVertex3f(x + halfResolution, y - halfResolution, z + halfResolution);
+                gl.glVertex3f(x + halfResolution, y + halfResolution, z + halfResolution);
+                gl.glVertex3f(x - halfResolution, y + halfResolution, z + halfResolution);
+            }
+
+            /* 3 */
+            if (visible[2]) {
+                gl.glVertex3f(x + halfResolution, y - halfResolution, z + halfResolution);
+                gl.glVertex3f(x + halfResolution, y - halfResolution, z - halfResolution);
+                gl.glVertex3f(x + halfResolution, y + halfResolution, z - halfResolution);
+                gl.glVertex3f(x + halfResolution, y + halfResolution, z + halfResolution);
+            }
+
+            /* 4 */
+            if (visible[3]) {
+                gl.glVertex3f(x - halfResolution, y - halfResolution, z - halfResolution);
+                gl.glVertex3f(x - halfResolution, y - halfResolution, z + halfResolution);
+                gl.glVertex3f(x - halfResolution, y + halfResolution, z + halfResolution);
+                gl.glVertex3f(x - halfResolution, y + halfResolution, z - halfResolution);
+            }
+
+            /* 5 */
+            if (visible[4]) {
+                gl.glVertex3f(x - halfResolution, y - halfResolution, z - halfResolution);
+                gl.glVertex3f(x + halfResolution, y - halfResolution, z - halfResolution);
+                gl.glVertex3f(x + halfResolution, y - halfResolution, z + halfResolution);
+                gl.glVertex3f(x - halfResolution, y - halfResolution, z + halfResolution);
+            }
+
+            /* 6 */
+            if (visible[5]) {
+                gl.glVertex3f(x + halfResolution, y + halfResolution, z - halfResolution);
+                gl.glVertex3f(x - halfResolution, y + halfResolution, z - halfResolution);
+                gl.glVertex3f(x - halfResolution, y + halfResolution, z + halfResolution);
+                gl.glVertex3f(x + halfResolution, y + halfResolution, z + halfResolution);
+            }
+        }
+        gl.glEnd();
+    }
+}
---
+++ src/org/vitrivr/cineast/core/data/m3d/Voxelizer.java
@@ -0,0 +1,273 @@
+package org.vitrivr.cineast.core.data.m3d;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.joml.Vector3f;
+
+import java.util.*;
+
+/**
+ * @author rgasser
+ * @version 1.0
+ * @created 06.01.17
+ */
+public class Voxelizer {
+    /**
+     *
+     */
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    /**
+     * Constant: Square-root of 3.
+     */
+    private static final float sqrt3 =  (float)Math.sqrt(3);
+
+    /**
+     * Resolution, i.e. size of a single voxel.
+     */
+    private final float resolution;
+
+    /**
+     * Half of the resolution. Pre-calculated for convenience.
+     */
+    private final float rc;
+
+    /**
+     * Half the resolution squared. Pre-calculated for convenience.
+     */
+    private final float rcsq;
+
+
+    /**
+     * Default constructor. Defines the resolution of the Voxelizer.
+     *
+     * @param resolution
+     */
+    public Voxelizer(float resolution) {
+        this.resolution = resolution;
+        this.rc = resolution/2;
+        this.rcsq = (float)Math.pow(this.rc,2);
+    }
+
+    /**
+     * Voxelizes the provided mesh, returning a VoxelGrid with the specified resolution.
+     *
+     * @param mesh Mesh that should be voxelized.
+     * @return Voxel representation of the mesh.
+     */
+    public VoxelGrid voxelize(Mesh mesh) {
+        long start =  System.currentTimeMillis();
+
+        /* Calculate bounding box of mesh. */
+        float[] boundingBox = this.bounds(mesh.getVertices());
+        short sizeX = (short)Math.abs((short)Math.ceil(((boundingBox[0]-boundingBox[1])/this.resolution)));
+        short sizeY = (short)Math.abs((short)Math.ceil(((boundingBox[2]-boundingBox[3])/this.resolution)));
+        short sizeZ = (short)Math.abs((short)Math.ceil(((boundingBox[4]-boundingBox[5])/this.resolution)));
+
+        /* Initializes a new voxel-grid. */
+        VoxelGrid grid = new VoxelGrid(sizeX, sizeY, sizeZ, this.resolution, false);
+
+        /* Prepare emtpy HashSets. */
+        for (Mesh.Face face : mesh.getFaces()) {
+            List<Vector3f> vertices = face.getVertices();
+            List<VoxelGrid.Voxel> enclosings = Voxelizer.this.enclosingGrid(vertices, boundingBox, grid);
+            for (VoxelGrid.Voxel enclosing : enclosings) {
+                /* Perform vertex-tests. */
+                if (Voxelizer.this.vertextTest(vertices.get(0), enclosing)) {
+                    grid.toggle(true, enclosing.getX(), enclosing.getY(), enclosing.getZ());
+                    continue;
+                }
+                if (Voxelizer.this.vertextTest(vertices.get(1), enclosing)) {
+                    grid.toggle(true, enclosing.getX(), enclosing.getY(), enclosing.getZ());
+                    continue;
+                }
+                if (Voxelizer.this.vertextTest(vertices.get(2), enclosing)) {
+                    grid.toggle(true, enclosing.getX(), enclosing.getY(), enclosing.getZ());
+                    continue;
+                }
+                 /* Perform edge-tests. */
+                if (Voxelizer.this.edgeTest(vertices.get(0), vertices.get(1), enclosing)) {
+                    grid.toggle(true, enclosing.getX(), enclosing.getY(), enclosing.getZ());
+                    continue;
+                }
+                if (Voxelizer.this.edgeTest(vertices.get(1), vertices.get(2), enclosing)) {
+                    grid.toggle(true, enclosing.getX(), enclosing.getY(), enclosing.getZ());
+                    continue;
+                }
+                if (Voxelizer.this.edgeTest(vertices.get(2), vertices.get(0), enclosing)) {
+                    grid.toggle(true, enclosing.getX(), enclosing.getY(), enclosing.getZ());
+                    continue;
+                }
+
+                /* Perform plane-tests. */
+                if (Voxelizer.this.planeTest(vertices.get(0), vertices.get(1), vertices.get(2), enclosing)) {
+                    grid.toggle(true, enclosing.getX(), enclosing.getY(), enclosing.getZ());
+                }
+            }
+        }
+
+        long stop = System.currentTimeMillis();
+        LOGGER.log(Level.DEBUG, String.format("Voxelization of mesh completed in %d ms (Size: %d x %d x %d).", (stop - start), grid.getSizeX(), grid.getSizeY(), grid.getSizeZ()));
+
+        return grid;
+    }
+
+    /**
+     * Performs the vertex-test described in [1]. Checks if the provided voxel's center is within the area
+     * of circle with radius L/2 around the vertex (L being the size of a voxel).
+     *
+     * @param vertex Vertex to be tested.
+     * @param voxel Voxel to be tested.
+     * @return true if the voxel's center is within the circle, false otherwise.
+     */
+    private boolean vertextTest(Vector3f vertex, VoxelGrid.Voxel voxel) {
+       return vertex.distanceSquared(voxel.getCenter()) > this.rcsq;
+    }
+
+    /**
+     * Performs the edge-test described in [1]. Checks if the provided voxel's center is enclosed in the cylinder
+     * around the line between vertex a and vertex b.
+     *
+     * @param a First vertex that constitutes the line used to draw a cylinder around.
+     * @param b Second vertex that constitutes the line used to draw a cylinder around.
+     * @param voxel Voxel to be tested.
+     * @return true if voxel's center is contained in cylinder, false otherwise.
+     */
+    private boolean edgeTest(Vector3f a, Vector3f b, VoxelGrid.Voxel voxel) {
+        Vector3f line = new Vector3f();
+        b.sub(a, line);
+        Vector3f pd = new Vector3f();
+        voxel.getCenter().sub(a, pd);
+
+        /* Calculate distance between a and b (Edge). */
+        float lsq = a.distanceSquared(b);
+        float dot = line.dot(pd);
+
+        if (dot < 0.0f || dot > lsq) {
+            return false;
+        } else {
+            float dsq = pd.lengthSquared() - ((float)Math.pow(dot,2))/lsq;
+            return dsq > rc;
+        }
+    }
+
+    /**
+     * Performs a simplified version of the plane-test described in [1]. Checks if the provided voxel's center is
+     * enclosed in the space spanned by two planes parallel to the face.
+     *
+     * The original version of the test performs three addition checks with planes that go through the edges. These
+     * tests are ommited because we only work on a reduced set of voxels that directly enclose the vertices in question.
+     *
+     * @param a First vertex that spans the face.
+     * @param b Second vertex that spans the face.
+     * @param c Thrid vertex that spans the face.
+     * @param voxel Voxel to be tested.
+     * @return true if voxel's center is contained in the area, false otherwise.
+
+     */
+    private boolean planeTest(Vector3f a, Vector3f b, Vector3f c, VoxelGrid.Voxel voxel) {
+        /* Retrieve center and corner of voxel. */
+        Vector3f vcenter = voxel.getCenter();
+        Vector3f vcorner = new Vector3f(vcenter.x + this.rc, vcenter.y + this.rc, vcenter.y + this.rc);
+
+        /* Calculate the plane-normal of the polygon. */
+        Vector3f ab = new Vector3f();
+        Vector3f ac = new Vector3f();
+        Vector3f planenorm = new Vector3f();
+        b.sub(a, ab); /* Vectors spanning the plane of the polygon. */
+        c.sub(a, ac);
+        ab.cross(ac, planenorm);
+
+        /* Calculate the distance t for enclosing planes. */
+        float t = this.rc*sqrt3*vcorner.dot(voxel.getCenter());
+
+        /* Derive new, displaced plane-normals. */
+        Vector3f planenorm_plus = new Vector3f(planenorm.x + t, planenorm.y + t, planenorm.z + t);
+        Vector3f planenorm_minus = new Vector3f(planenorm.x - t, planenorm.y - t, planenorm.z - t);
+
+        /* Check if the center is under the planenorm_plus and above the planenorm_minus. */
+        if (planenorm_plus.dot(voxel.getCenter()) < 0 && planenorm_minus.dot(voxel.getCenter()) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Calculates and returns the enclosing grid, i.e. a list of voxels from the
+     * grid that enclose the list of provided vertices.
+     *
+     * @param vertices The vertices for which an enclosing grid needs to be found.
+     * @param boundingBox Bounding-box of the mesh. Used for calculations.
+     * @param grid VoxelGrid to select voxels from.
+     * @return List of voxels that confine the provided vertices.
+     */
+    private List<VoxelGrid.Voxel> enclosingGrid(List<Vector3f> vertices, float[] boundingBox, VoxelGrid grid) {
+        /* Calculate bounding box for provided vertices. */
+        float bounds[] = this.bounds(vertices);
+
+        /* Derive max and min voxel-indices from bounding-boxes. */
+        int max_x = Math.abs((int)Math.ceil(((boundingBox[1]-bounds[0])/this.resolution)));
+        int min_x = Math.abs((int)Math.ceil(((boundingBox[1]-bounds[1])/this.resolution)));
+        int max_y = Math.abs((int)Math.ceil(((boundingBox[3]-bounds[2])/this.resolution)));
+        int min_y = Math.abs((int)Math.ceil(((boundingBox[3]-bounds[3])/this.resolution)));
+        int max_z = Math.abs((int)Math.ceil(((boundingBox[5]-bounds[4])/this.resolution)));
+        int min_z = Math.abs((int)Math.ceil(((boundingBox[5]-bounds[5])/this.resolution)));
+
+        assert min_x >= 0;
+        assert max_x < grid.getSizeX();
+        assert min_x <= max_x;
+
+        assert min_y >= 0;
+        assert max_y < grid.getSizeY();
+        assert min_y <= max_y;
+
+        assert min_z >= 0;
+        assert max_z < grid.getSizeY();
+        assert min_z <= max_z;
+
+        /* Initialize an empty ArrayList for the Voxel-Elements. */
+        List<VoxelGrid.Voxel> enclosing = new ArrayList<>((max_x-min_x) * (max_y-min_y) * (max_z-min_z));
+        for (int i = min_x; i <= max_x; i++) {
+            for (int j = min_y;j<= max_y; j++) {
+                for (int k = min_z; k <= max_z; k++) {
+                    enclosing.add(grid.get(i,j,k));
+                }
+            }
+        }
+
+        /* Return list of enclosing voxels. */
+        return enclosing;
+    }
+
+    /**
+     * Calculates and retruns a bounding box, given a list of vertices. The box encloses all the
+     * vertices.
+     *
+     * @param vertices List of vertices for which the bounding-box should be calculated.
+     * @return Float-array spanning the bounding-box {max_x, min_x, max_y, min_y, max_z, min_z}
+     */
+    private float[] bounds(List<Vector3f> vertices) {
+        /* Initialize the bounding-box. */
+        float bounds[] = {
+          -Float.MAX_VALUE, Float.MAX_VALUE,
+          -Float.MAX_VALUE, Float.MAX_VALUE,
+          -Float.MAX_VALUE, Float.MAX_VALUE
+        };
+
+        /* Find max and min y-values. */
+        for(Vector3f vertex : vertices) {
+            if (vertex.x() > bounds[0]) bounds[0] = vertex.x();
+            if (vertex.x() < bounds[1]) bounds[1] = vertex.x();
+            if (vertex.y() > bounds[2]) bounds[2] = vertex.y();
+            if (vertex.y() < bounds[3]) bounds[3] = vertex.y();
+            if (vertex.z() > bounds[4]) bounds[4] = vertex.z();
+            if (vertex.z() < bounds[5]) bounds[5] = vertex.z();
+        }
+
+        /* Return bounding-box. */
+        return bounds;
+    }
+}
