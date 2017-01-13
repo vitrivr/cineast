2e703cbf794565caef8bd060983a231b6c55eb36
Luca Rossetto <luca.rossetto@unibas.ch>
2017-01-13 16:10:10 +0100

    renamed ShotProvider to SegmentProvider

---
+++ src/org/vitrivr/cineast/core/data/providers/SegmentProvider.java
@@ -0,0 +1,15 @@
+package org.vitrivr.cineast.core.data.providers;
+
+import org.vitrivr.cineast.core.data.SegmentContainer;
+
+public interface SegmentProvider {
+
+  /**
+   * 
+   * @return the next {@link SegmentContainer} from the source or <code>null</code> if there are no more segments.
+   */
+	public SegmentContainer getNextSegment();
+
+	public void close();
+	
+}
--- src/org/vitrivr/cineast/core/segmenter/ShotSegmenter.java
+++ src/org/vitrivr/cineast/core/segmenter/ShotSegmenter.java
@@ -4,7 +4,7 @@ import org.vitrivr.cineast.core.data.Frame;
 import org.vitrivr.cineast.core.data.Histogram;
 import org.vitrivr.cineast.core.data.MediaType;
 import org.vitrivr.cineast.core.data.Shot;
-import org.vitrivr.cineast.core.data.providers.ShotProvider;
+import org.vitrivr.cineast.core.data.providers.SegmentProvider;
 import org.vitrivr.cineast.core.db.PersistencyWriter;
 import org.vitrivr.cineast.core.db.PersistentTuple;
 import org.vitrivr.cineast.core.db.SegmentLookup.SegmentDescriptor;
@@ -17,7 +17,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class ShotSegmenter implements ShotProvider{
+public class ShotSegmenter implements SegmentProvider{
 	
 	private static final double THRESHOLD = 0.05;
 	private static final int PRE_QUEUE_LEN = 10;
@@ -64,7 +64,7 @@ public class ShotSegmenter implements ShotProvider{
 	
 	
 	
-	public Shot getNextShot(){
+	public Shot getNextSegment(){
 		if(this.frameQueue.isEmpty()){
 			queueFrames();
 		}
