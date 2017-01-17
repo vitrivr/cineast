65bef92b7083090d2a0e8f2c0d8a5082ddbb3c27
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-01-17 22:51:32 +0100

    Added two convenience classes that allow to persist MultimediaObjectDescriptors and SegmentDescriptors.

---
+++ src/org/vitrivr/cineast/core/db/dao/MultimediaObjectWriter.java
@@ -0,0 +1,51 @@
+package org.vitrivr.cineast.core.db.dao;
+
+import org.vitrivr.cineast.core.config.Config;
+import org.vitrivr.cineast.core.data.entities.MultimediaObjectDescriptor;
+import org.vitrivr.cineast.core.db.PersistencyWriter;
+import org.vitrivr.cineast.core.db.PersistentTuple;
+
+/**
+ * @author rgasser
+ * @version 1.0
+ * @created 14.01.17
+ */
+public class MultimediaObjectWriter {
+
+    /**
+     * PersistencyWriter instance used to persist changes.
+     */
+    private PersistencyWriter<?> writer = Config.getDatabaseConfig().getWriterSupplier().get();
+
+    /**
+     * Default constructor.
+     */
+    public MultimediaObjectWriter() {
+        this.writer.setFieldNames(MultimediaObjectDescriptor.FIELDNAMES);
+        this.writer.open(MultimediaObjectDescriptor.ENTITY);
+    }
+
+    /**
+     * Persists the provided MultimediaObjectDescriptor in the database.
+     *
+     * @param descriptor MultimediaObjectDescriptor to persist
+     */
+    public void write(MultimediaObjectDescriptor descriptor) {
+        PersistentTuple tuple = this.writer.generateTuple(descriptor.getObjectId(), descriptor.getMediatypeId(), descriptor.getName(), descriptor.getPath());
+        this.writer.persist(tuple);
+    }
+
+    /**
+     * Closes the writer.
+     */
+    public void close() {
+        this.writer.close();
+    }
+
+    /**
+     *
+     */
+    public void finalize() {
+        this.close();
+    }
+}
---
+++ src/org/vitrivr/cineast/core/db/dao/SegmentWriter.java
@@ -0,0 +1,48 @@
+package org.vitrivr.cineast.core.db.dao;
+
+import org.vitrivr.cineast.core.config.Config;
+import org.vitrivr.cineast.core.data.entities.SegmentDescriptor;
+import org.vitrivr.cineast.core.db.PersistencyWriter;
+import org.vitrivr.cineast.core.db.PersistentTuple;
+
+/**
+ * @author rgasser
+ * @version 1.0
+ * @created 14.01.17
+ */
+public class SegmentWriter {
+    /**
+     * PersistencyWriter instance used to persist changes.
+     */
+    private PersistencyWriter<?> writer = Config.getDatabaseConfig().getWriterSupplier().get();
+
+    /**
+     * Default constructor.
+     */
+    public SegmentWriter() {
+        this.writer.setFieldNames(SegmentDescriptor.FIELDNAMES);
+        this.writer.open(SegmentDescriptor.ENTITY);
+    }
+
+    /**
+     * Persists the provided SegmentDescriptor in the database.
+     *
+     * @param descriptor SegmentDescriptor to persist
+     */
+    public void write(SegmentDescriptor descriptor) {
+        PersistentTuple tuple = this.writer.generateTuple(descriptor.getSegmentId(), descriptor.getObjectId(), descriptor.getSequenceNumber(), descriptor.getStart(), descriptor.getEnd());
+        this.writer.persist(tuple);
+    }
+
+    /**
+     * Closes the writer.
+     */
+    public void close() {
+        this.writer.close();
+    }
+
+    public void finalize() {
+        this.close();
+    }
+
+}
