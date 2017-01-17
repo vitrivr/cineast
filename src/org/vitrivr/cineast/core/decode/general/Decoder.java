7d3444f48e6279a77d568db26406e51be2978a74
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-01-17 22:48:09 +0100

    Added the Decoder, Segmenter and ExtractionFileHandler interfaces that build the backbone of the extraction process.

---
+++ src/org/vitrivr/cineast/core/decode/general/Decoder.java
@@ -0,0 +1,77 @@
+package org.vitrivr.cineast.core.decode.general;
+
+import java.nio.file.Path;
+import java.util.Set;
+
+/**
+ * General interface for Decoder classes. These classes take a file as input and return one to many
+ * content items of type T. Depending on what file-type this may be a single image, and set of audio
+ * frames etc.
+ *
+ * Decoders are initialized with the init() method and closed with the close() method. Some decoders
+ * may be reusable, which means that they can be re-initialized by calling init() again. Use the reusable()
+ * method to determine whether or not a Decoder can be re-used.
+ *
+ * @author rgasser
+ * @version 1.0
+ * @created 13.01.17
+ */
+public interface Decoder<T> {
+    /**
+     * Initializes the decoder with a file. This is a necessary step before content can be retrieved from
+     * the decoder by means of the getNext() method.
+     *
+     * @param path Path to the file that should be decoded.
+     * @return Current instance of the decoder.
+     */
+    Decoder<T> init(Path path);
+
+    /**
+     * Closes the DEcoder. This method should cleanup and relinquish all resources.
+     *
+     * Note: It is unsafe to re-use a Decoder after it has been closed.
+     */
+    void close();
+
+    /**
+     * Fetches the next piece of content of type T and returns it. This method can be safely invoked until
+     * complete() returns false. From which on this method will return null.
+     *
+     * @return Content of type T.
+     */
+    T getNext();
+
+    /**
+     * Returns the total number of content pieces T this decoder can return
+     * for a given file.
+     *
+     * @return
+     */
+    int count();
+
+    /**
+     * Indicates whether or not the current decoder has more content to return or not.
+     *
+     * @return True if more content can be fetched, false otherwise.
+     */
+    boolean complete();
+
+    /**
+     * Returns a set of the mime/types of supported files.
+     *
+     * @return Set of the mime-type of file formats that are supported by the current Decoder instance.
+     */
+    Set<String> supportedFiles();
+
+    /**
+     * Indicates whether or not a particular instance of the Decoder interface can be re-used. If this method returns
+     * true, it is save to call init() with a new file after the previous file has been fully read.
+     *
+     * This property can be leveraged to reduce the memory-footprint of the application.
+     *
+     * @return True if re-use is possible, false otherwise.
+     */
+    default boolean canBeReused() {
+        return false;
+    }
+}
---
+++ src/org/vitrivr/cineast/core/segmenter/general/Segmenter.java
@@ -0,0 +1,50 @@
+package org.vitrivr.cineast.core.segmenter.general;
+
+import org.vitrivr.cineast.core.data.SegmentContainer;
+import org.vitrivr.cineast.core.decode.general.Decoder;
+
+/**
+ * Segmenter's split a media-file into chunks (segments). The nature of that chunk is specific to
+ * the media-type and the segmenter's implementation. It could be a frame or shot from a video,
+ * a sequence from a song or a single image.
+ *
+ * @author rgasser
+ * @version 1.0
+ * @created 16.01.17
+ */
+public interface Segmenter<A> extends Runnable {
+    /**
+     * Method used to initialize the Segmenter. A class implementing the Decoder interface
+     * with the same type must be provided.
+     *
+     * @param decoder Decoder used for media-decoding.
+     */
+    void init(Decoder<A> decoder);
+
+    /**
+     * Returns the next SegmentContainer from the source OR null if there are no more segments in the queue. As
+     * generation of SegmentContainers can take some time (depending on the media-type), a null return does not
+     * necessarily mean that the Segmenter is done segmenting. Use the complete() method to check this.
+     *
+     * Important: This method should be designed to block and wait for an appropriate amount of time if the Segmenter
+     * is not yet ready to deliver another segment! It's up to the Segmenter how long that timeout should last.
+     *
+     * @return
+     */
+    SegmentContainer getNext() throws InterruptedException;
+
+    /**
+     * Indicates whether the Segmenter is complete i.e. no new segments are to be expected.
+     *
+     * @return true if work is complete, false otherwise.
+     */
+    boolean complete();
+
+    /**
+     * Closes the Segmenter. This method should cleanup and relinquish all resources. Especially,
+     * calling this method should also close the Decoder associated with this Segmenter instance.
+     *
+     * Note: It is unsafe to re-use a Segmenter after it has been closed.
+     */
+    void close();
+}
