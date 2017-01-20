4ced1feec91007a89385fd0116531ce58f4887f9
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-01-20 17:38:03 +0100

    Minor tweak.

--- src/org/vitrivr/cineast/core/run/filehandler/AbstractExtractionFileHandler.java
+++ src/org/vitrivr/cineast/core/run/filehandler/AbstractExtractionFileHandler.java
@@ -3,6 +3,7 @@ package org.vitrivr.cineast.core.run.filehandler;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.vitrivr.cineast.core.config.Config;
 import org.vitrivr.cineast.core.data.Pair;
 import org.vitrivr.cineast.core.data.SegmentContainer;
 import org.vitrivr.cineast.core.data.entities.MultimediaObjectDescriptor;
@@ -10,6 +11,7 @@ import org.vitrivr.cineast.core.data.entities.SegmentDescriptor;
 import org.vitrivr.cineast.core.db.dao.MultimediaObjectWriter;
 import org.vitrivr.cineast.core.db.dao.SegmentWriter;
 import org.vitrivr.cineast.core.decode.general.Decoder;
+import org.vitrivr.cineast.core.features.extractor.DefaultExtractorInitializer;
 import org.vitrivr.cineast.core.run.ExtractionContextProvider;
 import org.vitrivr.cineast.core.runtime.ExtractionPipeline;
 import org.vitrivr.cineast.core.segmenter.general.Segmenter;
@@ -61,6 +63,9 @@ public abstract class AbstractExtractionFileHandler<T> implements ExtractionFile
     /** ExtractionPipeline that extracts features from the segments. */
     private final ExtractionPipeline pipeline;
 
+    /** Used to measure the duration of an extraction run. */
+    private long start;
+
     /**
      * Default constructor used to initialize the class.
      *
@@ -70,7 +75,7 @@ public abstract class AbstractExtractionFileHandler<T> implements ExtractionFile
     public AbstractExtractionFileHandler(List<Path> files, ExtractionContextProvider context) {
         this.objectWriter = new MultimediaObjectWriter();
         this.segmentWriter = new SegmentWriter();
-        this.pipeline = new ExtractionPipeline(context);
+        this.pipeline = new ExtractionPipeline(context, new DefaultExtractorInitializer());
         this.files = files;
         this.context = context;
     }
@@ -88,6 +93,9 @@ public abstract class AbstractExtractionFileHandler<T> implements ExtractionFile
      */
     @Override
     public void run() {
+        /* Get start-timestamp. */
+        this.start = System.currentTimeMillis();
+
         /* Create new, initial decoder and segmenter. */
         Decoder<T> decoder = this.newDecoder();
         Segmenter<T> segmenter = this.newSegmenter();
@@ -107,7 +115,7 @@ public abstract class AbstractExtractionFileHandler<T> implements ExtractionFile
             LOGGER.info("Processing file under {} (id: {})", file.second.toString(), file.first);
 
             /* Pass file to decoder and decoder to segmenter. */
-            decoder.init(file.second);
+            decoder.init(file.second, Config.sharedConfig().getDecoders().get(this.context.sourceType()));
             segmenter.init(decoder);
 
             /* Initialize a new segment number. */
@@ -157,20 +165,23 @@ public abstract class AbstractExtractionFileHandler<T> implements ExtractionFile
         /* Shutdown the FileHandler. */
         this.shutdown();
     }
+
     /**
      * Stops the ExtractionPipeline and relinquishing all resources.
      */
     private void shutdown() {
-        LOGGER.info("File extraction complete! Shutting down...");
+        LOGGER.info("File decoding and segmenting complete! Shutting down...");
         this.executorService.shutdown();
+        this.pipeline.stop();
         try {
-            this.executorService.awaitTermination(15, TimeUnit.MINUTES);
-            this.pipeline.stop();
+            LOGGER.info("Waiting for ExtractionPipeline to terminate! This could take a while.");
+            this.executorService.awaitTermination(30, TimeUnit.MINUTES);
         } catch (InterruptedException e) {
-            LOGGER.warn("Interrupted while waiting for Executor to shutdown!");
+            LOGGER.warn("Interrupted while waiting for ExtractionPipeline to shutdown!");
         } finally {
             this.segmentWriter.close();
             this.objectWriter.close();
+            LOGGER.info("File extraction complete! It took {} minutes to extract {} files.", TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis()-this.start), this.files.size());
         }
     }
 
