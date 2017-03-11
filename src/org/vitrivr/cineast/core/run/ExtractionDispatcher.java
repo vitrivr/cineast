d008810dde648f4f663e59bd02b74b6e86a794d8
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-03-11 11:42:36 +0100

    Corrected a typo.

--- src/org/vitrivr/cineast/core/run/ExtractionDispatcher.java
+++ src/org/vitrivr/cineast/core/run/ExtractionDispatcher.java
@@ -13,7 +13,7 @@ import org.vitrivr.cineast.core.config.IngestConfig;
 import org.vitrivr.cineast.core.data.MediaType;
 import org.vitrivr.cineast.core.run.filehandler.AudioExtractionFileHandler;
 import org.vitrivr.cineast.core.run.filehandler.ImageExtractionFileHandler;
-import org.vitrivr.cineast.core.run.filehandler.ModelExtractionFileHandler;
+import org.vitrivr.cineast.core.run.filehandler.Model3DExtractionFileHandler;
 import org.vitrivr.cineast.core.run.filehandler.VideoExtractionFileHandler;
 import org.vitrivr.cineast.core.util.LogHelper;
 import org.vitrivr.cineast.core.util.json.JacksonJsonProvider;
@@ -98,7 +98,7 @@ public class ExtractionDispatcher {
                     this.fileHandlerThread = new Thread(new AudioExtractionFileHandler(this.paths, this.context));
                     break;
                 case MODEL3D:
-                    this.fileHandlerThread = new Thread(new ModelExtractionFileHandler(this.paths, this.context));
+                    this.fileHandlerThread = new Thread(new Model3DExtractionFileHandler(this.paths, this.context));
                     break;
                 default:
                     break;
