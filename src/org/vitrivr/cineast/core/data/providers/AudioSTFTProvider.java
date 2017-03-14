898f2615bcea32249f181eea9ebe79f95addf517
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-03-14 08:17:18 +0100

    The default implementation of AudioSTFTProvider now uses the windowsize and the function parameter.

--- src/org/vitrivr/cineast/core/data/providers/AudioSTFTProvider.java
+++ src/org/vitrivr/cineast/core/data/providers/AudioSTFTProvider.java
@@ -1,7 +1,6 @@
 package org.vitrivr.cineast.core.data.providers;
 
 import org.vitrivr.cineast.core.util.fft.STFT;
-import org.vitrivr.cineast.core.util.fft.windows.RectangularWindow;
 import org.vitrivr.cineast.core.util.fft.windows.WindowFunction;
 
 import java.util.Arrays;
@@ -23,10 +22,10 @@ public interface AudioSTFTProvider {
      * @return STFT of the current AudioSegment.
      */
     default STFT getSTFT(int windowsize, int overlap, WindowFunction function) {
-        double[] data = new double[512];
+        double[] data = new double[windowsize];
         Arrays.fill(data, 0.0);
         STFT stft = new STFT(data, 22050);
-        stft.forward(128,0, new RectangularWindow());
+        stft.forward(windowsize,windowsize, function);
         return stft;
     }
 }
