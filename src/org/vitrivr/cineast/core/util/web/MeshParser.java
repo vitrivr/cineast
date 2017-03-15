1dbacc1f54f37dcb6e11e687127fb25e51c1e692
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-03-15 23:20:48 +0100

    MeshParses now also uses the recent changes in the Mesh class.

--- src/org/vitrivr/cineast/core/util/web/MeshParser.java
+++ src/org/vitrivr/cineast/core/util/web/MeshParser.java
@@ -33,9 +33,6 @@ public class MeshParser extends DataURLParser {
 		/* Convert Base64 string into byte array. */
         byte[] bytes = dataURLtoByteArray(dataUrl, MIME_TYPE);
 
-        /* Initializes a new empty Mesh. */
-        Mesh mesh = new Mesh();
-
         ObjectMapper mapper = new ObjectMapper();
         try {
             /* Read the JSON structure of the transmitted mesh data. */
@@ -43,26 +40,29 @@ public class MeshParser extends DataURLParser {
             JsonNode vertices = node.get(VERTICES_PROPERTY_NAME_THREEV4);
             if (vertices == null) {
                 LOGGER.error("Submitted mesh does not contain a position array. Aborting...");
-                return mesh;
+                return Mesh.EMPTY;
             }
 
             vertices = vertices.get(ARRAY_PROPERTY_NAME_THREEV4);
             if (vertices == null || !vertices.isArray() || vertices.size() == 0)  {
                 LOGGER.error("Submitted mesh does not contain any vertices. Aborting...");
-                return mesh;
+                return Mesh.EMPTY;
             }
 
             JsonNode normals = node.get(NORMAL_PROPERTY_NAME_THREEV4);
             if (normals == null) {
                 LOGGER.error("Submitted mesh does not contain any normals. Aborting...");
-                return mesh;
+                return Mesh.EMPTY;
             }
 
             normals = normals.get(ARRAY_PROPERTY_NAME_THREEV4);
             if (normals == null  || !normals.isArray() || normals.size() == 0) {
                 LOGGER.error("Submitted mesh does not contain any normals. Aborting...");
-                return mesh;
+                return Mesh.EMPTY;
             }
+
+            /* Create new Mesh. */
+            Mesh mesh = new Mesh(vertices.size()/3, vertices.size(),vertices.size());
 
             /* Add all the vertices and normals in the structure. */
             for (int i=0; i<(vertices.size()/3); i++) {
@@ -82,10 +82,11 @@ public class MeshParser extends DataURLParser {
                 int idx = 3*i;
                 mesh.addFace(new Vector3i(idx+1, idx+2, idx+3), new Vector3i(idx+1, idx+2, idx+3));
             }
+
+            return mesh;
         } catch (IOException e) {
             LOGGER.error("Could not create 3d mesh from Base64 input because the file-format is not supported. {}", LogHelper.getStackTrace(e));
+            return Mesh.EMPTY;
         }
-
-        return mesh;
     }
 }
