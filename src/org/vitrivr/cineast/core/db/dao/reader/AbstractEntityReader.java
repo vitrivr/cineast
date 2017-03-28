f518081a4120a82b846690e4d0ffb8791421948d
Luca Rossetto <luca.rossetto@unibas.ch>
2017-03-28 15:48:29 +0200

    made AbstractEntityReader Closeable

--- src/org/vitrivr/cineast/core/db/dao/reader/AbstractEntityReader.java
+++ src/org/vitrivr/cineast/core/db/dao/reader/AbstractEntityReader.java
@@ -1,5 +1,7 @@
 package org.vitrivr.cineast.core.db.dao.reader;
 
+import java.io.Closeable;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.vitrivr.cineast.core.db.DBSelector;
@@ -9,7 +11,7 @@ import org.vitrivr.cineast.core.db.DBSelector;
  * @version 1.0
  * @created 02.03.17
  */
-public abstract class AbstractEntityReader {
+public abstract class AbstractEntityReader implements Closeable {
     protected static final Logger LOGGER = LogManager.getLogger();
 
     /** DBSelector instance used to perform the DB lookup. */
