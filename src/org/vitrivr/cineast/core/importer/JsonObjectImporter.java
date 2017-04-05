a87d432cc8bf2a7003f14dd363c1bea1c2c9de08
Luca Rossetto <luca.rossetto@unibas.ch>
2017-04-05 12:00:47 +0200

    re-wrote JsonObjectImporter to be streaming

--- src/org/vitrivr/cineast/core/importer/JsonObjectImporter.java
+++ src/org/vitrivr/cineast/core/importer/JsonObjectImporter.java
@@ -1,124 +1,59 @@
 package org.vitrivr.cineast.core.importer;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.vitrivr.cineast.core.data.providers.primitive.BooleanTypeProvider;
-import org.vitrivr.cineast.core.data.providers.primitive.DoubleTypeProvider;
-import org.vitrivr.cineast.core.data.providers.primitive.FloatTypeProvider;
-import org.vitrivr.cineast.core.data.providers.primitive.FloatVectorProvider;
-import org.vitrivr.cineast.core.data.providers.primitive.IntTypeProvider;
-import org.vitrivr.cineast.core.data.providers.primitive.LongTypeProvider;
-import org.vitrivr.cineast.core.data.providers.primitive.NothingProvider;
 import org.vitrivr.cineast.core.data.providers.primitive.PrimitiveTypeProvider;
-import org.vitrivr.cineast.core.data.providers.primitive.StringTypeProvider;
 import org.vitrivr.cineast.core.util.LogHelper;
 
-import com.eclipsesource.json.JsonArray;
-import com.eclipsesource.json.JsonObject;
-import com.eclipsesource.json.JsonValue;
-import com.eclipsesource.json.ParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
-public class JsonObjectImporter implements Importer<JsonObject> {
+public class JsonObjectImporter implements Importer<ObjectNode> {
 
-	private JsonArray array;
-	private int cursor = 0;
+	private final JsonParser parser;
+	private final ObjectMapper mapper = new ObjectMapper();
+	private final File inputFile;
 	private static final Logger LOGGER = LogManager.getLogger();
 	
-	public JsonObjectImporter(File input) throws FileNotFoundException, IOException {
-		try{
-			this.array = JsonValue.readFrom(new FileReader(input)).asArray();
-		}catch(UnsupportedOperationException | ParseException e){
-			this.array = new JsonArray();
-			LOGGER.error("Error while reading json file: {}, {}",input.getAbsolutePath(),  LogHelper.getStackTrace(e));
-		}
+	public JsonObjectImporter(File input) throws IOException{
+	  this.inputFile = input;
+    this.parser = this.mapper.getFactory().createParser(input);
+    if(parser.nextToken() != JsonToken.START_ARRAY) {
+      throw new IllegalStateException("Expected an array");
+    }
 	} 
 	
 	@Override
-	public JsonObject readNext() {
-		if(this.cursor >= this.array.size()){
-			return null;
-		}
-		JsonValue val = this.array.get(this.cursor++);
-		try{
-			return val.asObject();
-		}catch(UnsupportedOperationException e){
-			return null;
-		}
+	public ObjectNode readNext() {
+		try {
+      if(parser.nextToken() == JsonToken.START_OBJECT){
+        return mapper.readTree(parser);
+      }
+    } catch (IOException e) {
+      LOGGER.error("error while reading json file '{}': {}", this.inputFile.getAbsolutePath(), LogHelper.getStackTrace(e));
+    }
+		return null;
 	}
 
 	@Override
-	public Map<String, PrimitiveTypeProvider> convert(JsonObject jobj) {
-		if(jobj == null){
-			return null;
-		}
-		
-		HashMap<String, PrimitiveTypeProvider> map = new HashMap<>();
-		
-		for(String name : jobj.names()){
-			map.put(name, convert(jobj.get(name)));
-		}
-		
-		return map;
+	public Map<String, PrimitiveTypeProvider> convert(ObjectNode node) {
+	  @SuppressWarnings("unchecked")
+    Map<String, Object> result = mapper.convertValue(node, Map.class);
+    Map<String, PrimitiveTypeProvider> map = new HashMap<>(result.size());
+    
+    for(String key : result.keySet()){
+      Object o = result.get(key);
+     map.put(key, PrimitiveTypeProvider.fromObject(o));
+    }
+    return map;
 		
 	}
 
-	public static PrimitiveTypeProvider convert(JsonValue jval){
-		if(jval == null || jval.isNull()){
-			return null;
-		}
-		if(jval.isBoolean()){
-			return new BooleanTypeProvider(jval.asBoolean());
-		}
-		if (jval.isNumber()) {
-			try {
-				return new IntTypeProvider(jval.asInt());
-			} catch (NumberFormatException e) {
-				// ignore
-			}
-			try {
-				return new LongTypeProvider(jval.asLong());
-			} catch (NumberFormatException e) {
-				// ignore
-			}
-			try {
-				return new FloatTypeProvider(jval.asFloat());
-			} catch (NumberFormatException e) {
-				// ignore
-			}
-			try {
-				return new DoubleTypeProvider(jval.asDouble());
-			} catch (NumberFormatException e) {
-				// ignore
-			}
-			return new DoubleTypeProvider(Double.NaN);
-		}
-		if(jval.isString()){
-			return new StringTypeProvider(jval.asString());
-		}
-		if(jval.isArray()){
-			JsonArray jarr = jval.asArray();
-			if(jarr.isEmpty()){
-				return new FloatVectorProvider(new float[0]);
-			}
-			float[] arr = new float[jarr.size()];
-			for(int i = 0; i < arr.length; ++i){
-				try{
-					arr[i] = jarr.get(i).asFloat();
-				}catch(NumberFormatException | UnsupportedOperationException e){
-					//ignore
-				}
-			}
-			return new FloatVectorProvider(arr);
-		}
-		
-		return new NothingProvider();
-	}
-	
 }
