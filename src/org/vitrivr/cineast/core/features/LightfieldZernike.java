73670bd2bcb1497945e6c1c3f49b7301f89415ad
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-04-12 10:57:12 +0200

    Cleanup

--- src/org/vitrivr/cineast/core/features/LightfieldZernike.java
+++ src/org/vitrivr/cineast/core/features/LightfieldZernike.java
@@ -1,15 +1,10 @@
 package org.vitrivr.cineast.core.features;
 
-import com.twelvemonkeys.image.ImageUtil;
-import gnu.trove.map.hash.TObjectDoubleHashMap;
 import org.apache.commons.math3.complex.Complex;
-import org.vitrivr.cineast.core.config.Config;
+
 import org.vitrivr.cineast.core.config.QueryConfig;
 import org.vitrivr.cineast.core.data.Pair;
 import org.vitrivr.cineast.core.data.StringDoublePair;
-import org.vitrivr.cineast.core.data.m3d.Mesh;
-import org.vitrivr.cineast.core.data.m3d.ReadableMesh;
-import org.vitrivr.cineast.core.data.providers.primitive.PrimitiveTypeProvider;
 import org.vitrivr.cineast.core.data.segments.SegmentContainer;
 
 import org.vitrivr.cineast.core.features.abstracts.AbstractLightfieldDescriptor;
@@ -18,12 +13,9 @@ import org.vitrivr.cineast.core.util.images.ZernikeHelper;
 import org.vitrivr.cineast.core.util.math.MathConstants;
 import org.vitrivr.cineast.core.util.math.ZernikeMoments;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
 
 /**
  * @author rgasser
