bc84e0228f65d3ae39c0fac14524d50bb821792e
Lukas Beck <lukas.beck@unibas.ch>
2017-04-25 10:33:22 +0200

    Refactored ReadableFloatVector: created one static method for calculating euclidean distance.

--- src/org/vitrivr/cineast/core/data/ReadableFloatVector.java
+++ src/org/vitrivr/cineast/core/data/ReadableFloatVector.java
@@ -4,17 +4,27 @@ import java.util.List;
 
 public interface ReadableFloatVector {
 
-  double getEuclideanDistance(ReadableFloatVector other);
-  
-	int getElementCount();
-	
-	float getElement(int num);
-	
-	/**
-	 * maps the vector to a float array
-	 * @param arr the array to write into. If arr is null or does not have the correct length, a new array is generated instead
-	 */
-	float[] toArray(float[] arr);
-	
-	List<Float> toList(List<Float> list);
+  int getElementCount();
+
+  float getElement(int num);
+
+  /**
+   * maps the vector to a float array
+   *
+   * @param arr the array to write into. If arr is null or does not have the correct length, a new
+   * array is generated instead
+   */
+  float[] toArray(float[] arr);
+
+  List<Float> toList(List<Float> list);
+
+  static double getEuclideanDistance(ReadableFloatVector first, ReadableFloatVector second) {
+    int len = Math.min(first.getElementCount(), second.getElementCount());
+    double sum = 0d;
+    for (int i = 0; i < len; ++i) {
+      double diff = first.getElement(i) - second.getElement(i);
+      sum += diff * diff;
+    }
+    return Math.sqrt(sum);
+  }
 }
