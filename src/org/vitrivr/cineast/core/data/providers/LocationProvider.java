a63da9ef990781269305e7410da8185a1bc8651f
Lukas Beck <lukas.beck@unibas.ch>
2017-04-25 16:52:44 +0200

    Added location and respective provider to SegmentContainer.

---
+++ src/org/vitrivr/cineast/core/data/providers/LocationProvider.java
@@ -0,0 +1,10 @@
+package org.vitrivr.cineast.core.data.providers;
+
+import java.util.Optional;
+import org.vitrivr.cineast.core.data.Location;
+
+public interface LocationProvider {
+  default Optional<Location> getLocation() {
+    return Optional.empty();
+  }
+}
--- src/org/vitrivr/cineast/core/data/segments/SegmentContainer.java
+++ src/org/vitrivr/cineast/core/data/segments/SegmentContainer.java
@@ -1,7 +1,29 @@
 package org.vitrivr.cineast.core.data.segments;
 
-import org.vitrivr.cineast.core.data.providers.*;
+import org.vitrivr.cineast.core.data.providers.AudioFrameProvider;
+import org.vitrivr.cineast.core.data.providers.AudioSTFTProvider;
+import org.vitrivr.cineast.core.data.providers.AvgImgProvider;
+import org.vitrivr.cineast.core.data.providers.DurationProvider;
+import org.vitrivr.cineast.core.data.providers.FrameListProvider;
+import org.vitrivr.cineast.core.data.providers.IdProvider;
+import org.vitrivr.cineast.core.data.providers.LocationProvider;
+import org.vitrivr.cineast.core.data.providers.MedianImgProvider;
+import org.vitrivr.cineast.core.data.providers.MostRepresentativeFrameProvider;
+import org.vitrivr.cineast.core.data.providers.PathProvider;
+import org.vitrivr.cineast.core.data.providers.SubtitleItemProvider;
+import org.vitrivr.cineast.core.data.providers.TagProvider;
 
-public interface SegmentContainer extends IdProvider, AvgImgProvider, DurationProvider, MedianImgProvider, MostRepresentativeFrameProvider, SubtitleItemProvider, PathProvider, TagProvider, FrameListProvider, AudioFrameProvider, AudioSTFTProvider {
-
+public interface SegmentContainer extends
+    IdProvider,
+    AvgImgProvider,
+    DurationProvider,
+    MedianImgProvider,
+    MostRepresentativeFrameProvider,
+    SubtitleItemProvider,
+    PathProvider,
+    TagProvider,
+    FrameListProvider,
+    AudioFrameProvider,
+    AudioSTFTProvider,
+    LocationProvider {
 }
