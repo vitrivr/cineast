156f7b5e1d1ebfa300ef2c0c5a17ef622c80ec16
Luca Rossetto <luca.rossetto@unibas.ch>
2017-04-27 09:00:51 +0200

    minor cleanup

--- src/org/vitrivr/cineast/core/data/FixedSizePriorityQueue.java
+++ src/org/vitrivr/cineast/core/data/FixedSizePriorityQueue.java
@@ -65,10 +65,4 @@ public class FixedSizePriorityQueue<E> extends TreeSet<E> {
     return this.maxSize;
   }
 
-  private static class NaturalComparator<T extends Comparable<? super T>> implements Comparator<T> {
-    @Override
-    public int compare(T o1, T o2) {
-      return o1.compareTo(o2);
-    }
-  }
 }
