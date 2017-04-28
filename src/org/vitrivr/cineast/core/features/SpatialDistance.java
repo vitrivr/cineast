dcfa10724fa3f09e01eb266ae97ee45dd88b36b3
Lukas Beck <lukas.beck@unibas.ch>
2017-04-28 14:51:39 +0200

    Created MetadataFeatureModule that combines MetadataExtractor and Retriever. Changed SpatialDistance accordingly.

--- src/org/vitrivr/cineast/core/features/SpatialDistance.java
+++ src/org/vitrivr/cineast/core/features/SpatialDistance.java
@@ -3,89 +3,50 @@ package org.vitrivr.cineast.core.features;
 import com.drew.metadata.exif.GpsDirectory;
 import com.google.common.collect.ImmutableList;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.Supplier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.vitrivr.cineast.core.config.Config;
-import org.vitrivr.cineast.core.config.QueryConfig;
-import org.vitrivr.cineast.core.config.ReadableQueryConfig;
 import org.vitrivr.cineast.core.config.ReadableQueryConfig.Distance;
 import org.vitrivr.cineast.core.data.CorrespondenceFunction;
 import org.vitrivr.cineast.core.data.Location;
-import org.vitrivr.cineast.core.data.ReadableFloatVector;
-import org.vitrivr.cineast.core.data.distance.DistanceElement;
-import org.vitrivr.cineast.core.data.distance.ObjectDistanceElement;
 import org.vitrivr.cineast.core.data.entities.MultimediaMetadataDescriptor;
-import org.vitrivr.cineast.core.data.entities.SimpleFeatureDescriptor;
-import org.vitrivr.cineast.core.data.score.ScoreElement;
 import org.vitrivr.cineast.core.data.segments.SegmentContainer;
 import org.vitrivr.cineast.core.db.DBSelector;
-import org.vitrivr.cineast.core.db.DBSelectorSupplier;
-import org.vitrivr.cineast.core.db.PersistencyWriter;
 import org.vitrivr.cineast.core.db.dao.reader.SegmentLookup;
 import org.vitrivr.cineast.core.db.dao.writer.SimpleFeatureDescriptorWriter;
+import org.vitrivr.cineast.core.features.abstracts.MetadataFeatureModule;
 import org.vitrivr.cineast.core.features.retriever.Retriever;
 import org.vitrivr.cineast.core.metadata.JsonMetadataExtractor;
 import org.vitrivr.cineast.core.metadata.MetadataExtractor;
-import org.vitrivr.cineast.core.setup.EntityCreator;
 import org.vitrivr.cineast.core.util.OptionalUtil;
 import org.vitrivr.cineast.core.util.images.MetadataUtil;
 
 /**
  * A feature that calculates similarity based on an approximation of the great-circle distance
- * between two documents. The feature extraction and retrieval bases on objects rather than
- * segments.
+ * between two objects.
  *
- * <p>As a {@link MetadataExtractor} it tries to extract the latitude and longitude coordinates from
- * the object itself, and storing the object id and the feature vector as
- * {@code [latitude, longitude]}.
+ * <p>It extracts the latitude and longitude coordinates provided by Exif GPS data, if available,
+ * or a supplementary JSON file. See {@link #extractFeature(Path)} for more information.
  *
- * <p>It extracts the feature data using the GPS data of the Exif format. If not present, it tries
- * to retrieve the coordinates from a JSON file named after the original document,
- * e.g. {@code image_0001.json} for {@code image_0001.jpg}. To successfully extract the coordinates,
- * the JSON file must contain a top level object with {@code "latitude"} and {@code "longitude"}
- * key-value pairs, for example:
- *
- * <pre>
- *   {
- *     "latitude": 47.57,
- *     "longitude": 7.6
- *   }</pre>
- *
- * <p>During retrieval, it does a k-nearest neighbor search based on the coordinates by making use
- * of the <i>Haversine</i> distance. Because the {@link Retriever} interface features similarity
- * only on segments, during similarity search based on an existing segment (i.e. {@code segmentId})
- * the feature has to perform a {@link SegmentLookup} in order to retrieve the object id of the
- * given segment.
- *
- * <p>As of now, the feature uses a linear {@link CorrespondenceFunction} with a maximum distance of
- * {@code 1000m}. This correspondence <i>heavily</i> influences the quality of the retrieval and
- * is likely to be unfit for some data sets.
+ * <p>During retrieval, it does similarity search using the <i>Haversine</i> distance as an
+ * approximation of the great-circle distance. As of now, the feature uses a linear
+ * correspondence function with a maximum distance of {@code 1000m}. This correspondence
+ * <i>heavily</i> influences the quality of the retrieval and is likely to be unfit for some data
+ * sets.
  */
-public class SpatialDistance implements MetadataExtractor, Retriever {
-  private static final Logger logger = LogManager.getLogger();
-
+public class SpatialDistance extends MetadataFeatureModule<Location> {
   private static final String METADATA_DOMAIN = "LOCATION";
   private static final String KEY_LATITUDE = "latitude";
   private static final String KEY_LONGITUDE = "longitude";
 
-  private static final String ENTITY_NAME = "features_SpatialDistance";
-  private static final String ID_COLUMN_NAME = "id"; // Constant used in ADAMproEntityCreator
-  private static final String FEATURE_COLUMN_NAME = "feature";
-  private static final int WRITER_BATCHSIZE = 10; // Taken from AbstractFeatureModule
+  private static final String FEATURE_NAME = "features_SpatialDistance";
 
   // TODO: Find most suitable maximum distance, maybe even as a property to the feature
   private static final double MAXIMUM_DISTANCE = 1000d;
   private static final CorrespondenceFunction CORRESPONDENCE =
       CorrespondenceFunction.linear(MAXIMUM_DISTANCE);
-
-  private SimpleFeatureDescriptorWriter featureWriter;
-  private DBSelector dbSelector;
-  private SegmentLookup segmentLookup;
 
   // Empty public constructor necessary for instantiation through reflection
   public SpatialDistance() {}
@@ -95,72 +56,46 @@ public class SpatialDistance implements MetadataExtractor, Retriever {
     return METADATA_DOMAIN;
   }
 
-  /* Initialization */
   @Override
-  public void init() {
-    PersistencyWriter<?> writer = Config.sharedConfig().getDatabase().getWriterSupplier().get();
-    this.featureWriter = new SimpleFeatureDescriptorWriter(writer, ENTITY_NAME, WRITER_BATCHSIZE);
-    this.featureWriter.init();
+  public String featureEntityName() {
+    return FEATURE_NAME;
   }
 
   @Override
-  public void init(DBSelectorSupplier selectorSupply) {
-    this.dbSelector = selectorSupply.get();
-    this.dbSelector.open(ENTITY_NAME);
-    this.segmentLookup = new SegmentLookup(selectorSupply.get());
+  public Distance defaultDistance() {
+    return Distance.haversine;
   }
 
   @Override
-  public void initalizePersistentLayer(Supplier<EntityCreator> supply) {
-    supply.get().createFeatureEntity(ENTITY_NAME, true, FEATURE_COLUMN_NAME);
-  }
-
-  /* Finishing */
-  @Override
-  public void dropPersistentLayer(Supplier<EntityCreator> supply) {
-    supply.get().dropEntity(ENTITY_NAME);
+  public CorrespondenceFunction defaultCorrespondence() {
+    return CORRESPONDENCE;
   }
 
   @Override
-  public void finish() {
-    if (this.featureWriter != null) {
-      this.featureWriter.close();
-      this.featureWriter = null;
-    }
-
-    if (this.dbSelector != null) {
-      this.dbSelector.close();
-      this.dbSelector = null;
-    }
-
-    if (this.segmentLookup != null) {
-      this.segmentLookup.close();
-      this.segmentLookup = null;
-    }
+  public Optional<Location> extractFeature(SegmentContainer segmentContainer) {
+    return segmentContainer.getLocation();
   }
 
-  /* Extraction */
   /**
-   * Extracts the latitude and longitude coordinates from the specified path using Exif data or a
-   * supplementary JSON file (see the class description for more details), stores them in the
-   * feature table with the given {@code objectId} and returns two descriptors based on the
-   * coordinates.
+   * Extracts the latitude and longitude coordinates from the specified path using Exif GPS data. If
+   * not present, it retrieves the coordinates from a complementary JSON file named after the
+   * original document, e.g. {@code image_0001.json} for {@code image_0001.jpg}, by parsing the
+   * {@code "latitude"} and {@code "longitude"} key-value pairs. For example:
    *
-   * @param objectId ID of the multimedia object for which metadata will be generated.
+   * <pre>
+   *   {
+   *     ...
+   *     "latitude": 47.57,
+   *     "longitude": 7.6
+   *     ...
+   *   }</pre>
+   *
    * @param objectPath Path to the file for which the coordinates should be extracted.
-   * @return a list of two {@code MultimediaMetadataDescriptor}s containing the two coordinates, if
-   *         found and successfully extracted from the path, otherwise an empty list.
+   * @return an {@link Optional} containing a {@link Location} based on the extracted Exif or JSON
+   *         coordinates, if found, otherwise an empty {@code Optional}.
    */
   @Override
-  public List<MultimediaMetadataDescriptor> extract(String objectId, Path objectPath) {
-    Optional<Location> location = this.extractLocation(objectId, objectPath);
-    location.ifPresent(l -> this.storeLocation(objectId, l));
-    return location
-        .map(l -> this.createLocationDescriptors(objectId, l))
-        .orElse(Collections.emptyList());
-  }
-
-  private Optional<Location> extractLocation(String objectId, Path objectPath) {
+  public Optional<Location> extractFeature(Path objectPath) {
     return OptionalUtil.or(this.extractExifGpsLocation(objectPath),
         () -> this.extractJsonMetadataLocation(objectPath));
   }
@@ -189,12 +124,8 @@ public class SpatialDistance implements MetadataExtractor, Retriever {
         .map(o -> ((Number) o).floatValue());
   }
 
-  private void storeLocation(String objectId, Location location) {
-    this.featureWriter.write(new SimpleFeatureDescriptor(objectId, location));
-  }
-
-  private List<MultimediaMetadataDescriptor> createLocationDescriptors(String objectId,
-      Location location) {
+  @Override
+  public List<MultimediaMetadataDescriptor> createDescriptors(String objectId, Location location) {
     return ImmutableList.of(
         MultimediaMetadataDescriptor
             .newMultimediaMetadataDescriptor(objectId, this.domain(),
@@ -203,44 +134,5 @@ public class SpatialDistance implements MetadataExtractor, Retriever {
             .newMultimediaMetadataDescriptor(objectId, this.domain(),
                 KEY_LONGITUDE, location.getLongitude())
     );
-  }
-
-  /* Retrieval */
-  @Override
-  public List<ScoreElement> getSimilar(SegmentContainer sc, ReadableQueryConfig rqc) {
-    return sc.getLocation()
-        .map(location -> this.getSimilar(location, rqc))
-        .orElse(Collections.emptyList());
-  }
-
-  /**
-   * Performs the similarity search based on an existing segment. Because feature data is based on
-   * objects, this function performs a {@link SegmentLookup} to retrieve the object id of the given
-   * segment.
-   */
-  @Override
-  public List<ScoreElement> getSimilar(String segmentId, ReadableQueryConfig rqc) {
-    return this.segmentLookup.lookUpSegment(segmentId)
-        .map(descriptor -> descriptor.getObjectId())
-        .map(objId -> this.dbSelector.getFeatureVectors(ID_COLUMN_NAME, objId, FEATURE_COLUMN_NAME))
-        .flatMap(features -> features.stream().findFirst())
-        .map(feature -> Location.of(feature))
-        .map(location -> this.getSimilar(location, rqc))
-        .orElse(Collections.emptyList());
-  }
-
-  private List<ScoreElement> getSimilar(Location location, ReadableQueryConfig rqc) {
-    QueryConfig qc = QueryConfig.clone(rqc).setDistanceIfEmpty(Distance.haversine);
-    int maxResultsPerModule = Config.sharedConfig().getRetriever().getMaxResultsPerModule();
-    List<ObjectDistanceElement> distances = this.dbSelector.getNearestNeighbours(
-        maxResultsPerModule,
-        ReadableFloatVector.toArray(location),
-        FEATURE_COLUMN_NAME,
-        ObjectDistanceElement.class,
-        qc
-    );
-
-    CorrespondenceFunction correspondence = qc.getCorrespondenceFunction().orElse(CORRESPONDENCE);
-    return DistanceElement.toScore(distances, correspondence);
   }
 }
---
+++ src/org/vitrivr/cineast/core/features/abstracts/MetadataFeatureModule.java
@@ -0,0 +1,186 @@
+package org.vitrivr.cineast.core.features.abstracts;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+import org.vitrivr.cineast.core.config.Config;
+import org.vitrivr.cineast.core.config.QueryConfig;
+import org.vitrivr.cineast.core.config.ReadableQueryConfig;
+import org.vitrivr.cineast.core.config.ReadableQueryConfig.Distance;
+import org.vitrivr.cineast.core.data.CorrespondenceFunction;
+import org.vitrivr.cineast.core.data.ReadableFloatVector;
+import org.vitrivr.cineast.core.data.distance.DistanceElement;
+import org.vitrivr.cineast.core.data.distance.ObjectDistanceElement;
+import org.vitrivr.cineast.core.data.entities.MultimediaMetadataDescriptor;
+import org.vitrivr.cineast.core.data.entities.SimpleFeatureDescriptor;
+import org.vitrivr.cineast.core.data.score.ScoreElement;
+import org.vitrivr.cineast.core.data.segments.SegmentContainer;
+import org.vitrivr.cineast.core.db.DBSelector;
+import org.vitrivr.cineast.core.db.DBSelectorSupplier;
+import org.vitrivr.cineast.core.db.PersistencyWriter;
+import org.vitrivr.cineast.core.db.dao.reader.SegmentLookup;
+import org.vitrivr.cineast.core.db.dao.writer.SimpleFeatureDescriptorWriter;
+import org.vitrivr.cineast.core.features.retriever.Retriever;
+import org.vitrivr.cineast.core.metadata.MetadataExtractor;
+import org.vitrivr.cineast.core.setup.EntityCreator;
+
+/**
+ * Feature module that bases its feature data on the object (usually metadata) itself instead of
+ * individual segments.
+ *
+ * @param <T> the type of the feature data
+ */
+public abstract class MetadataFeatureModule<T extends ReadableFloatVector>
+    implements MetadataExtractor, Retriever {
+  private static final String ID_COLUMN_NAME = "id"; // Constant used in ADAMproEntityCreator
+  private static final String FEATURE_COLUMN_NAME = "feature";
+  private static final int WRITER_BATCHSIZE = 10; // Taken from AbstractFeatureModule
+
+  private SimpleFeatureDescriptorWriter featureWriter;
+  private DBSelector dbSelector;
+  private SegmentLookup segmentLookup;
+
+  protected MetadataFeatureModule() {}
+
+  /** Returns the name of the feature entity as stored in the persistent layer. */
+  public abstract String featureEntityName();
+
+  /** Returns the default distance if none is set. */
+  public abstract Distance defaultDistance();
+
+  /** Returns the default correspondence function if none is set. */
+  public abstract CorrespondenceFunction defaultCorrespondence();
+
+  /** Returns a list of descriptors of the given feature data. */
+  public abstract List<MultimediaMetadataDescriptor> createDescriptors(String objectId, T feature);
+
+  /**
+   * Returns an {@link Optional} containing the extracted feature data from the file, if found,
+   * otherwise an empty {@code Optional}.
+   */
+  public abstract Optional<T> extractFeature(Path path);
+
+  /**
+   * Returns an {@link Optional} containing the extracted feature data from the segment container,
+   * if available, otherwise an empty {@code Optional}.
+   */
+  public abstract Optional<T> extractFeature(SegmentContainer segmentContainer);
+
+  @Override
+  public void initalizePersistentLayer(Supplier<EntityCreator> supply) {
+    supply.get().createFeatureEntity(this.featureEntityName(), true, FEATURE_COLUMN_NAME);
+  }
+
+  @Override
+  public void dropPersistentLayer(Supplier<EntityCreator> supply) {
+    supply.get().dropEntity(this.featureEntityName());
+  }
+
+  @Override
+  public void init() {
+    PersistencyWriter<?> writer = Config.sharedConfig().getDatabase().getWriterSupplier().get();
+    this.featureWriter = new SimpleFeatureDescriptorWriter(writer, this.featureEntityName(),
+        WRITER_BATCHSIZE);
+    this.featureWriter.init();
+  }
+
+  @Override
+  public void init(DBSelectorSupplier selectorSupply) {
+    this.dbSelector = selectorSupply.get();
+    this.dbSelector.open(this.featureEntityName());
+    this.segmentLookup = new SegmentLookup(selectorSupply.get());
+  }
+
+  public boolean isExtractorInitialized() {
+    return this.featureWriter != null;
+  }
+
+  public boolean isRetrieverInitialized() {
+    return this.dbSelector != null && this.segmentLookup != null;
+  }
+
+  @Override
+  public void finish() {
+    if (this.featureWriter != null) {
+      this.featureWriter.close();
+      this.featureWriter = null;
+    }
+
+    if (this.dbSelector != null) {
+      this.dbSelector.close();
+      this.dbSelector = null;
+    }
+
+    if (this.segmentLookup != null) {
+      this.segmentLookup.close();
+      this.segmentLookup = null;
+    }
+  }
+
+  /**
+   * Extracts the feature data from a given object, stores it if present and returns a list of
+   * descriptors based on the data.
+   */
+  @Override
+  public List<MultimediaMetadataDescriptor> extract(String objectId, Path path) {
+    checkState(this.isExtractorInitialized(), "extract called before init");
+    Optional<T> feature = this.extractFeature(path);
+    feature.ifPresent(v -> this.featureWriter.write(new SimpleFeatureDescriptor(objectId, v)));
+    return feature
+        .map(floatVector -> this.createDescriptors(objectId, floatVector))
+        .orElse(Collections.emptyList());
+  }
+
+  /**
+   * Returns similar <i>objects</i> to the feature data contained in the given segment container.
+   */
+  @Override
+  public List<ScoreElement> getSimilar(SegmentContainer sc, ReadableQueryConfig qc) {
+    this.checkIfRetrieverInitialized();
+    return this.extractFeature(sc)
+        .map(floatVector -> this.getSimilar(ReadableFloatVector.toArray(floatVector), qc))
+        .orElse(Collections.emptyList());
+  }
+
+  /**
+   * Returns similar <i>objects</i> to the object of the given segment.
+   *
+   * <p>Note that this methods performs a {@link SegmentLookup} in order to retrieve the object id
+   * of the given segment.
+   */
+  @Override
+  public List<ScoreElement> getSimilar(String segmentId, ReadableQueryConfig rqc) {
+    this.checkIfRetrieverInitialized();
+    return this.segmentLookup.lookUpSegment(segmentId)
+        .map(descriptor -> descriptor.getObjectId())
+        .map(objId -> this.dbSelector.getFeatureVectors(ID_COLUMN_NAME, objId, FEATURE_COLUMN_NAME))
+        .flatMap(features -> features.stream().findFirst())
+        .map(feature -> this.getSimilar(feature, rqc))
+        .orElse(Collections.emptyList());
+  }
+
+  private void checkIfRetrieverInitialized() {
+    checkState(this.isRetrieverInitialized(), "getSimilar called before init");
+  }
+
+  private List<ScoreElement> getSimilar(float[] feature, ReadableQueryConfig rqc) {
+    QueryConfig qc = QueryConfig.clone(rqc).setDistanceIfEmpty(this.defaultDistance());
+
+    int maxResultsPerModule = Config.sharedConfig().getRetriever().getMaxResultsPerModule();
+    List<ObjectDistanceElement> distances = this.dbSelector.getNearestNeighbours(
+        maxResultsPerModule,
+        feature,
+        FEATURE_COLUMN_NAME,
+        ObjectDistanceElement.class,
+        qc
+    );
+
+    CorrespondenceFunction correspondence = qc.getCorrespondenceFunction()
+        .orElse(this.defaultCorrespondence());
+    return DistanceElement.toScore(distances, correspondence);
+  }
+}
