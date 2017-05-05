a80d107ef42309d03dbbd7ba41f1689b4867f520
Lukas Beck <lukas.beck@unibas.ch>
2017-05-05 09:37:57 +0200

    Removed resolved TODO note.

--- src/org/vitrivr/cineast/core/data/providers/InstantProvider.java
+++ src/org/vitrivr/cineast/core/data/providers/InstantProvider.java
@@ -3,7 +3,6 @@ package org.vitrivr.cineast.core.data.providers;
 import java.time.Instant;
 import java.util.Optional;
 
-// TODO: maybe rename to "InstantProvider"??
 public interface InstantProvider {
   default Optional<Instant> getInstant() {
     return Optional.empty();
