b36112360deade4820875d6c625dbaef0ae77dbb
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-05-08 10:15:14 +0200

    ADDED: Convenice method to instantiate Converters from their FQN.

--- src/org/vitrivr/cineast/core/util/ReflectionHelper.java
+++ src/org/vitrivr/cineast/core/util/ReflectionHelper.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import org.vitrivr.cineast.core.decode.general.Converter;
 import org.vitrivr.cineast.core.features.abstracts.AbstractFeatureModule;
 import org.vitrivr.cineast.core.features.codebook.CodebookGenerator;
 import org.vitrivr.cineast.core.features.extractor.Extractor;
@@ -81,6 +82,23 @@ public class ReflectionHelper {
 			return instanciate(c);
 		} catch (ClassNotFoundException | InstantiationException e) {
 			LOGGER.fatal("Failed to create CodebookGenerator. Could not find or access class with name {}.", name, LogHelper.getStackTrace(e));
+			return null;
+		}
+	}
+
+	/**
+	 * Tries to instantiate a new, named Covnerter object. If the methods succeeds to do so,
+	 * that instance is returned by the method.
+	 *
+	 * @param fqn The fully-qualified name of the converter.
+	 * @return Instance of Converter or null, if instantiation fails.
+	 */
+	public static Converter newConverter(String fqn) {
+		try {
+			Class<Converter> c = (Class<Converter>) Class.forName(fqn);
+			return instanciate(c);
+		} catch (ClassNotFoundException e) {
+			LOGGER.fatal("Failed to create Converter. Could not find or access class with name {}.", fqn, LogHelper.getStackTrace(e));
 			return null;
 		}
 	}
