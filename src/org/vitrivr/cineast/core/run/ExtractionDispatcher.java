9c3b8e3a0551beb6c07b996f24ff0a752dc49d8d
Luca Rossetto <luca.rossetto@unibas.ch>
2017-09-25 09:30:22 +0200

    added missing case for 3d model extraction

--- src/org/vitrivr/cineast/core/run/ExtractionDispatcher.java
+++ src/org/vitrivr/cineast/core/run/ExtractionDispatcher.java
@@ -14,6 +14,7 @@ import org.vitrivr.cineast.core.config.IngestConfig;
 import org.vitrivr.cineast.core.data.MediaType;
 import org.vitrivr.cineast.core.run.filehandler.AudioExtractionFileHandler;
 import org.vitrivr.cineast.core.run.filehandler.ImageExtractionFileHandler;
+import org.vitrivr.cineast.core.run.filehandler.Model3DExtractionFileHandler;
 import org.vitrivr.cineast.core.run.filehandler.VideoExtractionFileHandler;
 import org.vitrivr.cineast.core.util.LogHelper;
 import org.vitrivr.cineast.core.util.json.JacksonJsonProvider;
@@ -25,88 +26,101 @@ import org.vitrivr.cineast.core.util.json.JacksonJsonProvider;
  */
 public class ExtractionDispatcher {
 
-    private static final Logger LOGGER = LogManager.getLogger();
+  private static final Logger LOGGER = LogManager.getLogger();
 
-    /** ExtractionContextProvider used to setup the extraction. */
-    private ExtractionContextProvider context;
+  /** ExtractionContextProvider used to setup the extraction. */
+  private ExtractionContextProvider context;
 
-    /** List of files due for extraction. */
-    private Iterator<Path> paths;
+  /** List of files due for extraction. */
+  private Iterator<Path> paths;
 
-    /** Reference to the thread that is being used to run the ExtractionFileHandler. */
-    private Thread fileHandlerThread;
+  /** Reference to the thread that is being used to run the ExtractionFileHandler. */
+  private Thread fileHandlerThread;
 
-    /**
-     *
-     * @param jobFile
-     */
-    public boolean initialize(File jobFile) throws IOException {
-        File outputLocation = Config.sharedConfig().getExtractor().getOutputLocation();
-        if(outputLocation == null){
-          LOGGER.error("invalid output location specified in config");
-          return false;
-        }
-        outputLocation.mkdirs();
-        if(!outputLocation.canWrite()){
-          LOGGER.error("cannot write to specified output location: '{}'", outputLocation.getAbsolutePath());
-          return false;
-        }
-      
-        JacksonJsonProvider reader = new JacksonJsonProvider();
-        this.context = reader.toObject(jobFile, IngestConfig.class);
-        
-        /* Check if context could be read and an input path was specified. */
-        if (context == null || this.context.inputPath() == null) return false;
-        Path jobDirectory = jobFile.getAbsoluteFile().toPath().getParent();
-        Path path = jobDirectory.resolve(this.context.inputPath()).normalize().toAbsolutePath();
-        
-        /*
-         * Recursively add all files under that path to the List of files that should be processed. Uses the context-provider
-         * to determine the depth of recursion, skip files and limit the number of files.
-         */
-        if (!Files.exists(path)) {
-            LOGGER.warn("The path '{}' specified in the extraction configuration does not exist!", path.toString());
-            return false;
-        }
-        this.paths = Files.walk(path, this.context.depth(), FileVisitOption.FOLLOW_LINKS)
-                     .filter(p -> {
-                         try {
-                             return Files.exists(p) && Files.isRegularFile(p) && !Files.isHidden(p) && Files.isReadable(p);
-                         } catch (IOException e) {
-                             LOGGER.error("An IO exception occurred while testing the media file at '{}'.", p.toString(), LogHelper.getStackTrace(e));
-                             return false;
-                         }
-                     }).iterator();
-        return true;
+  /**
+   *
+   * @param jobFile
+   */
+  public boolean initialize(File jobFile) throws IOException {
+    File outputLocation = Config.sharedConfig().getExtractor().getOutputLocation();
+    if (outputLocation == null) {
+      LOGGER.error("invalid output location specified in config");
+      return false;
+    }
+    outputLocation.mkdirs();
+    if (!outputLocation.canWrite()) {
+      LOGGER.error("cannot write to specified output location: '{}'",
+          outputLocation.getAbsolutePath());
+      return false;
     }
 
-    /**
-     * Starts extraction by dispatching a new ExtractionFileHandler thread.
-     *
-     * @throws IOException If an error occurs during pre-processing of the files.
+    JacksonJsonProvider reader = new JacksonJsonProvider();
+    this.context = reader.toObject(jobFile, IngestConfig.class);
+
+    /* Check if context could be read and an input path was specified. */
+    if (context == null || this.context.inputPath() == null)
+      return false;
+    Path jobDirectory = jobFile.getAbsoluteFile().toPath().getParent();
+    Path path = jobDirectory.resolve(this.context.inputPath()).normalize().toAbsolutePath();
+
+    /*
+     * Recursively add all files under that path to the List of files that should be processed. Uses
+     * the context-provider to determine the depth of recursion, skip files and limit the number of
+     * files.
      */
-    public void start() throws IOException {
-        if (this.fileHandlerThread == null) {
-            MediaType sourceType = this.context.sourceType();
-            switch (sourceType) {
-                case IMAGE:
-                    this.fileHandlerThread = new Thread(new ImageExtractionFileHandler(this.paths, this.context));
-                    break;
-                case VIDEO:
-                    this.fileHandlerThread = new Thread(new VideoExtractionFileHandler(this.paths, this.context));
-                    break;
-                case AUDIO:
-                    this.fileHandlerThread = new Thread(new AudioExtractionFileHandler(this.paths, this.context));
-                    break;
-                default:
-                    break;
-            }
-            if (fileHandlerThread != null) {
-                this.fileHandlerThread.setName("extraction-file-handler-thread");
-                this.fileHandlerThread.start();
-            }
-        } else {
-            LOGGER.warn("You cannot start the current instance of ExtractionDispatcher again!");
-        }
+    if (!Files.exists(path)) {
+      LOGGER.warn("The path '{}' specified in the extraction configuration does not exist!",
+          path.toString());
+      return false;
     }
+    this.paths = Files.walk(path, this.context.depth(), FileVisitOption.FOLLOW_LINKS).filter(p -> {
+      try {
+        return Files.exists(p) && Files.isRegularFile(p) && !Files.isHidden(p)
+            && Files.isReadable(p);
+      } catch (IOException e) {
+        LOGGER.error("An IO exception occurred while testing the media file at '{}'.", p.toString(),
+            LogHelper.getStackTrace(e));
+        return false;
+      }
+    }).iterator();
+    return true;
+  }
+
+  /**
+   * Starts extraction by dispatching a new ExtractionFileHandler thread.
+   *
+   * @throws IOException
+   *           If an error occurs during pre-processing of the files.
+   */
+  public void start() throws IOException {
+    if (this.fileHandlerThread == null) {
+      MediaType sourceType = this.context.sourceType();
+      switch (sourceType) {
+      case IMAGE:
+        this.fileHandlerThread = new Thread(
+            new ImageExtractionFileHandler(this.paths, this.context));
+        break;
+      case VIDEO:
+        this.fileHandlerThread = new Thread(
+            new VideoExtractionFileHandler(this.paths, this.context));
+        break;
+      case AUDIO:
+        this.fileHandlerThread = new Thread(
+            new AudioExtractionFileHandler(this.paths, this.context));
+        break;
+      case MODEL3D:
+        this.fileHandlerThread = new Thread(
+            new Model3DExtractionFileHandler(this.paths, this.context));
+        break;
+      default:
+        break;
+      }
+      if (fileHandlerThread != null) {
+        this.fileHandlerThread.setName("extraction-file-handler-thread");
+        this.fileHandlerThread.start();
+      }
+    } else {
+      LOGGER.warn("You cannot start the current instance of ExtractionDispatcher again!");
+    }
+  }
 }
