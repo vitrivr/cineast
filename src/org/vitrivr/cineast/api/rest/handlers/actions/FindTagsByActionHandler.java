d60f6e229ccc3dd58496dda169efa5de83fa2227
Ralph Gasser <rg@pontius.ch>
2017-12-07 11:41:03 +0100

    Made some additions to the FindTagsByActionHandler and the TagHandler in order to support matching queries.

--- src/org/vitrivr/cineast/api/rest/handlers/actions/FindTagsByActionHandler.java
+++ src/org/vitrivr/cineast/api/rest/handlers/actions/FindTagsByActionHandler.java
@@ -2,6 +2,7 @@ package org.vitrivr.cineast.api.rest.handlers.actions;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
@@ -13,60 +14,73 @@ import org.vitrivr.cineast.core.data.tag.Tag;
 import org.vitrivr.cineast.core.db.dao.TagHandler;
 
 public class FindTagsByActionHandler extends ParsingActionHandler<IdList> {
+    /** The {@link TagHandler} instance used for lookup of {@link Tag}s. */
+    private static final TagHandler TAG_HANDLER = new TagHandler();
 
-  private static TagHandler tagHandler = new TagHandler();
-  private static final Logger LOGGER = LogManager.getLogger();
+    /** */
+    private static final Logger LOGGER = LogManager.getLogger();
 
-  
-  private static final  String ATTRIBUTE_NAME = ":attribute";
-  private static final String VALUE_NAME = ":value";
-  
-  
-  
-  private final boolean post;
-  
-  public FindTagsByActionHandler(boolean post){
-    this.post = post;
-  }
-  
-  @Override
-  public Object invoke(IdList context, Map<String, String> parameters)
-      throws ActionHandlerException {
+    /** Name of the GET parameter that denotes the attribute that should be used for lookup. */
+    private static final String GET_PARAMETER_ATTRIBUTE = ":attribute";
 
-    if(post){
-      
-      if(context == null || context.getIds().length == 0){
-        return Collections.emptyList();
-      }
-      
-      return tagHandler.getTagsById(context.getIds());
-      
-    }else{
-      String attribute = parameters.get(ATTRIBUTE_NAME);
-      String value = parameters.get(VALUE_NAME);
-      
-      ArrayList<Tag> list = new ArrayList<>();
-      
-      switch(attribute.toLowerCase()){
-      case "id":{
-        list.add(tagHandler.getTagById(value));
-        break;
-      }
-      case "name":{
-        list.addAll(tagHandler.getTagsByName(value));
-        break;
-      }
-      default:{
-        LOGGER.error("Unknown attribute '{}' in FindTagsByActionHandler", attribute);
-      }
-      }
-      return list;
+    /** Name of the GET parameter that denotes the value that should be used for lookup. */
+    private static final String GET_PARAMETER_VALUE = ":value";
+
+    /** Supported field names (attributes). */
+    private static final String FIELD_ID = "id";
+    private static final String FIELD_NAME = "name";
+    private static final String FIELD_MATCHING = "matchingname";
+
+    /** Indicates whether the HTTP request is a POST request. If false, GET is assumed. */
+    private final boolean post;
+
+    /**
+     * Constructor for {@link FindTagsByActionHandler}.
+     *
+     * @param post True if request is POST request. Otherwise false.
+     */
+    public FindTagsByActionHandler(boolean post) {
+        this.post = post;
     }
-  }
 
-  @Override
-  public Class<IdList> inClass() {
-    return IdList.class;
-  }
+    /**
+     * Performs the lookup of {@link Tag}s in the system.
+     *
+     * TODO: Check if IdList etc. is still required.
+     *
+     * @param context Object that is handed to the invocation, usually parsed from the request body. May be NULL!
+     * @param parameters Map containing named parameters in the URL.
+     * @return
+     * @throws ActionHandlerException
+     */
+    @Override
+    public List<Tag> invoke(IdList context, Map<String, String> parameters) throws ActionHandlerException {
+        if (post) {
+            if (context == null || context.getIds().length == 0) {
+                return Collections.emptyList();
+            }
+            return TAG_HANDLER.getTagsById(context.getIds());
+        } else {
+            final String attribute = parameters.get(GET_PARAMETER_ATTRIBUTE);
+            final String value = parameters.get(GET_PARAMETER_VALUE);
+            switch (attribute.toLowerCase()) {
+                case FIELD_ID:
+                    final List<Tag> list = new ArrayList<>(1);
+                    list.add(TAG_HANDLER.getTagById(value));
+                    return list;
+                case FIELD_NAME:
+                    return TAG_HANDLER.getTagsByName(value);
+                case FIELD_MATCHING:
+                    return TAG_HANDLER.getTagsByMatchingName(value);
+                default:
+                    LOGGER.error("Unknown attribute '{}' in FindTagsByActionHandler", attribute);
+                    return new ArrayList<>(0);
+            }
+        }
+    }
 
+    @Override
+    public Class<IdList> inClass() {
+        return IdList.class;
+    }
 }
