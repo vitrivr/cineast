06ac4ee4717bd403e7aaee7ea99b1acd38f818ec
Ralph Gasser <rg@pontius.ch>
2018-01-04 18:26:34 +0100

    Added a handle to query neighbouring segments through the WebSocket API.

---
+++ src/org/vitrivr/cineast/core/data/messages/query/NeighboringSegmentQuery.java
@@ -0,0 +1,58 @@
+package org.vitrivr.cineast.core.data.messages.query;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+import org.vitrivr.cineast.core.config.QueryConfig;
+import org.vitrivr.cineast.core.config.ReadableQueryConfig;
+import org.vitrivr.cineast.core.data.MediaType;
+import org.vitrivr.cineast.core.data.messages.interfaces.Message;
+import org.vitrivr.cineast.core.data.messages.interfaces.MessageType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NeighboringSegmentQuery extends Query {
+
+    /** ID of the {@link org.vitrivr.cineast.core.data.entities.SegmentDescriptor} for which neighbors should be retrieved. */
+    private final String segmentId;
+
+    /** Number of neighbors that should be retrieved. */
+    private final int count;
+
+    /** */
+    @JsonCreator
+    public NeighboringSegmentQuery(@JsonProperty(value = "segmentId", required = true) String segmentId,
+                                   @JsonProperty(value = "count", required = false) Integer count,
+                                   @JsonProperty(value = "config", required = false) QueryConfig config) {
+        super(config);
+        this.segmentId = segmentId;
+        this.count = count == null ? 3 : count;
+    }
+
+    /**
+     * Returns a list of segment ID's that can be used for lookup to satisfy this query.
+     *
+     * @return List of segment IDs that satisfy this {@link NeighboringSegmentQuery}.
+     */
+    public List<String> getSegmentIds() {
+        final Pair<String,Long> decomposed = MediaType.parsesSegmentId(this.segmentId);
+        final long segmentNumber = decomposed.getRight();
+        final long start = Math.max(0, segmentNumber - this.count);
+        final long end = Math.max(0, segmentNumber + this.count);
+        final ArrayList<String> list = new ArrayList<>((int)(end-start));
+        for (long i=start; i<=end; i++) {
+            if (i != segmentNumber) {
+                list.add(MediaType.generateSegmentId(decomposed.getLeft(), i));
+            }
+        }
+        return list;
+    }
+
+
+    @Override
+    public MessageType getMessageType() {
+        return null;
+    }
+}
