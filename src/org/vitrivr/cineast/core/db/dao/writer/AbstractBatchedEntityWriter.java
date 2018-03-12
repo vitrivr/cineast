e4eca192c3d795014a29e1459114a754901a05ef
Ralph Gasser <rg@pontius.ch>
2018-03-12 11:18:44 +0100

    Optimized the AbstractBatchedEntityWriter.

--- src/org/vitrivr/cineast/core/db/dao/writer/AbstractBatchedEntityWriter.java
+++ src/org/vitrivr/cineast/core/db/dao/writer/AbstractBatchedEntityWriter.java
@@ -1,9 +1,9 @@
 package org.vitrivr.cineast.core.db.dao.writer;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 import org.vitrivr.cineast.core.db.PersistencyWriter;
 import org.vitrivr.cineast.core.db.PersistentTuple;
@@ -14,119 +14,90 @@ import org.vitrivr.cineast.core.db.PersistentTuple;
  * @created 27.01.17
  */
 public abstract class AbstractBatchedEntityWriter<T> implements AutoCloseable {
-  /** */
-  private final ArrayDeque<PersistentTuple> buffer;
+    /** The {@link Queue} used to store {@link PersistentTuple}s until they are flushed to disk. */
+    private final ArrayBlockingQueue<PersistentTuple> buffer;
 
-  /** */
-  private final int batchsize;
+    /** {@link PersistencyWriter} instance used to persist changes to the underlying persistence layer. */
+    protected PersistencyWriter<?> writer;
 
-  /** PersistencyWriter instance used to persist changes. */
-  protected PersistencyWriter<?> writer;
-
-  /**
-   *
-   * @param writer
-   * @param batchsize
-   */
-  protected AbstractBatchedEntityWriter(PersistencyWriter<?> writer, int batchsize, boolean init) {
-    this.buffer = new ArrayDeque<>(batchsize);
-    this.batchsize = batchsize;
-    this.writer = writer;
-    if (init) {
-      this.init();
-    }
-  }
-
-  /**
-   *
-   */
-  protected abstract void init();
-
-  /**
-   *
-   * @param entity
-   * @return
-   */
-  protected abstract PersistentTuple generateTuple(T entity);
-
-
-  /**
-   * Persists the provided entity descriptor in the database.
-   *
-   * @param entity Entity descriptor to persist
-   */
-  public void write(T entity) {
-    PersistentTuple tuple = this.generateTuple(entity);
-    if (this.batchsize == 1) {
-      this.writeSingle(tuple);
-    } else {
-      this.writeBatched(tuple);
-    }
-  }
-
-  /**
-   *
-   * @param entity
-   */
-  public void write(List<T> entity) {
-    if (entity.size() < this.batchsize) {
-      List<PersistentTuple> tuples = entity.stream().map(this::generateTuple)
-          .collect(Collectors.toList());
-      this.writer.persist(tuples);
-    } else {
-      entity.stream().map(this::generateTuple).forEach(this::writeBatched);
-    }
-  }
-
-  /**
-   *
-   */
-  public final void flush() {
-    List<PersistentTuple> batch = new ArrayList<>(buffer.size());
-    PersistentTuple t = null;
-    while ((t = this.buffer.poll()) != null) {
-      batch.add(t);
-    }
-    this.writer.persist(batch);
-  }
-
-  /**
-   *
-   * @param tuple
-   */
-  private void writeSingle(PersistentTuple tuple) {
-    this.writer.persist(tuple);
-  }
-
-  /**
-   *
-   * @param tuple
-   */
-  private void writeBatched(PersistentTuple tuple) {
-    this.buffer.offer(tuple);
-    if (buffer.size() >= this.batchsize) {
-      this.flush();
-    }
-  }
-
-  /**
-   * Flushes the buffer and closes the writer.
-   */
-  @Override
-  public final void close() {
-    if (this.buffer.size() > 0) {
-      this.flush();
+    /**
+     * @param writer
+     * @param batchsize
+     */
+    protected AbstractBatchedEntityWriter(PersistencyWriter<?> writer, int batchsize, boolean init) {
+        this.buffer = new ArrayBlockingQueue<>(batchsize);
+        this.writer = writer;
+        if (init) {
+            this.init();
+        }
     }
 
-    if (this.writer != null) {
-      this.writer.close();
-      this.writer = null;
-    }
-  }
+    /**
+     *
+     */
+    protected abstract void init();
 
-  @Override
-  public void finalize() throws Throwable {
-    this.close();
-    super.finalize();
-  }
+    /**
+     * @param entity
+     * @return
+     */
+    protected abstract PersistentTuple generateTuple(T entity);
+
+    /**
+     * Persists the provided entity by first converting it to a {@link PersistentTuple} and subsequently writing that tuple to the local buffer.
+     * If the buffer is full, i.e. the batch size was reached, then buffer is flushed first.
+     *
+     * @param entity The entity that should be persisted.
+     */
+    public void write(T entity) {
+        final PersistentTuple tuple = this.generateTuple(entity);
+        if (this.buffer.remainingCapacity() == 0) {
+            this.flush();
+        }
+        this.buffer.offer(tuple);
+    }
+
+    /**
+     * @param entity
+     */
+    public void write(List<T> entity) {
+        entity.forEach(this::write);
+    }
+
+    /**
+     * Drains the content of the buffer and writes it to the underlying persistence layer using the local {@link PersistencyWriter} instance.
+     */
+    public final void flush() {
+        final List<PersistentTuple> batch = new ArrayList<>(buffer.size());
+        this.buffer.drainTo(batch);
+        this.writer.persist(batch);
+    }
+
+    /**
+     * Flushes the buffer and closes the local {@link PersistencyWriter}.
+     */
+    @Override
+    public final void close() {
+        if (this.writer != null) {
+            if (this.buffer.size() > 0) {
+                this.flush();
+            }
+
+            if (this.writer != null) {
+                this.writer.close();
+                this.writer = null;
+            }
+        }
+    }
+
+    /**
+     * Closes the {@link AbstractBatchedEntityWriter} upon finalization.
+     *
+     * @throws Throwable If finalization goes wrong.
+     */
+    @Override
+    public void finalize() throws Throwable {
+        this.close();
+        super.finalize();
+    }
 }
