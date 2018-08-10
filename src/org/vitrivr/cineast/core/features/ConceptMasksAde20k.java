c41852d3c81e61c6c78d29eb39f5406c66763d63
Luca Rossetto <luca.rossetto@unibas.ch>
2018-08-10 17:41:54 +0200

    Added smaller version of ConceptMasks feature module

---
+++ src/org/vitrivr/cineast/core/features/ConceptMasksAde20k.java
@@ -0,0 +1,88 @@
+package org.vitrivr.cineast.core.features;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import org.tensorflow.Tensor;
+import org.tensorflow.types.UInt8;
+import org.vitrivr.cineast.core.config.ReadableQueryConfig;
+import org.vitrivr.cineast.core.data.CorrespondenceFunction;
+import org.vitrivr.cineast.core.data.FloatVectorImpl;
+import org.vitrivr.cineast.core.data.frames.VideoFrame;
+import org.vitrivr.cineast.core.data.score.ScoreElement;
+import org.vitrivr.cineast.core.data.segments.SegmentContainer;
+import org.vitrivr.cineast.core.features.abstracts.AbstractFeatureModule;
+import org.vitrivr.cineast.core.features.neuralnet.tf.models.deeplab.DeepLab;
+import org.vitrivr.cineast.core.features.neuralnet.tf.models.deeplab.DeepLabAde20k;
+import org.vitrivr.cineast.core.features.neuralnet.tf.models.deeplab.DeepLabCityscapes;
+import org.vitrivr.cineast.core.features.neuralnet.tf.models.deeplab.DeepLabLabel;
+import org.vitrivr.cineast.core.features.neuralnet.tf.models.deeplab.DeepLabPascalVoc;
+import org.vitrivr.cineast.core.util.GridPartitioner;
+
+public class ConceptMasksAde20k extends AbstractFeatureModule {
+
+  private static final int GRID_PARTITIONS = 16;
+
+  private final DeepLab ade20k;
+
+  public ConceptMasksAde20k() {
+    super("features_conceptmasksade20k", 1);
+    this.correspondence = CorrespondenceFunction.hyperbolic(10); //TODO determine distance
+    this.ade20k = new DeepLabAde20k();
+
+  }
+
+  private static List<DeepLabLabel> linearize(DeepLabLabel[][] labels) {
+    ArrayList<DeepLabLabel> list = new ArrayList<>(labels.length * labels[0].length);
+    for (int i = 0; i < labels.length; ++i) {
+      for (int j = 0; j < labels[0].length; ++j) {
+        list.add(labels[i][j]);
+      }
+    }
+    return list;
+  }
+
+  @Override
+  public synchronized void processSegment(SegmentContainer shot) {
+
+    if (shot == null || shot.getMostRepresentativeFrame() == null
+        || shot.getMostRepresentativeFrame().getImage() == null
+        || shot.getMostRepresentativeFrame().getImage() == VideoFrame.EMPTY_VIDEO_FRAME) {
+      return;
+    }
+
+    Tensor<UInt8> inputTensor = DeepLab
+        .prepareImage(shot.getMostRepresentativeFrame().getImage().getBufferedImage());
+
+    int[][] tmp = this.ade20k.processImage(inputTensor);
+
+    List<DeepLabLabel> ade20kLabels = linearize(
+        DeepLabLabel.fromAde20kId(tmp));
+
+    inputTensor.close();
+
+    ArrayList<LinkedList<DeepLabLabel>> ade20kPartitions = GridPartitioner
+        .partition(ade20kLabels, tmp.length, tmp[0].length, GRID_PARTITIONS, GRID_PARTITIONS);
+
+
+
+    float[] vector = new float[2 * GRID_PARTITIONS * GRID_PARTITIONS];
+
+    for (int i = 0; i < GRID_PARTITIONS * GRID_PARTITIONS; ++i) {
+      DeepLabLabel dominantLabel = DeepLabLabel.getDominantLabel(ade20kPartitions.get(i));
+      vector[2 * i] = dominantLabel.getEmbeddX();
+      vector[2 * i + 1] = dominantLabel.getEmbeddY();
+
+    }
+
+    persist(shot.getId(), new FloatVectorImpl(vector));
+  }
+
+  @Override
+  public List<ScoreElement> getSimilar(SegmentContainer sc, ReadableQueryConfig qc) {
+    //TODO
+    return Collections.emptyList();
+  }
+
+}
