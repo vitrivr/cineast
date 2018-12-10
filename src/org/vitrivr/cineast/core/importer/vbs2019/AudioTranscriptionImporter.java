bfddb700f98f72043efd5d3b133624d2e8773705
Silvan Heller <silvan.heller@unibas.ch>
2018-12-10 16:03:54 +0100

    adjusting import to new format

--- src/org/vitrivr/cineast/core/importer/vbs2019/AudioTranscriptionImporter.java
+++ src/org/vitrivr/cineast/core/importer/vbs2019/AudioTranscriptionImporter.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.IOException;
+import java.io.StreamTokenizer;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -22,17 +23,19 @@ import org.vitrivr.cineast.core.importer.Importer;
 
 public class AudioTranscriptionImporter implements Importer<Pair<String, String>> {
 
-  private final String objectID;
-  private final Iterator<Entry<String, JsonNode>> elements;
   private static final Logger LOGGER = LogManager.getLogger();
+  private final JsonParser parser;
+  private final ObjectMapper mapper;
+  private Iterator<Entry<String, JsonNode>> elements;
 
   public AudioTranscriptionImporter(Path input) throws IOException {
-    objectID = input.getFileName().toString().replace(".json", "");
-    ObjectMapper mapper = new ObjectMapper();
-    JsonParser parser = mapper.getFactory().createParser(input.toFile());
-    if (parser.nextToken() == JsonToken.START_OBJECT) {
-      ObjectNode node = mapper.readTree(parser);
-      elements = node.fields();
+    mapper = new ObjectMapper();
+    parser = mapper.getFactory().createParser(input.toFile());
+    if (parser.nextToken() == JsonToken.START_ARRAY) {
+      if (parser.nextToken() == JsonToken.START_OBJECT) {
+        ObjectNode node = mapper.readTree(parser);
+        elements = node.fields();
+      }
       if (elements == null) {
         throw new IOException("Empty file");
       }
@@ -43,7 +46,23 @@ public class AudioTranscriptionImporter implements Importer<Pair<String, String>
 
   private synchronized Optional<Pair<String, String>> nextPair() {
     if (!elements.hasNext()) {
-      return Optional.empty();
+      do {
+        try {
+          if (parser.nextToken() == JsonToken.START_OBJECT) {
+            ObjectNode nextMovie = mapper.readTree(parser);
+            if (nextMovie == null) {
+              LOGGER.info("Audiofile is done");
+              return Optional.empty();
+            }
+            elements = nextMovie.fields();
+          } else {
+            LOGGER.error("File done");
+            return Optional.empty();
+          }
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      } while (!elements.hasNext());
     }
     Entry<String, JsonNode> next = elements.next();
     return Optional.of(new Pair<>(next.getKey(), next.getValue().asText()));
@@ -61,8 +80,7 @@ public class AudioTranscriptionImporter implements Importer<Pair<String, String>
   @Override
   public Map<String, PrimitiveTypeProvider> convert(Pair<String, String> data) {
     final HashMap<String, PrimitiveTypeProvider> map = new HashMap<>(2);
-    String id = "v_" + objectID + "_" + data.first;
-    map.put(SimpleFulltextFeatureDescriptor.FIELDNAMES[0], PrimitiveTypeProvider.fromObject(id));
+    map.put(SimpleFulltextFeatureDescriptor.FIELDNAMES[0], PrimitiveTypeProvider.fromObject(data.first));
     map.put(SimpleFulltextFeatureDescriptor.FIELDNAMES[1], PrimitiveTypeProvider.fromObject(data.second));
     return map;
   }
