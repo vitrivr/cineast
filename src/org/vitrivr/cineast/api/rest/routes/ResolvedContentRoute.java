9d7d9ec51646ece7072ef39a73478e3666b44c76
Luca Rossetto <luca.rossetto@unibas.ch>
2019-03-27 10:57:18 +0100

    Changed ResolvedContentRoute to behave more like spark static content serving logic

--- src/org/vitrivr/cineast/api/rest/routes/ResolvedContentRoute.java
+++ src/org/vitrivr/cineast/api/rest/routes/ResolvedContentRoute.java
@@ -1,25 +1,21 @@
 package org.vitrivr.cineast.api.rest.routes;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.channels.WritableByteChannel;
 import java.util.Map;
 import org.vitrivr.cineast.api.rest.resolvers.ResolutionResult;
 import org.vitrivr.cineast.api.rest.resolvers.Resolver;
 import spark.Request;
 import spark.Response;
 import spark.Route;
-
+import spark.utils.GzipUtils;
+import spark.utils.IOUtils;
 
 public class ResolvedContentRoute implements Route {
 
   private Resolver resolver;
 
-  public ResolvedContentRoute(Resolver resolver){
+  public ResolvedContentRoute(Resolver resolver) {
     this.resolver = resolver;
   }
 
@@ -28,53 +24,36 @@ public class ResolvedContentRoute implements Route {
 
     Map<String, String> params = request.params();
 
-    String id = null;
+    String id;
 
-    if (params != null && params.containsKey(":id")){
+    if (params != null && params.containsKey(":id")) {
       id = params.get(":id");
-    }else{
-      response.status(404);
+    } else {
+      response.status(400);
+      response.raw().getWriter().write("Bad request");
+      response.raw().getWriter().flush();
       return 404;
     }
 
     ResolutionResult rresult = this.resolver.resolve(id);
 
-    if(rresult == null){
-      response.status(404);
+    if (rresult == null) {
+      response.status(400);
+      response.raw().getWriter().write("Bad request");
+      response.raw().getWriter().flush();
       return 404;
     }
 
-
     response.type(rresult.mimeType);
-    response.header("Transfer-Encoding", "identity");
-    InputStream inStream = rresult.stream;
 
-    OutputStream out = response.raw().getOutputStream();
-
-    final ReadableByteChannel inputChannel = Channels.newChannel(inStream);
-    final WritableByteChannel outputChannel = Channels.newChannel(out);
-
-    fastCopy(inputChannel, outputChannel);
-
-    out.flush();
-
-    return null;
-  }
-
-  public static void fastCopy(final ReadableByteChannel src, final WritableByteChannel dest) throws IOException {
-    final ByteBuffer buffer = ByteBuffer.allocateDirect(16 * 1024);
-
-    while(src.read(buffer) != -1) {
-      buffer.flip();
-      dest.write(buffer);
-      buffer.compact();
+    try (InputStream inputStream = rresult.stream;
+        OutputStream wrappedOutputStream =
+            GzipUtils.checkAndWrap(request.raw(), response.raw(), false)) {
+      IOUtils.copy(inputStream, wrappedOutputStream);
+      wrappedOutputStream.flush();
+      response.raw().getOutputStream().close();
     }
 
-    buffer.flip();
-
-    while(buffer.hasRemaining()) {
-      dest.write(buffer);
-    }
-
+    return 200;
   }
 }
