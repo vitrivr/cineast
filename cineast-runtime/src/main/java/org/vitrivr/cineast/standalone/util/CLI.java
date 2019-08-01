abe36b283a0f02d475235fdec16cd3628e47a0ea
Luca Rossetto <luca.rossetto@unibas.ch>
2019-08-01 23:27:01 +0200

    Added first rudimentary JLine3-based CLI

--- cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/util/CLI.java
+++ cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/util/CLI.java
@@ -1,8 +1,16 @@
 package org.vitrivr.cineast.standalone.util;
 
 import com.github.rvesse.airline.parser.errors.ParseRestrictionViolatedException;
+import org.jline.builtins.Completers;
+import org.jline.reader.Completer;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.impl.completer.AggregateCompleter;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
+import java.io.IOException;
 import java.util.NoSuchElementException;
-import java.util.Scanner;
 
 /**
  * Helper class that can be used to start an interactive CLI.
@@ -12,21 +20,45 @@ import java.util.Scanner;
  */
 public class CLI {
 
+    private static final String PROMPT = "cineast> ";
+
     private CLI() {}
 
     /**
      * Starts the interactive CLI. This is method will block.
      */
     public static void start(Class<?> cliClass) {
-        final Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the interactive Cineast CLI.");
+
+        Terminal terminal = null;
+        try {
+            terminal = TerminalBuilder.terminal(); //basic terminal
+        } catch (IOException e) {
+            System.err.println("Could not initialize Terminal: ");
+            System.err.println(e.getMessage());
+            System.err.println("Exiting...");
+            System.exit(-1);
+        }
+
+        Completer completer = new AggregateCompleter(
+            new Completers.FileNameCompleter() //TODO figure out a way to build a completer based on airline commands
+        );
+
+        LineReader lineReader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .completer(completer)
+                .build();
+
+
+        terminal.writer().println("Welcome to the interactive Cineast CLI.");
+
         try {
             while (true) {
-                System.out.println("Please enter a command...");
-                final String line = scanner.nextLine();
+
+                final String line = lineReader.readLine(PROMPT);
                 if (line.toLowerCase().equals("exit") || line.toLowerCase().equals("quit") ) {
                     break;
                 }
+
 
                 /* Try to parse user input. */
                 try {
