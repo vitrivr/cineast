5c8621e4fca7d75424195bd1e9c3dfba373e6387
Luca Rossetto <luca.rossetto@unibas.ch>
2019-08-05 17:02:05 +0200

    Parameter parsing in cli now respects quotation marks

--- cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/util/CLI.java
+++ cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/util/CLI.java
@@ -14,8 +14,11 @@ import org.jline.utils.AttributedStringBuilder;
 import org.jline.utils.AttributedStyle;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
@@ -66,6 +69,7 @@ public class CLI {
 
         terminal.writer().println("Welcome to the interactive Cineast CLI.");
 
+
         try {
             while (true) {
 
@@ -76,8 +80,7 @@ public class CLI {
 
                 /* Try to parse user input. */
                 try {
-                    //cli = new com.github.rvesse.airline.Cli<>(cliClass);
-                    final Runnable command = cli.parse(line.split(" "));
+                    final Runnable command = cli.parse(CLI.splitLine(line));
                     command.run();
                 } catch (ParseRestrictionViolatedException e) {
                     terminal.writer().println(
@@ -94,5 +97,29 @@ public class CLI {
         } catch (IllegalStateException | NoSuchElementException e) {
             System.out.println("System.in was closed; exiting");
         }
+    }
+
+    final static Pattern lineSplitRegex = Pattern.compile("[^\\s\"']+|\"([^\"]*)\"|'([^']*)'");
+
+    //based on https://stackoverflow.com/questions/366202/regex-for-splitting-a-string-using-space-when-not-surrounded-by-single-or-double/366532
+    private static String[] splitLine(String line){
+        if (line == null || line.isEmpty()){
+            return new String[0];
+        }
+        List<String> matchList = new ArrayList<String>();
+        Matcher regexMatcher = lineSplitRegex.matcher(line);
+        while (regexMatcher.find()) {
+            if (regexMatcher.group(1) != null) {
+                // Add double-quoted string without the quotes
+                matchList.add(regexMatcher.group(1));
+            } else if (regexMatcher.group(2) != null) {
+                // Add single-quoted string without the quotes
+                matchList.add(regexMatcher.group(2));
+            } else {
+                // Add unquoted word
+                matchList.add(regexMatcher.group());
+            }
+        }
+        return matchList.toArray(new String[matchList.size()]);
     }
 }
