afc9e7c978e212b187f8220c71e407d6986b0bfd
Ralph Gasser <ralph.gasser@unibas.ch>
2019-08-07 15:51:47 +0200

    Minor fix.

--- cineast-api/src/main/java/org/vitrivr/cineast/api/Main.java
+++ cineast-api/src/main/java/org/vitrivr/cineast/api/Main.java
@@ -4,8 +4,6 @@ import org.vitrivr.cineast.standalone.cli.CineastCli;
 import org.vitrivr.cineast.standalone.config.Config;
 import org.vitrivr.cineast.standalone.util.CLI;
 
-import java.util.Arrays;
-
 public class Main {
     /**
      * Entrypoint for Cineast API application.
@@ -14,14 +12,14 @@ public class Main {
      */
     public static void main(String[] args) {
         /* (Force) load application config. */
-        if (args.length < 2) {
-            System.err.println("Starting Cineast requires at least two arguments: the path to the configuration file and a command (cineast <config> <command>. Cineast will shutdown...");
+        if (args.length < 1) {
+            System.err.println("Starting Cineast requires least one argument: the path to the configuration. Cineast API will shutdown...");
             System.exit(1);
         }
 
         /* (Force) load application config. */
         if (Config.loadConfig(args[0]) == null) {
-            System.err.println("Failed to load Cineast configuration from '" + args[0] + "'. Cineast will shutdown...");
+            System.err.println("Failed to load Cineast configuration from '" + args[0] + "'. Cineast API will shutdown...");
             System.exit(1);
         }
 
