e3049efbab1748cb8efaac959983d031d853afc3
Silvan Heller <silvan.heller@unibas.ch>
2019-08-12 17:19:35 +0200

    Asking for objectid if none is specified

--- cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/cli/MetadataCommand.java
+++ cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/cli/MetadataCommand.java
@@ -2,17 +2,14 @@ package org.vitrivr.cineast.standalone.cli;
 
 import com.github.rvesse.airline.annotations.Command;
 import com.github.rvesse.airline.annotations.Option;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import org.vitrivr.cineast.core.config.DatabaseConfig;
 import org.vitrivr.cineast.core.data.entities.MediaObjectMetadataDescriptor;
 import org.vitrivr.cineast.core.db.dao.reader.MediaObjectMetadataReader;
-
 import org.vitrivr.cineast.standalone.config.Config;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A CLI command that can be used to lookup metadata for one or many media objects.
@@ -22,10 +19,16 @@ import java.util.List;
  */
 @Command(name = "metadata", description = "Performs a metadata lookup for the media objects specified.")
 public class MetadataCommand implements Runnable {
-    @Option(name = { "-o", "--objectid" }, title = "Object ID", description = "The ID of the object to lookup metadata for.")
+
+    @Option(name = {"-o", "--objectid"}, title = "Object ID", description = "The ID of the object to lookup metadata for.")
     private String objectId;
 
     public void run() {
+        if (this.objectId == null) {
+            System.out.println("No objectid specified, please enter one: ");
+            Scanner in = new Scanner(System.in);
+            this.objectId = in.nextLine();
+        }
         final List<String> objectIds = new ArrayList<>(1);
         objectIds.add(this.objectId);
         final DatabaseConfig config = Config.sharedConfig().getDatabase();
