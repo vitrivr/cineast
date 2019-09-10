b80a9235a40268d773e403a820a9c949f843287e
Luca Rossetto <rossetto@ifi.uzh.ch>
2019-09-10 17:07:58 +0200

    Some very first functionality

--- cineast-api/src/main/java/org/vitrivr/cineast/api/grpc/CineastQueryService.java
+++ cineast-api/src/main/java/org/vitrivr/cineast/api/grpc/CineastQueryService.java
@@ -1,12 +1,31 @@
 package org.vitrivr.cineast.api.grpc;
 
 import io.grpc.stub.StreamObserver;
+import org.vitrivr.cineast.api.grpc.util.MediaObjectUtil;
+import org.vitrivr.cineast.core.data.entities.MediaObjectDescriptor;
+import org.vitrivr.cineast.core.db.dao.reader.MediaObjectReader;
+import org.vitrivr.cineast.standalone.config.Config;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CineastQueryService extends CineastQueryGrpc.CineastQueryImplBase {
 
     @Override
     public void getMediaObjects(CineastGrpc.MediaObjectIdList request, StreamObserver<CineastGrpc.MediaObjectQueryResult> responseObserver) {
-        //TODO
+
+        Set<String> ids = request.getIdsList().stream().map(CineastGrpc.MediaObjectId::getId).collect(Collectors.toSet());
+
+        MediaObjectReader reader = new MediaObjectReader(Config.sharedConfig().getDatabase().getSelectorSupplier().get());
+
+        Map<String, MediaObjectDescriptor> objects = reader.lookUpObjects(ids);
+
+        CineastGrpc.MediaObjectQueryResult result = CineastGrpc.MediaObjectQueryResult.newBuilder().addAllObjects(
+                objects.values().stream().map(MediaObjectUtil::fromMediaObjectDescriptor).collect(Collectors.toList())
+        ).build();
+
+        responseObserver.onNext(result);
         responseObserver.onCompleted();
     }
 
