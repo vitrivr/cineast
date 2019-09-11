fc26215a2cf63fdea7cbaba8eeba58bf91d6da68
Luca Rossetto <rossetto@ifi.uzh.ch>
2019-09-11 16:57:51 +0200

    Added segment lookup functionality

--- cineast-api/src/main/java/org/vitrivr/cineast/api/grpc/CineastQueryService.java
+++ cineast-api/src/main/java/org/vitrivr/cineast/api/grpc/CineastQueryService.java
@@ -2,8 +2,11 @@ package org.vitrivr.cineast.api.grpc;
 
 import io.grpc.stub.StreamObserver;
 import org.vitrivr.cineast.api.grpc.util.MediaObjectUtil;
+import org.vitrivr.cineast.api.grpc.util.MediaSegmentUtil;
 import org.vitrivr.cineast.core.data.entities.MediaObjectDescriptor;
+import org.vitrivr.cineast.core.data.entities.MediaSegmentDescriptor;
 import org.vitrivr.cineast.core.db.dao.reader.MediaObjectReader;
+import org.vitrivr.cineast.core.db.dao.reader.MediaSegmentReader;
 import org.vitrivr.cineast.standalone.config.Config;
 
 import java.util.Map;
@@ -16,9 +19,7 @@ public class CineastQueryService extends CineastQueryGrpc.CineastQueryImplBase {
     public void getMediaObjects(CineastGrpc.MediaObjectIdList request, StreamObserver<CineastGrpc.MediaObjectQueryResult> responseObserver) {
 
         Set<String> ids = request.getIdsList().stream().map(CineastGrpc.MediaObjectId::getId).collect(Collectors.toSet());
-
         MediaObjectReader reader = new MediaObjectReader(Config.sharedConfig().getDatabase().getSelectorSupplier().get());
-
         Map<String, MediaObjectDescriptor> objects = reader.lookUpObjects(ids);
 
         CineastGrpc.MediaObjectQueryResult result = CineastGrpc.MediaObjectQueryResult.newBuilder().addAllObjects(
@@ -31,7 +32,16 @@ public class CineastQueryService extends CineastQueryGrpc.CineastQueryImplBase {
 
     @Override
     public void getMediaSegments(CineastGrpc.MediaSegmentIdList request, StreamObserver<CineastGrpc.MediaSegmentQueryResult> responseObserver) {
-        //TODO
+
+        Set<String> ids = request.getIdsList().stream().map(CineastGrpc.MediaSegmentId::getId).collect(Collectors.toSet());
+        MediaSegmentReader reader = new MediaSegmentReader(Config.sharedConfig().getDatabase().getSelectorSupplier().get());
+        Map<String, MediaSegmentDescriptor> segments = reader.lookUpSegments(ids);
+
+        CineastGrpc.MediaSegmentQueryResult result = CineastGrpc.MediaSegmentQueryResult.newBuilder().addAllSegments(
+            segments.values().stream().map(MediaSegmentUtil::fromMediaSegmentDescriptor).collect(Collectors.toList())
+        ).build();
+
+        responseObserver.onNext(result);
         responseObserver.onCompleted();
     }
 
