195bd0d8ac5c87bca9a71313bc7f55cef24cd1e0
Silvan Heller <silvan.heller@unibas.ch>
2019-11-06 13:45:39 +0100

    improving modularity of cli retrieval, adding specific text retrieval test for debugging & testing

--- cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/cli/CineastCli.java
+++ cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/cli/CineastCli.java
@@ -3,7 +3,7 @@ package org.vitrivr.cineast.standalone.cli;
 import com.github.rvesse.airline.annotations.Cli;
 import com.github.rvesse.airline.help.Help;
 
-@Cli(name = "cineast-api", description = "The CLI provided by the Cineast API.", commands = {OptimizeEntitiesCommand.class, CodebookCommand.class, DatabaseSetupCommand.class, EvaluationCommand.class, ExtractionCommand.class, ImportCommand.class, ThreeDeeTestCommand.class, RetrieveCommand.class, MetadataCommand.class, Help.class, SingleObjRetrievalCommand.class})
+@Cli(name = "cineast-api", description = "The CLI provided by the Cineast API.", commands = {OptimizeEntitiesCommand.class, CodebookCommand.class, DatabaseSetupCommand.class, EvaluationCommand.class, ExtractionCommand.class, ImportCommand.class, ThreeDeeTestCommand.class, RetrieveCommand.class, MetadataCommand.class, Help.class, SingleObjRetrievalCommand.class, TextRetrievalTest.class})
 public class CineastCli {
 
 }
