b7d5a976dde362db2953ff0e5861cedcbf173588
Silvan Heller <silvan.heller@unibas.ch>
2019-11-08 18:11:14 +0100

    v2 setup script

--- vbs_setup.sh
+++ vbs_setup.sh
@@ -3,21 +3,29 @@
 ./gradlew cineast-runtime:fatJar
 ./gradlew cineast-api:fatJar
 
-# base-folder
-base = $1
-java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json setup --clean
-java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --type PROTO --input $base/extracted_combined/ --threads 5
-java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --type VBS2020 --input $base/text/ --threads 3
-
 restart_cottontail () {
   kill $(pgrep --full cottontail)
   cd ..
   cd cottontaildb
   tmux new-window -d -n cottontaildb "./gradlew run"
   cd ..
+  cd cineast
 }
 
-restart_cottontail()
+# base-folder
+base=$1
+sleep=5
+restart_cottontail
+sleep $sleep
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json setup --clean
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --type PROTO --input $base/extracted_combined/ --threads 5
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --type VBS2020 --input $base/text/ --threads 3
+
+
+
+restart_cottontail
+sleep $sleep
 java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json optimize
-restart_cottontail()
+restart_cottontail
+sleep $sleep
 tmux new-window -d -n cineast "java -jar cineast-runtime/build/libs/cineast-api-2.5-full.jar vbs.json"
