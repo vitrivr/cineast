df39d35199729833eb59d9c6053b9b1563a60052
Silvan Heller <silvan.heller@unibas.ch>
2019-11-11 07:53:38 +0100

    adding logging to file for setupscript

--- vbs_setup.sh
+++ vbs_setup.sh
@@ -18,15 +18,15 @@ threads=$2
 sleep=5
 restart_cottontail
 sleep $sleep
-java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json setup --clean
-java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --type PROTO --input $base/extracted_combined/ --threads $threads
-java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --type VBS2020 --input $base/text/ --threads $threads
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json setup --clean >> vbs_setup.log
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --type PROTO --input $base/extracted_combined/ --threads $threads >> proto_import.log
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --type VBS2020 --input $base/text/ --threads $threads >> text_import.log
 
 
 
 restart_cottontail
 sleep $sleep
-java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json optimize
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json optimize >> vbs_optimize.log
 restart_cottontail
 sleep $sleep
 tmux new-window -d -n cineast "java -jar cineast-runtime/build/libs/cineast-api-2.5-full.jar vbs.json"
