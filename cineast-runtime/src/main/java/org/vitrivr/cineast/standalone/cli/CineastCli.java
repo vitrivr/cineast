469ce1cc22e2097372177750312dfe3c1b5d3b22
Silvan Heller <silvan.heller@unibas.ch>
2019-12-28 12:14:14 +0100

    adding command to drop individual table

--- cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/cli/CineastCli.java
+++ cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/cli/CineastCli.java
@@ -2,8 +2,9 @@ package org.vitrivr.cineast.standalone.cli;
 
 import com.github.rvesse.airline.annotations.Cli;
 import com.github.rvesse.airline.help.Help;
+import org.vitrivr.cineast.standalone.cli.db.DropTableCommand;
 
-@Cli(name = "cineast-api", description = "The CLI provided by the Cineast API.", commands = {CreateIndicesCommand.class, OptimizeEntitiesCommand.class, CodebookCommand.class, DatabaseSetupCommand.class, EvaluationCommand.class, ExtractionCommand.class, ImportCommand.class, ThreeDeeTestCommand.class, RetrieveCommand.class, MetadataCommand.class, Help.class, SingleObjRetrievalCommand.class, TextRetrievalTest.class})
+@Cli(name = "cineast-api", description = "The CLI provided by the Cineast API.", commands = {DropTableCommand.class, CreateIndicesCommand.class, OptimizeEntitiesCommand.class, CodebookCommand.class, DatabaseSetupCommand.class, EvaluationCommand.class, ExtractionCommand.class, ImportCommand.class, ThreeDeeTestCommand.class, RetrieveCommand.class, MetadataCommand.class, Help.class, SingleObjRetrievalCommand.class, TextRetrievalTest.class})
 public class CineastCli {
 
 }
