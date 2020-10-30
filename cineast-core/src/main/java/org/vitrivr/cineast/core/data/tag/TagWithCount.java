17a0fb3a4918603eef00287fe7f9e190c85be6f8
cristinailli <cristina.illi@unibas.ch>
2020-10-30 10:38:30 +0100

    forgot to include this in ab6bd90

---
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/data/tag/TagWithCount.java
@@ -0,0 +1,30 @@
+package org.vitrivr.cineast.core.data.tag;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Object that extends a tag with its number of occurrences
+ */
+
+public class TagWithCount extends CompleteTag {
+
+  @JsonProperty
+  public int getCount() {
+    return count;
+  }
+
+  private int count;
+
+  public TagWithCount(String id, String name, String description,
+      Preference preference, int count) {
+    super(id, name, description, preference);
+    this.count = count;
+  }
+
+  public TagWithCount(Tag tag, int count) {
+    super(tag.getId(), tag.getName(), tag.getDescription(), tag.getPreference());
+    this.count = count;
+  }
+
+
+}
