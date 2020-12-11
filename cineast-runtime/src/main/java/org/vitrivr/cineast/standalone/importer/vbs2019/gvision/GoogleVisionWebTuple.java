0583ae625216d3093d8ed956cf73430986ae317f
silvanheller <silvan.heller@unibas.ch>
2020-12-11 15:40:59 +0100

    removing preference which never belonged here

--- cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/importer/vbs2019/gvision/GoogleVisionWebTuple.java
+++ cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/importer/vbs2019/gvision/GoogleVisionWebTuple.java
@@ -1,24 +1,20 @@
 package org.vitrivr.cineast.standalone.importer.vbs2019.gvision;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import org.vitrivr.cineast.core.data.tag.Preference;
 
 public class GoogleVisionWebTuple {
 
   public final String labelId;
   public final String description;
   public final float score;
-  public final Preference preference;
 
-  public GoogleVisionWebTuple(String labelId, String description, float score, Preference preference) {
+  public GoogleVisionWebTuple(String labelId, String description, float score) {
     this.labelId = labelId;
     this.description = description;
     this.score = score;
-    this.preference = preference;
   }
 
   public GoogleVisionWebTuple(JsonNode node) {
-    this(node.get("id").asText(), node.get("decription").asText(),
-        (float) node.get("score").asDouble(), null);
+    this(node.get("id").asText(), node.get("decription").asText(), (float) node.get("score").asDouble());
   }
 }
