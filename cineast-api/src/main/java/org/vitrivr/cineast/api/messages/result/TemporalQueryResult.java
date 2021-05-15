bb6365ad16964963bb46658fe52b16b8d26191d3
Viktor Gsteiger <v.gsteiger@unibas.ch>
2021-05-15 10:49:18 +0200

    Implemented requested toSTring method
    - As discussed with Silvan in the PR

--- cineast-api/src/main/java/org/vitrivr/cineast/api/messages/result/TemporalQueryResult.java
+++ cineast-api/src/main/java/org/vitrivr/cineast/api/messages/result/TemporalQueryResult.java
@@ -2,6 +2,8 @@ package org.vitrivr.cineast.api.messages.result;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.List;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.vitrivr.cineast.api.messages.abstracts.AbstractQueryResultMessage;
 import org.vitrivr.cineast.api.messages.interfaces.MessageType;
 import org.vitrivr.cineast.core.data.TemporalObject;
@@ -16,6 +18,11 @@ public class TemporalQueryResult extends AbstractQueryResultMessage<TemporalObje
   @Override
   public MessageType getMessageType() {
     return MessageType.QR_TEMPORAL;
+  }
+
+  @Override
+  public String toString() {
+    return ReflectionToStringBuilder.toString(this, ToStringStyle.JSON_STYLE);
   }
 
 }
