b2095b0ae784f066edf55d46b257a9be830c633a
Tanmay Singal <tanmaysingal@icloud.com>
2021-05-29 13:59:16 +0530

    IIIFConfig file that extends IngestConfig

--- cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/config/IIIFConfig.java
+++ cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/config/IIIFConfig.java
@@ -1,27 +1,55 @@
 package org.vitrivr.cineast.standalone.config;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.LinkedList;
 import java.util.List;
+import org.vitrivr.cineast.core.config.CacheConfig;
+import org.vitrivr.cineast.core.config.DatabaseConfig;
+import org.vitrivr.cineast.core.config.SegmenterConfig;
+import org.vitrivr.cineast.core.data.MediaType;
 
 /**
+ * IIIF configuration used to fetch media files from remote servers.
+ *
  * @author singaltanmay
  * @version 1.0
  * @created 28.05.21
  */
-public class IIIFConfig {
+public class IIIFConfig extends IngestConfig {
 
   /**
    * List of IIIF resource URLs
    */
-  private List<String> resourceUrlList = new LinkedList<>();
+  private List<String> resourceUrls;
 
-  public List<String> getResourceUrlList() {
-    return resourceUrlList;
+  @JsonCreator
+  public IIIFConfig(
+      @JsonProperty(value = "type") MediaType type,
+      @JsonProperty(value = "input", required = true) InputConfig input,
+      @JsonProperty(value = "extractors") List<ExtractorConfig> extractors,
+      @JsonProperty(value = "exporters") List<ExtractorConfig> exporters,
+      @JsonProperty(value = "metadata") List<MetadataConfig> metadata,
+      @JsonProperty(value = "database") DatabaseConfig database,
+      @JsonProperty(value = "pipeline") ExtractionPipelineConfig pipeline,
+      @JsonProperty(value = "segmenter") SegmenterConfig segmenter,
+      @JsonProperty(value = "imagecache") CacheConfig cacheConfig,
+      @JsonProperty(value = "resourceUrls") List<String> resourceUrls) {
+    super(type, input, extractors, exporters, metadata, database, pipeline, segmenter, cacheConfig);
+
+    /* Initialize list of ExtractorConfig. */
+    if (resourceUrls == null) {
+      resourceUrls = new LinkedList<>();
+    }
+    this.resourceUrls = resourceUrls;
   }
 
-  @JsonProperty(required = false)
-  public void setResourceUrlList(List<String> resourceUrlList) {
-    this.resourceUrlList = resourceUrlList;
+  public List<String> getResourceUrls() {
+    return resourceUrls;
+  }
+
+  @JsonProperty
+  public void setResourceUrls(List<String> resourceUrls) {
+    this.resourceUrls = resourceUrls;
   }
 }
