ae23230af90ab25e144a42aee754c63d55ed4342
Tanmay Singal <tanmaysingal@icloud.com>
2021-06-05 12:46:27 +0530

    Download IIIF image and information and perform extraction

--- cineast-core/src/main/java/org/vitrivr/cineast/core/iiif/imageapi/ImageInformationRequest.java
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/iiif/imageapi/ImageInformationRequest.java
@@ -1,12 +1,17 @@
 package org.vitrivr.cineast.core.iiif.imageapi;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -16,21 +21,17 @@ import org.jetbrains.annotations.Nullable;
  */
 public class ImageInformationRequest {
 
-  /**
-   * @param url The Image Information request URL used to make a request to the IIIF server.
-   * @return Received Image Information deserialized into {@link ImageInformation}
-   * @throws IOException if an HTTP connection was not established successfully.
-   */
-  public static ImageInformation fetchImageInformation(String url) throws IOException {
-    HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-    connection.setRequestProperty("accept", "application/json");
-    InputStream responseStream = connection.getInputStream();
-    String response = IOUtils.toString(responseStream, StandardCharsets.UTF_8);
-    return parseImageInformationJson(response);
+  private static final Logger LOGGER = LogManager.getLogger();
+
+  private final String url;
+
+  public ImageInformationRequest(String url) {
+    this.url = url;
   }
 
   /**
    * This has been created as a separate function to help with unit testing.
+   *
    * @param response The JSON response received from the server
    * @return {@link ImageInformation}
    */
@@ -43,5 +44,33 @@ public class ImageInformationRequest {
       e.printStackTrace();
     }
     return imageInformation;
+  }
+
+  /**
+   * @param filePath The base directory where the file has to be written
+   * @param fileName The name of the output file without the extension
+   * @throws IOException If the Http request or writing to file fails
+   */
+  public void saveToFile(String filePath, String fileName) throws IOException {
+    String imageInformation = this.fetchImageInformation();
+    File file = new File(filePath + "/" + fileName + ".json");
+    FileOutputStream fileOutputStream = new FileOutputStream(file);
+    BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+    //write byte array to file
+    bufferedOutputStream.write(imageInformation.getBytes());
+    bufferedOutputStream.close();
+    fileOutputStream.close();
+    LOGGER.debug("Image information request's json response data written to file successfully. Request url:\t" + url);
+  }
+
+  /**
+   * @return Received Image Information JSON String
+   * @throws IOException if an HTTP connection was not established successfully.
+   */
+  public String fetchImageInformation() throws IOException {
+    HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+    connection.setRequestProperty("accept", "application/json");
+    InputStream responseStream = connection.getInputStream();
+    return IOUtils.toString(responseStream, StandardCharsets.UTF_8);
   }
 }
