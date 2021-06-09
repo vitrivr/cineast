f068e73fbd3e8a82d89b7d5fc97260f1743088a3
Tanmay Singal <tanmaysingal@icloud.com>
2021-06-09 17:25:50 +0530

    Switched to an IIIFConfig embedded in InputConfig

--- cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/config/IIIFConfig.java
+++ cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/config/IIIFConfig.java
@@ -1,13 +1,7 @@
 package org.vitrivr.cineast.standalone.config;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.LinkedList;
 import java.util.List;
-import org.vitrivr.cineast.core.config.CacheConfig;
-import org.vitrivr.cineast.core.config.DatabaseConfig;
-import org.vitrivr.cineast.core.config.SegmenterConfig;
-import org.vitrivr.cineast.core.data.MediaType;
 
 /**
  * IIIF configuration used to fetch media files from remote servers.
@@ -16,40 +10,55 @@ import org.vitrivr.cineast.core.data.MediaType;
  * @version 1.0
  * @created 28.05.21
  */
-public class IIIFConfig extends IngestConfig {
+public class IIIFConfig {
 
-  /**
-   * List of IIIF resource URLs
-   */
-  private List<String> resourceUrls;
+  @JsonProperty(value = "url", required = true)
+  private String baseUrl;
 
-  @JsonCreator
-  public IIIFConfig(
-      @JsonProperty(value = "type") MediaType type,
-      @JsonProperty(value = "input", required = true) InputConfig input,
-      @JsonProperty(value = "extractors") List<ExtractorConfig> extractors,
-      @JsonProperty(value = "exporters") List<ExtractorConfig> exporters,
-      @JsonProperty(value = "metadata") List<MetadataConfig> metadata,
-      @JsonProperty(value = "database") DatabaseConfig database,
-      @JsonProperty(value = "pipeline") ExtractionPipelineConfig pipeline,
-      @JsonProperty(value = "segmenter") SegmenterConfig segmenter,
-      @JsonProperty(value = "imagecache") CacheConfig cacheConfig,
-      @JsonProperty(value = "resourceUrls") List<String> resourceUrls) {
-    super(type, input, extractors, exporters, metadata, database, pipeline, segmenter, cacheConfig);
+  @JsonProperty(value = "items")
+  private List<IIIFItem> iiifItems;
 
-    /* Initialize list of ExtractorConfig. */
-    if (resourceUrls == null) {
-      resourceUrls = new LinkedList<>();
+  public String getBaseUrl() {
+    return baseUrl;
+  }
+
+  public void setBaseUrl(String baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  public List<IIIFItem> getIiifItems() {
+    return iiifItems;
+  }
+
+  public void setIiifItems(List<IIIFItem> iiifItems) {
+    this.iiifItems = iiifItems;
+  }
+
+  public static class IIIFItem {
+
+    private String identifier;
+    private String rotation;
+
+    public IIIFItem() {
     }
-    this.resourceUrls = resourceUrls;
+
+    @JsonProperty
+    public String getIdentifier() {
+      return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+      this.identifier = identifier;
+    }
+
+    @JsonProperty
+    public String getRotation() {
+      return rotation;
+    }
+
+    public void setRotation(String rotation) {
+      this.rotation = rotation;
+    }
   }
 
-  public List<String> getResourceUrls() {
-    return resourceUrls;
-  }
-
-  @JsonProperty
-  public void setResourceUrls(List<String> resourceUrls) {
-    this.resourceUrls = resourceUrls;
-  }
 }
