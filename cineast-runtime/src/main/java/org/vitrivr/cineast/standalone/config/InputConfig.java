31a78e6b7e8c9bddaf30c96887160b0a1e63fb55
Tanmay Singal <tanmaysingal@icloud.com>
2021-06-09 17:25:50 +0530

    Switched to an IIIFConfig embedded in InputConfig

--- cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/config/InputConfig.java
+++ cineast-runtime/src/main/java/org/vitrivr/cineast/standalone/config/InputConfig.java
@@ -5,50 +5,66 @@ import org.vitrivr.cineast.core.config.IdConfig;
 
 
 public class InputConfig {
-    private String path;
-    private String name;
-    private String relTo;
-    private Integer depth = 1;
 
-    private IdConfig id = new IdConfig();
+  private String path;
+  private String name;
+  private String relTo;
+  private Integer depth = 1;
+  private IIIFConfig iiif;
 
-    @JsonProperty
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+  private IdConfig id = new IdConfig();
 
-    @JsonProperty(required = true)
-    public String getPath() {
-        return path;
-    }
-    public void setPath(String path) {
-        this.path = path;
-    }
+  @JsonProperty
+  public String getName() {
+    return name;
+  }
 
-    @JsonProperty()
-    public String getRelTo() {
-        return relTo;
-    }
-    public void setRelTo(String relTo) {
-        this.relTo = relTo;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    @JsonProperty
-    public IdConfig getId() {
-        return id;
-    }
-    public void setId(IdConfig id) {
-        this.id = id;
-    }
+  @JsonProperty(required = true)
+  public String getPath() {
+    return path;
+  }
 
-    @JsonProperty
-    public Integer getDepth() {
-        return depth;
-    }
-    public void setDepth(Integer depth) {
-        this.depth = depth;
-    }
+  public void setPath(String path) {
+    this.path = path;
+  }
+
+  @JsonProperty()
+  public String getRelTo() {
+    return relTo;
+  }
+
+  public void setRelTo(String relTo) {
+    this.relTo = relTo;
+  }
+
+  @JsonProperty
+  public IdConfig getId() {
+    return id;
+  }
+
+  public void setId(IdConfig id) {
+    this.id = id;
+  }
+
+  @JsonProperty
+  public Integer getDepth() {
+    return depth;
+  }
+
+  public void setDepth(Integer depth) {
+    this.depth = depth;
+  }
+
+  @JsonProperty
+  public IIIFConfig getIiif() {
+    return iiif;
+  }
+
+  public void setIiif(IIIFConfig iiif) {
+    this.iiif = iiif;
+  }
 }
