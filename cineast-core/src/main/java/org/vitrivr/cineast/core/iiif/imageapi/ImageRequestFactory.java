ac51345186a5748a2bf1bf5cfe7527f40d7e5e61
Tanmay Singal <tanmaysingal@icloud.com>
2021-06-10 22:18:21 +0530

    Ran extraction job using ImageRequestFactory

---
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/iiif/imageapi/ImageRequestFactory.java
@@ -0,0 +1,89 @@
+package org.vitrivr.cineast.core.iiif.imageapi;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import org.vitrivr.cineast.core.iiif.IIIFConfig;
+import org.vitrivr.cineast.core.iiif.IIIFConfig.IIIFItem;
+import org.vitrivr.cineast.core.iiif.imageapi.ImageInformation.IMAGE_API_VERSION;
+import org.vitrivr.cineast.core.iiif.imageapi.v2_1_1.ImageRequestBuilder_v2_1_1;
+import org.vitrivr.cineast.core.iiif.imageapi.v2_1_1.ImageRequestBuilder_v2_1_1_Impl;
+
+/**
+ * @author singaltanmay
+ * @version 1.0
+ * @created 10.06.21
+ */
+public class ImageRequestFactory {
+
+  private final IMAGE_API_VERSION imageApiVersion;
+  private final IIIFConfig iiifConfig;
+
+  public ImageRequestFactory(IIIFConfig iiifConfig) {
+    this.iiifConfig = iiifConfig;
+    imageApiVersion = ImageInformation.getImageApiVersionNumeric(iiifConfig.getImageApiVersion());
+  }
+
+  public List<ImageRequest> createImageRequests(String jobDirectoryString, String itemPrefixString) {
+    if (imageApiVersion == IMAGE_API_VERSION.TWO_POINT_ONE_POINT_ONE) {
+      return runImageApi_v2_1_1_job(jobDirectoryString, itemPrefixString);
+    }
+    return new LinkedList<>();
+  }
+
+  private List<ImageRequest> runImageApi_v2_1_1_job(String jobDirectoryString, String itemPrefixString) {
+    List<ImageRequest> imageRequests = new LinkedList<>();
+    List<IIIFItem> iiifItems = iiifConfig.getIiifItems();
+    if (iiifItems == null) {
+      return imageRequests;
+    }
+    for (final IIIFItem iiifItem : iiifItems) {
+      String identifier = iiifItem.getIdentifier();
+      final String imageName = itemPrefixString + identifier;
+
+      ImageInformation imageInformation = null;
+      try {
+        final ImageInformationRequest informationRequest = new ImageInformationRequest(iiifConfig.getBaseUrl() + "/" + identifier);
+        informationRequest.saveToFile(jobDirectoryString, imageName);
+        imageInformation = informationRequest.getImageInformation(null);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      ImageRequestBuilder_v2_1_1 builder;
+      if (imageInformation != null) {
+        builder = new ImageRequestBuilder_v2_1_1_Impl(imageInformation);
+      } else {
+        builder = new ImageRequestBuilder_v2_1_1_Impl(iiifConfig.getBaseUrl());
+      }
+
+      float rotationDegree;
+      if (iiifItem.getRotation() != null) {
+        try {
+          rotationDegree = Float.parseFloat(iiifItem.getRotation());
+        } catch (NumberFormatException e) {
+          e.printStackTrace();
+          continue;
+        }
+      } else {
+        rotationDegree = 0;
+      }
+
+      ImageRequest imageRequest = builder
+          .setRegionFull()
+          .setSizeFull()
+          .setRotation(rotationDegree, false)
+          .setQuality(ImageRequestBuilder.QUALITY_DEFAULT)
+          .setExtension(ImageRequestBuilder.EXTENSION_JPG)
+          .build();
+
+      try {
+        imageRequest.saveToFile(jobDirectoryString, imageName);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      imageRequests.add(imageRequest);
+    }
+    return imageRequests;
+  }
+}
