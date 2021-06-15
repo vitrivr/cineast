28c7c9fb5b502bdeb262070fa8de62d9b94382a4
Tanmay Singal <tanmaysingal@icloud.com>
2021-06-16 00:53:48 +0530

    Percent encoded the path paramters of ImageRequest
    Former-commit-id: c891bea147823e5db7bf6af655f23981cfe7ebcb

---
+++ cineast-core/src/test/java/org/vitrivr/cineast/core/iiif/imageapi/ImageRequestTest.java
@@ -0,0 +1,39 @@
+package org.vitrivr.cineast.core.iiif.imageapi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author singaltanmay
+ * @version 1.0
+ * @created 16.06.21
+ */
+public class ImageRequestTest {
+
+  /**
+   * Tests the percent encoding of url path parameters
+   */
+  @Test
+  void percentEncode() {
+    String encoded = ImageRequest.percentEncode("https://example.com/?54#a%3C69:FT[@someone]");
+    assertEquals("https:%2F%2Fexample.com%2F%3F54%23a%253C69:FT%5B%40someone%5D", encoded);
+  }
+
+  /**
+   * Tests the order of the path parameters in the final url
+   */
+  @Test
+  void generateIIIFRequestUrl() {
+    ImageRequest imageRequest = new ImageRequest();
+    imageRequest.setBaseUrl("https://example.com/test");
+    imageRequest.setRegion("full");
+    imageRequest.setSize("max");
+    imageRequest.setRotation("0");
+    imageRequest.setQuality("default");
+    imageRequest.setExtension("jpg");
+    String url = imageRequest.generateIIIFRequestUrl();
+    assertEquals("https://example.com/test/full/max/0/default.jpg", url);
+  }
+
+}
