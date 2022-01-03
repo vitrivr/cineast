35942ca6e7354440f97f6444b10431a5f77b1a8e
Ralph Gasser <rg@pontius.ch>
2022-01-03 21:25:39 +0100

    Removed embedded Cottontail DB.

--- cineast-core/src/test/java/org/vitrivr/cineast/core/db/cottontaildb/CottontailIntegrationDBProvider.java
+++ cineast-core/src/test/java/org/vitrivr/cineast/core/db/cottontaildb/CottontailIntegrationDBProvider.java
@@ -1,45 +1,21 @@
 package org.vitrivr.cineast.core.db.cottontaildb;
 
-import java.nio.file.Paths;
 import java.util.function.Supplier;
 import org.vitrivr.cineast.core.config.DatabaseConfig;
 import org.vitrivr.cineast.core.db.DBSelector;
 import org.vitrivr.cineast.core.db.IntegrationDBProvider;
 import org.vitrivr.cineast.core.db.PersistencyWriter;
 import org.vitrivr.cineast.core.db.setup.EntityCreator;
-import org.vitrivr.cottontail.CottontailKt;
 import org.vitrivr.cottontail.client.language.dml.Insert;
-import org.vitrivr.cottontail.config.CacheConfig;
-import org.vitrivr.cottontail.config.Config;
-import org.vitrivr.cottontail.config.ExecutionConfig;
-import org.vitrivr.cottontail.config.MapDBConfig;
-import org.vitrivr.cottontail.config.ServerConfig;
-import org.vitrivr.cottontail.server.grpc.CottontailGrpcServer;
 
 public class CottontailIntegrationDBProvider implements IntegrationDBProvider<Insert> {
-
-
-  private static final Config COTTONTAIL_TEST_CONFIG = new Config(
-      Paths.get("./build/data/cottontaildb-data/"),
-      false,
-      false,
-      null,
-      new MapDBConfig(),
-      new ServerConfig(),
-      new ExecutionConfig(),
-      new CacheConfig()
-  );
 
   private static final Supplier<DatabaseConfig> WRAPPER_CONFIG_PROVIDER = () -> {
     DatabaseConfig config = new DatabaseConfig();
     config.setHost("localhost");
-    config.setPort(COTTONTAIL_TEST_CONFIG.getServer().getPort());
+    config.setPort(1865);
     return config;
   };
-
-
-  /** The embedded {@link CottontailGrpcServer} used by this {@link CottontailIntegrationDBProvider}. */
-  private final CottontailGrpcServer embedded;
 
   /** The {@link CottontailWrapper} used to establish a database connection. */
   private final CottontailWrapper wrapper;
@@ -48,13 +24,7 @@ public class CottontailIntegrationDBProvider implements IntegrationDBProvider<In
    * Constructor.
    */
   public CottontailIntegrationDBProvider() {
-    this.embedded = CottontailKt.embedded(COTTONTAIL_TEST_CONFIG);
-    try {
-      this.wrapper = new CottontailWrapper(WRAPPER_CONFIG_PROVIDER.get().getHost(), WRAPPER_CONFIG_PROVIDER.get().getPort());
-    } catch (Throwable e) {
-      this.embedded.stop(); /* Stop server to relinquish locks on catalogue. */
-      throw e;
-    }
+    this.wrapper = new CottontailWrapper(WRAPPER_CONFIG_PROVIDER.get().getHost(), WRAPPER_CONFIG_PROVIDER.get().getPort());
   }
 
   CottontailWrapper getWrapper() {
@@ -78,8 +48,6 @@ public class CottontailIntegrationDBProvider implements IntegrationDBProvider<In
 
   @Override
   public void close() {
-    if (this.embedded.isRunning()) {
-      this.embedded.stop();
-    }
+    /* No op. */
   }
 }
