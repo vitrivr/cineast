d9efa6b55df7dc5fb3601f7017508736046a0f43
Silvan Heller <silvan.heller@unibas.ch>
2022-02-10 22:12:01 +0100

    Fixes for Cottontail DB Adapter (#260)
    * CottontailSelector.existsEntity() properly handles NOT_FOUND exception. Fixes #255
    
    * Connection output is less verbose. What is logged is logged at DEBUG level now. Addresses #258
    
    * Initialization steps are now wrapped in a try...catch block, which should prevent failing components from killing the CLI. Addresses #257
    
    * More fine-grained try...catch blocks, so that we know exactly, which component failed.
    
    * Removed try...catch around server.start()
    
    Co-authored-by: Ralph Gasser <rg@pontius.ch>

--- cineast-core/src/main/java/org/vitrivr/cineast/core/db/cottontaildb/CottontailWrapper.java
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/db/cottontaildb/CottontailWrapper.java
@@ -65,11 +65,11 @@ public final class CottontailWrapper implements AutoCloseable {
     final NettyChannelBuilder builder = NettyChannelBuilder.forAddress(host, port).usePlaintext();
     final ManagedChannel channel = builder.build();
     Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      LOGGER.info("Closing connection to Cottontail DB.");
+      LOGGER.debug("Closing connection to Cottontail DB.");
       channel.shutdownNow();
     }));
     watch.stop();
-    LOGGER.info("Connected to Cottontail DB in {} ms at {}:{}", watch.getTime(TimeUnit.MILLISECONDS), host, port);
+    LOGGER.debug("Connected to Cottontail DB in {} ms at {}:{}", watch.getTime(TimeUnit.MILLISECONDS), host, port);
     return channel;
   }
 
@@ -91,11 +91,7 @@ public final class CottontailWrapper implements AutoCloseable {
     this.client = new SimpleClient(sharedChannel(host, port));
     boolean pingSuccessful = this.client.ping();
     watch.stop();
-    if (pingSuccessful) {
-      LOGGER.info("Connected to Cottontail in {} ms at {}:{}", watch.getTime(TimeUnit.MILLISECONDS), host, port);
-    } else {
-      LOGGER.warn("Could not connect to Cottontail at {}:{}", host, port);
-    }
+    if (!pingSuccessful) LOGGER.warn("Could not ping Cottontail DB instance at {}:{}", host, port);
   }
 
   /**
