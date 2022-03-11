bc210d14e3944de2b32bb0cb182015913ae68597
Florian Spiess <florian.spiess@unibas.ch>
2022-03-11 14:57:20 +0100

    Removed deprecated ADAMpro database connector. (#276)
    Former-commit-id: beef1be71f8bb18cd84c643837f1528044414123

--- cineast-core/src/main/java/org/vitrivr/cineast/core/db/DataSource.java
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/db/DataSource.java
@@ -3,10 +3,6 @@ package org.vitrivr.cineast.core.db;
 import java.io.File;
 import java.util.function.Supplier;
 import org.vitrivr.cineast.core.config.DatabaseConfig;
-import org.vitrivr.cineast.core.db.adampro.ADAMproEntityCreator;
-import org.vitrivr.cineast.core.db.adampro.ADAMproSelector;
-import org.vitrivr.cineast.core.db.adampro.ADAMproWrapper;
-import org.vitrivr.cineast.core.db.adampro.ADAMproWriter;
 import org.vitrivr.cineast.core.db.cottontaildb.CottontailEntityCreator;
 import org.vitrivr.cineast.core.db.cottontaildb.CottontailSelector;
 import org.vitrivr.cineast.core.db.cottontaildb.CottontailWrapper;
@@ -32,10 +28,7 @@ public enum DataSource {
   JSON,
   COTTONTAIL,
   POLYPHENY,
-  INMEMORY,
-
-  @Deprecated
-  ADAMPRO;
+  INMEMORY;
 
   /**
    * Returns a new {@link PersistencyWriterSupplier}
@@ -53,8 +46,6 @@ public enum DataSource {
         return () -> new PolyphenyWriter(new PolyphenyWrapper(config.getHost(), config.getPort()), config.getBatchsize());
       case JSON:
         return () -> new JsonFileWriter(new File(config.getHost()));
-      case ADAMPRO:
-        return () -> new ADAMproWriter(new ADAMproWrapper(config));
       default:
         throw new IllegalStateException("No supplier for " + this + " selector.");
     }
@@ -76,8 +67,6 @@ public enum DataSource {
         return () -> new PolyphenySelector(new PolyphenyWrapper(config.getHost(), config.getPort()));
       case JSON:
         return () -> new JsonSelector(new File(config.getHost()));
-      case ADAMPRO:
-        return () -> new ADAMproSelector(new ADAMproWrapper(config));
       default:
         throw new IllegalStateException("No supplier for " + this + " selector.");
     }
@@ -99,8 +88,6 @@ public enum DataSource {
         return () -> new CottontailEntityCreator(new CottontailWrapper(config.getHost(), config.getPort()));
       case POLYPHENY:
         return () -> new PolyphenyEntityCreator(new PolyphenyWrapper(config.getHost(), config.getPort()));
-      case ADAMPRO:
-        return () -> new ADAMproEntityCreator(new ADAMproWrapper(config));
       default:
         throw new IllegalStateException("No supplier for " + this + " entity creator.");
     }
