df5d9f064ed4b1c3b508542ed2bae22f4af788b4
Luca Rossetto <lucaro@lucaro.ch>
2022-03-18 07:49:35 +0100

    Added caching to FileSystemObjectResolver

--- cineast-api/src/main/java/org/vitrivr/cineast/api/rest/resolvers/FileSystemObjectResolver.java
+++ cineast-api/src/main/java/org/vitrivr/cineast/api/rest/resolvers/FileSystemObjectResolver.java
@@ -1,53 +1,80 @@
 package org.vitrivr.cineast.api.rest.resolvers;
 
-import java.io.File;
-import java.io.IOException;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.vitrivr.cineast.core.data.entities.MediaObjectDescriptor;
 import org.vitrivr.cineast.core.db.dao.reader.MediaObjectReader;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 public class FileSystemObjectResolver implements ObjectResolver, AutoCloseable {
 
-  private final MediaObjectReader lookup;
-  private final File baseDir;
+    private final MediaObjectReader lookup;
+    private final File baseDir;
 
-  private final ObjectToFileResolver object2File;
+    private final LoadingCache<String, MediaObjectDescriptor> descriptorLoadingCache;
 
-  public FileSystemObjectResolver(File basedir, MediaObjectReader lookup) {
-    this.lookup = lookup;
-    this.baseDir = basedir;
-    object2File = (dir, obj) -> new File(dir, obj.getPath());
-  }
+    private final ObjectToFileResolver object2File;
 
-  public FileSystemObjectResolver(File basedir, MediaObjectReader lookup, ObjectToFileResolver transform) {
-    this.lookup = lookup;
-    this.baseDir = basedir;
-    object2File = transform;
-  }
+    private static final Logger LOGGER = LogManager.getLogger();
 
-
-  @Override
-  public ResolutionResult resolve(String id) {
-
-    if (id == null) {
-      return null;
+    public FileSystemObjectResolver(File basedir, MediaObjectReader lookup) {
+        this(basedir, lookup, (dir, obj) -> new File(dir, obj.getPath()));
     }
 
-    MediaObjectDescriptor descriptor = this.lookup.lookUpObjectById(id);
+    public FileSystemObjectResolver(File basedir, MediaObjectReader lookup, ObjectToFileResolver transform) {
+        this.lookup = lookup;
+        this.baseDir = basedir;
+        this.object2File = transform;
 
-    if (!descriptor.exists()) {
-      return null;
+        this.descriptorLoadingCache = CacheBuilder.newBuilder()
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .build(
+                        new CacheLoader<>() {
+                            @Override
+                            public MediaObjectDescriptor load(String key) {
+                                return lookup.lookUpObjectById(key);
+                            }
+                        }
+                );
     }
 
-    try {
-      return new ResolutionResult(object2File.resolve(baseDir, descriptor));
-    } catch (IOException e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
 
-  @Override
-  public void close() {
-    this.lookup.close();
-  }
+    @Override
+    public ResolutionResult resolve(String id) {
+
+        if (id == null) {
+            return null;
+        }
+
+        MediaObjectDescriptor descriptor;
+        try {
+            descriptor = descriptorLoadingCache.get(id);
+        } catch (ExecutionException e) {
+            LOGGER.error(e);
+            return null;
+        }
+
+        if (!descriptor.exists()) {
+            return null;
+        }
+
+        try {
+            return new ResolutionResult(object2File.resolve(baseDir, descriptor));
+        } catch (IOException e) {
+            LOGGER.error(e);
+            return null;
+        }
+    }
+
+    @Override
+    public void close() {
+        this.lookup.close();
+    }
 }
