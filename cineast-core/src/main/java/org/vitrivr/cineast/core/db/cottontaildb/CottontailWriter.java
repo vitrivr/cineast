69279b8d6936d6c224987681d681e88c51d876ed
Ralph Gasser <ppanopticon@users.noreply.github.com>
2022-03-30 21:30:59 +0200

    Fixed issue that would cause a deadlock during batch inserts. (#287)
    Former-commit-id: 44ffa3b02a70ef9d0423cb1f503c48f24911b0a7

--- cineast-core/src/main/java/org/vitrivr/cineast/core/db/cottontaildb/CottontailWriter.java
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/db/cottontaildb/CottontailWriter.java
@@ -82,6 +82,9 @@ public final class CottontailWriter extends AbstractPersistencyWriter<Insert> {
           LOGGER.trace("Inserting msg of size {} into {}", insert.size(), this.fqn);
           this.cottontail.client.insert(insert);
           insert = new BatchInsert().into(this.fqn).columns(this.names);
+          if(useTransactions){
+            insert.txId(txId);
+          }
         }
       }
       if (insert.getBuilder().getInsertsCount() > 0) {
