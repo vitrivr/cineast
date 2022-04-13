9591b8a959c69dc7470c5cb859cfb7a099dd3cfc
Silvan Heller <silvan.heller@unibas.ch>
2022-04-13 16:06:58 +0200

    returning empty result on error which is consistent with rest api behavior

--- cineast-api/src/main/java/org/vitrivr/cineast/api/rest/handlers/actions/mediaobject/FindObjectPaginationGetHandler.java
+++ cineast-api/src/main/java/org/vitrivr/cineast/api/rest/handlers/actions/mediaobject/FindObjectPaginationGetHandler.java
@@ -6,6 +6,7 @@ import static org.vitrivr.cineast.api.util.APIConstants.SKIP_NAME;
 import io.javalin.http.Context;
 import io.javalin.plugin.openapi.dsl.OpenApiBuilder;
 import io.javalin.plugin.openapi.dsl.OpenApiDocumentation;
+import java.util.ArrayList;
 import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -24,15 +25,18 @@ public class FindObjectPaginationGetHandler implements GetRestHandler<MediaObjec
   public MediaObjectQueryResult doGet(Context ctx) {
     final Map<String, String> parameters = ctx.pathParamMap();
 
-    final var skipParam = parameters.get(SKIP_NAME);
-    final var skip = skipParam == null ? 0 : Integer.parseInt(skipParam);
-    final var limitParam = parameters.get(LIMIT_NAME);
-    final var limit = limitParam == null ? Integer.MAX_VALUE : Integer.parseInt(limitParam);
-
     try (final MediaObjectReader ol = new MediaObjectReader(Config.sharedConfig().getDatabase().getSelectorSupplier().get())) {
+      final var skipParam = parameters.get(SKIP_NAME);
+      final var skip = skipParam == null ? 0 : Integer.parseInt(skipParam);
+      final var limitParam = parameters.get(LIMIT_NAME);
+      final var limit = limitParam == null ? Integer.MAX_VALUE : Integer.parseInt(limitParam);
+
       var result = ol.getAllObjects(skip, limit);
       LOGGER.trace("returning {} elements for skip {} and limit {}", result.size(), skip, limit);
       return new MediaObjectQueryResult("", result);
+    } catch (Exception e) {
+      LOGGER.error("Error during request", e);
+      return new MediaObjectQueryResult("", new ArrayList<>());
     }
   }
 
