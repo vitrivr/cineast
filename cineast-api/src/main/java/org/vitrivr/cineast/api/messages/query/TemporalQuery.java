103fcc1af4a74c52db90a3cbcf9f1bb0f1f2b5d8
Florian Spiess <florian.spiess@unibas.ch>
2022-05-19 15:45:57 +0200

    Zero-Config REST Temporal Queries (#317)

--- cineast-api/src/main/java/org/vitrivr/cineast/api/messages/query/TemporalQuery.java
+++ cineast-api/src/main/java/org/vitrivr/cineast/api/messages/query/TemporalQuery.java
@@ -2,6 +2,7 @@ package org.vitrivr.cineast.api.messages.query;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.ArrayList;
 import java.util.List;
 import org.vitrivr.cineast.api.messages.interfaces.MessageType;
 import org.vitrivr.cineast.api.messages.interfaces.Query;
@@ -10,7 +11,7 @@ import org.vitrivr.cineast.core.db.dao.MetadataAccessSpecification;
 /**
  * This object represents a temporal-query message of temporal query version 2, i.e. a request for a temporally staged similarity-search.
  */
-public record TemporalQuery(@JsonProperty(required = true) List<StagedSimilarityQuery> queries, List<MetadataAccessSpecification> metadataAccessSpec, TemporalQueryConfig config, @JsonProperty(required = true) MessageType messageType) implements Query {
+public record TemporalQuery(@JsonProperty(required = true) List<StagedSimilarityQuery> queries, List<MetadataAccessSpecification> metadataAccessSpec, TemporalQueryConfig config, MessageType messageType) implements Query {
 
   /**
    * Convenience getter for timeDistances.
@@ -19,7 +20,7 @@ public record TemporalQuery(@JsonProperty(required = true) List<StagedSimilarity
    */
   @JsonIgnore
   public List<Float> getTimeDistances() {
-    return config().timeDistances;
+    return config == null ? new ArrayList<>() : config().timeDistances;
   }
 
   /**
@@ -29,7 +30,7 @@ public record TemporalQuery(@JsonProperty(required = true) List<StagedSimilarity
    */
   @JsonIgnore
   public Float getMaxLength() {
-    return config().maxLength;
+    return config == null ? null : config().maxLength;
   }
 
 }
