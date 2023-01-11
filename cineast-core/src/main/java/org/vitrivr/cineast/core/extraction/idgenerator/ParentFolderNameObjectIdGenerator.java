6bc39c139ff030f6831d365f713e39a8b16442ac
Raphael <raphael.waltenspuel@unibas.ch>
2023-01-11 19:13:38 +0100

    Changed to Optional<String> instead of null to match interface

--- cineast-core/src/main/java/org/vitrivr/cineast/core/extraction/idgenerator/ParentFolderNameObjectIdGenerator.java
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/extraction/idgenerator/ParentFolderNameObjectIdGenerator.java
@@ -2,14 +2,17 @@ package org.vitrivr.cineast.core.extraction.idgenerator;
 
 import java.nio.file.Path;
 import java.util.Map;
+import java.util.Optional;
 import org.apache.commons.io.FilenameUtils;
 import org.vitrivr.cineast.core.data.MediaType;
 
 /**
  * Generates object IDs from the containing parent directory of the object file.
+ * <p>
  * The prerequisite is that there is only one media type in the folder at a time that is to be extracted.
  * Includes type prefix if "prefix" property is set to true.
  * For example, this ObjectIdGenerator is suitable for 3D models that are organized in folders together with textures, lights, etc .
+ * <p>
  * E.g. if you have a model in the folder <i>./models/314159/scene.gltf</i> you will get the id <i>{"id":"314159_1",... </i> or with prefix <i>{"id":"prefix_314159_1",...</i>
  */
 public class ParentFolderNameObjectIdGenerator implements ObjectIdGenerator {
@@ -31,13 +34,14 @@ public class ParentFolderNameObjectIdGenerator implements ObjectIdGenerator {
   }
 
   @Override
-  public String next(Path path, MediaType type) {
+  public Optional<String> next(Path path, MediaType type) {
 
     if (path == null) {
-      return "null";
+      return Optional.empty();
     }
 
     String filename = FilenameUtils.getBaseName(path.toFile().getParentFile().getName());
-    return prefix ? MediaType.generateId(type, filename) : filename;
+    String id = prefix ? MediaType.generateId(type, filename) : filename;
+    return Optional.of(id);
   }
 }
