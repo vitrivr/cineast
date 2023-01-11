f19e4a843b1a5e6f2031e3adeebddadbc616705a
Raphael <raphael.waltenspuel@unibas.ch>
2023-01-11 19:04:56 +0100

    Added explanation and prerequisites to Javadoc

--- cineast-core/src/main/java/org/vitrivr/cineast/core/extraction/idgenerator/ParentFolderNameObjectIdGenerator.java
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/extraction/idgenerator/ParentFolderNameObjectIdGenerator.java
@@ -7,8 +7,10 @@ import org.vitrivr.cineast.core.data.MediaType;
 
 /**
  * Generates object IDs from the containing parent directory of the object file.
- * <p>
+ * The prerequisite is that there is only one media type in the folder at a time that is to be extracted.
  * Includes type prefix if "prefix" property is set to true.
+ * For example, this ObjectIdGenerator is suitable for 3D models that are organized in folders together with textures, lights, etc .
+ * E.g. if you have a model in the folder <i>./models/314159/scene.gltf</i> you will get the id <i>{"id":"314159_1",... </i> or with prefix <i>{"id":"prefix_314159_1",...</i>
  */
 public class ParentFolderNameObjectIdGenerator implements ObjectIdGenerator {
 
