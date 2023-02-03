3cf9c16cf081739340bbf22a2ae81fcc12ffc278
Raphael <raphael.waltenspuel@unibas.ch>
2023-02-03 07:54:02 +0100

    Front image for node

--- cineast-core/src/main/java/org/vitrivr/cineast/core/features/VisualTextCoEmbedding.java
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/features/VisualTextCoEmbedding.java
@@ -325,7 +325,7 @@ public class VisualTextCoEmbedding extends AbstractFeatureModule {
     var renderOptions = new RenderOptions() {{
       this.showTextures = true;
     }};
-    var viewpointStrategy = ViewpointStrategy.UPPER_LEFT;
+    var viewpointStrategy = ViewpointStrategy.FRONT;
     // Get camera viewpoint for chhosen strategy
     var camerapositions = getCameraPositions(viewpointStrategy, model);
     // Render an image for each camera position
