ae80d575c9c756a5ddd286690829312437c12bf8
Raphael <raphael.waltenspuel@unibas.ch>
2023-04-25 11:37:45 +0200

    Changed default return value of  ModelProvider to EMPTY model

--- cineast-core/src/main/java/org/vitrivr/cineast/core/data/providers/ModelProvider.java
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/data/providers/ModelProvider.java
@@ -1,6 +1,7 @@
 package org.vitrivr.cineast.core.data.providers;
 
 import org.vitrivr.cineast.core.data.m3d.texturemodel.IModel;
+import org.vitrivr.cineast.core.data.m3d.texturemodel.Model;
 import org.vitrivr.cineast.core.data.m3d.texturemodel.ModelLoader;
 
 public interface ModelProvider {
@@ -11,13 +12,6 @@ public interface ModelProvider {
    * @return Model
    */
   default IModel getModel() {
-    return lazyCreateModel();
-  }
-  // TODO Remove when Segment loader is implemented
-  default  IModel lazyCreateModel() {
-    var model = ModelLoader.loadModel("test",
-        "C:\\Users\\rapha\\Documents\\myRepo\\ch.unibas\\Class\\vitrivr\\bsc-raphael-waltenspuel\\datasets_git\\TestSketchfab\\76a3f7bf75d049458dfaa48aa342e0b8\\baluster_vase_from_a_five-piece_garniture\\scene.gltf"
-    );
-    return model;
+    return Model.EMPTY;
   }
 }
