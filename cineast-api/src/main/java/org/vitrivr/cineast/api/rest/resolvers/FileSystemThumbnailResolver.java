f94b88b5de71fa2ed25084d4e85ebda23550d52e
Florian Spiess <florian.spiess@unibas.ch>
2023-12-01 12:58:44 +0100

    Update FileSystemThumbnailResolver to include candidates without media type prefix.

--- cineast-api/src/main/java/org/vitrivr/cineast/api/rest/resolvers/FileSystemThumbnailResolver.java
+++ cineast-api/src/main/java/org/vitrivr/cineast/api/rest/resolvers/FileSystemThumbnailResolver.java
@@ -23,25 +23,35 @@ public class FileSystemThumbnailResolver implements ThumbnailResolver {
     }
 
     String[] split = segmentId.split("_");
-    if (split.length < 3) {
-      LOGGER.error("invalid segment id {}", segmentId);
+    if (split.length < 2) {
+      LOGGER.error("unknown segment id format {}", segmentId);
       return null;
     }
 
     File[] candidates = new File[]{
-        new File(baseFolder, split[0] + "_" + split[1] + "/" + split[2] + ".jpg"),
-        new File(baseFolder, split[0] + "_" + split[1] + "/" + segmentId + ".jpg"),
-        new File(baseFolder, split[0] + "_" + String.join("_", Arrays.copyOfRange(split, 1, split.length - 1)) + "/" + segmentId + ".jpg"),
-        new File(baseFolder, split[0] + "_" + String.join("_", Arrays.copyOfRange(split, 1, split.length - 1)) + "/" + segmentId + ".png"),
-        new File(baseFolder, split[0] + "_" + split[1] + "/" + split[2] + ".png"),
-        new File(baseFolder, split[0] + "_" + split[1] + "/" + segmentId + ".png"),
-        new File(baseFolder, split[1] + "/" + split[2] + ".jpg"),
-        new File(baseFolder, split[1] + "/" + split[2] + ".png"),
-        new File(baseFolder, split[1] + "/" + split[1] + "_" + split[2] + ".jpg"),
-        new File(baseFolder, split[1] + "/" + split[1] + "_" + split[2] + ".png"),
-        new File(baseFolder, split[1] + "/shot" + split[1] + "_" + split[2] + ".jpg"),
-        new File(baseFolder, split[1] + "/shot" + split[1] + "_" + split[2] + ".png"),
+        new File(baseFolder, segmentId + ".jpg"),
+        new File(baseFolder, split[0] + "/" + split[1] + ".jpg")
     };
+
+    // If there are 3 or more elements in the split array, include appropriate candidates
+    if (split.length >= 3) {
+      File[] extraCandidates = new File[]{
+          new File(baseFolder, split[0] + "_" + split[1] + "/" + split[2] + ".jpg"),
+          new File(baseFolder, split[0] + "_" + split[1] + "/" + segmentId + ".jpg"),
+          new File(baseFolder, split[0] + "_" + String.join("_", Arrays.copyOfRange(split, 1, split.length - 1)) + "/" + segmentId + ".jpg"),
+          new File(baseFolder, split[0] + "_" + String.join("_", Arrays.copyOfRange(split, 1, split.length - 1)) + "/" + segmentId + ".png"),
+          new File(baseFolder, split[0] + "_" + split[1] + "/" + split[2] + ".png"),
+          new File(baseFolder, split[0] + "_" + split[1] + "/" + segmentId + ".png"),
+          new File(baseFolder, split[1] + "/" + split[2] + ".jpg"),
+          new File(baseFolder, split[1] + "/" + split[2] + ".png"),
+          new File(baseFolder, split[1] + "/" + split[1] + "_" + split[2] + ".jpg"),
+          new File(baseFolder, split[1] + "/" + split[1] + "_" + split[2] + ".png"),
+          new File(baseFolder, split[1] + "/shot" + split[1] + "_" + split[2] + ".jpg"),
+          new File(baseFolder, split[1] + "/shot" + split[1] + "_" + split[2] + ".png")
+      };
+      candidates = Arrays.copyOf(candidates, candidates.length + extraCandidates.length);
+      System.arraycopy(extraCandidates, 0, candidates, candidates.length - extraCandidates.length, extraCandidates.length);
+    }
 
     for (File candidate : candidates) {
       if (candidate.exists() && candidate.canRead()) {
