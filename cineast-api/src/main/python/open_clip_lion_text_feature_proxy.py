15b3d8d2f631a74a56ec2970565dc608deed7c2e
Florian Spiess <florian.spiess@unibas.ch>
2024-03-25 14:30:11 +0100

    Set model to eval mode

--- cineast-api/src/main/python/open_clip_lion_text_feature_proxy.py
+++ cineast-api/src/main/python/open_clip_lion_text_feature_proxy.py
@@ -13,6 +13,7 @@ args = parser.parse_args()
 
 model, _, _ = open_clip.create_model_and_transforms('xlm-roberta-base-ViT-B-32', pretrained='laion5b_s13b_b90k')
 model = model.to(args.device)
+model.eval()
 tokenizer = open_clip.get_tokenizer('xlm-roberta-base-ViT-B-32')
 
 app = Flask(__name__)
